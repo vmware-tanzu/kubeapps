79e14bc6bf12578250cf70a7687d91a0764062f2
Tuna <ng.tuna@gmail.com>
2017-10-27 15:34:34 +0700

    first try to kubeapps up/down

--- cmd/up.go
+++ cmd/up.go
@@ -1,35 +1,68 @@
-package main
+package cmd
 
 import (
-	"github.com/Sirupsen/logrus"
+	"os"
+
+	"github.com/ksonnet/kubecfg/metadata"
+	"github.com/ksonnet/kubecfg/pkg/kubecfg"
 	"github.com/spf13/cobra"
 	"k8s.io/client-go/pkg/api"
+	"path/filepath"
+)
+
+const (
+	GcTag = "bitnami/kubeapps"
 )
 
 var upCmd = &cobra.Command{
 	Use:   "up FLAG",
 	Short: "install KubeApps components",
 	Long:  `install KubeApps components`,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
+		c := kubecfg.ApplyCmd{}
 		ns, err := cmd.Flags().GetString("namespace")
+		c.DefaultNamespace = ns
 		if err != nil {
-			logrus.Fatal(err.Error())
+			return err
 		}
 
-		out, err := cmd.Flags().GetString("out")
+		c.Create = true
+
+		c.DryRun, err = cmd.Flags().GetBool("dry-run")
 		if err != nil {
-			logrus.Fatal(err.Error())
+			return err
 		}
 
-		dryRun, err := cmd.Flags().GetBool("dry-run")
+		c.GcTag = GcTag
+
+		c.ClientPool, c.Discovery, err = restClientPool()
 		if err != nil {
-			logrus.Fatal(err.Error())
+			return err
 		}
+
+		cwd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		wd := metadata.AbsPath(cwd)
+
+		home, err := getHome()
+		if err != nil {
+			return err
+		}
+		kaManifestDir := filepath.Join(home, KUBEAPPS_DIR)
+
+		objs, err := parseObjects(kaManifestDir)
+		if err != nil {
+			return err
+		}
+
+		return c.Run(objs, wd)
 	},
 }
 
 func init() {
+	RootCmd.AddCommand(upCmd)
 	upCmd.Flags().StringP("namespace", "", api.NamespaceDefault, "Specify namespace for the KubeApps components")
 	upCmd.Flags().Bool("dry-run", false, "Provides output to be submitted to the server")
-	upCmd.Flags().StringP("out", "o", "", "Output format. One of: json|yaml")
 }
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+
+	"github.com/kubeapps/installer/cmd"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	if err := cmd.RootCmd.Execute(); err != nil {
+		logrus.Error(err.Error())
+		os.Exit(1)
+	}
+}
