e7dbeb889d2743c79abc51acbac6441bd8cd8504
Tuna <ng.tuna@gmail.com>
2017-11-06 11:41:00 +0700

    minor changes

--- cmd/version.go
+++ cmd/version.go
@@ -11,7 +11,7 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version of Kubeapps Installer",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Installer version: " + VERSION)
+		fmt.Println("Kubeapps Installer version: " + VERSION)
 	},
 }
 
