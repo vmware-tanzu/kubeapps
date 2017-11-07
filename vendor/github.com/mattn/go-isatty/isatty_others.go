793a910ae64c54ccd85f7c10f7164e9566e3b409
Tuna <ng.tuna@gmail.com>
2017-11-07 13:53:28 +0700

    resolve glide conflict, strip vendor

--- vendor/github.com/mattn/go-isatty/isatty_others.go
+++ vendor/github.com/mattn/go-isatty/isatty_others.go
@@ -1,10 +1,9 @@
-// +build darwin
 // +build !windows
 // +build !appengine
 
 package isatty
 
-// IsCygwinTerminal() return true if the file descriptor is a cygwin or msys2
+// IsCygwinTerminal return true if the file descriptor is a cygwin or msys2
 // terminal. This is also always false on this environment.
 func IsCygwinTerminal(fd uintptr) bool {
 	return false
