587fdc887191d2ba7f23fdf3eee18a9057837039
Andres <andres@bitnami.com>
2018-02-07 11:57:56 +0100

    Update autogenerated code

--- pkg/client/informers/externalversions/internalinterfaces/factory_interfaces.go
+++ pkg/client/informers/externalversions/internalinterfaces/factory_interfaces.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Bitnami.
+Copyright 2018 Bitnami.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -19,11 +19,12 @@ limitations under the License.
 package internalinterfaces
 
 import (
+	time "time"
+
 	versioned "github.com/kubeapps/apprepository-controller/pkg/client/clientset/versioned"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	cache "k8s.io/client-go/tools/cache"
-	time "time"
 )
 
 type NewInformerFunc func(versioned.Interface, time.Duration) cache.SharedIndexInformer
