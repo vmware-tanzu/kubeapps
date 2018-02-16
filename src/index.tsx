825c3e355e6975357b81477aed81d3501ae76af7
Adnan Abdulhussein <adnan@prydoni.us>
2018-02-16 16:17:53 +0000

    disables the service worker (#91)
    Disable the service worker as it currently loads index.html from cache for
    all unknown paths. This doesn't work well with our Ingress setup because if
    you visit /kubeless/ you end up getting the Dashboard instead of the Kubeapps
    UI. We can consider re-enabling this once the Kubeless UI is integrated in
    the Dashboard and we no longer need the external link.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,14 @@ import * as ReactDOM from "react-dom";
 
 import Root from "./containers/Root";
 import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<Root />, document.getElementById("root") as HTMLElement);
-registerServiceWorker();
+
+// Disable the service worker as it currently loads index.html from cache for
+// all unknown paths. This doesn't work well with our Ingress setup because if
+// you visit /kubeless/ you end up getting the Dashboard instead of the Kubeapps
+// UI. We can consider re-enabling this once the Kubeless UI is integrated in
+// the Dashboard and we no longer need the external link.
+
+// registerServiceWorker();
