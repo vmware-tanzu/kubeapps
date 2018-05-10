19800865da241d4502a2e3aa58aedd9111e0bbbd
Adnan Abdulhussein <adnan@bitnami.com>
2018-05-10 17:39:55 +0100

    call out to `gcloud` command to get active account

--- pkg/gke/gke.go
+++ pkg/gke/gke.go
@@ -18,13 +18,9 @@ package gke
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
-	"os/user"
-	"path/filepath"
-	"runtime"
+	"os/exec"
+	"strings"
 
-	"github.com/go-ini/ini"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
@@ -58,58 +54,14 @@ func BuildCrbObject(user string) ([]*unstructured.Unstructured, error) {
 }
 
 // GetActiveUser returns user logging to gcloud
-func GetActiveUser(gcloudPath string) (string, error) {
-	activeConfigPath := filepath.Join(gcloudPath, "active_config")
-	activeConfig, err := ioutil.ReadFile(activeConfigPath)
+func GetActiveUser() (string, error) {
+	stdout, err := exec.Command("gcloud", "config", "get-value", "core/account").Output()
 	if err != nil {
-		return "", fmt.Errorf("can't read file active_config, please ensure your gcloud configuration is at %s or set CLOUDSDK_CONFIG to the correct location", gcloudPath)
+		return "", fmt.Errorf("error executing `gcloud config get-value core/account`, please make sure the Google Cloud SDK is in your system path: %v", err)
 	}
-
-	configPath := filepath.Join(gcloudPath, "configurations")
-	if _, err := os.Stat(filepath.Join(configPath, "config_"+string(activeConfig))); os.IsNotExist(err) {
-		return "", fmt.Errorf("the config file for active_config doesn't exist: %v", err)
+	activeUser := strings.TrimSpace(string(stdout))
+	if activeUser == "" {
+		return "", fmt.Errorf("could not get active user from your Google Cloud SDK configuration, please login using `gcloud auth login`")
 	}
-
-	cfg, err := ini.Load(filepath.Join(configPath, "config_"+string(activeConfig)))
-	if err != nil {
-		return "", fmt.Errorf("can't load file for the active_config: %v", err)
-	}
-
-	core, err := cfg.GetSection("core")
-	if err != nil {
-		return "", fmt.Errorf("can't get section [core]: %v", err)
-	}
-	account, err := core.GetKey("account")
-	if err != nil {
-		return "", fmt.Errorf("can't get key account: %v", err)
-	}
-
-	return account.Value(), nil
-}
-
-// SdkConfigPath returns path to gcloud sdk config
-var SdkConfigPath = func() (string, error) {
-	if path, ok := os.LookupEnv("CLOUDSDK_CONFIG"); ok {
-		return path, nil
-	}
-	if runtime.GOOS == "windows" {
-		return filepath.Join(os.Getenv("APPDATA"), "gcloud"), nil
-	}
-	homeDir := guessUnixHomeDir()
-	if homeDir == "" {
-		return "", fmt.Errorf("unable to get current user home directory: os/user lookup failed; $HOME is empty")
-	}
-	return filepath.Join(homeDir, ".config", "gcloud"), nil
-}
-
-func guessUnixHomeDir() string {
-	// Prefer $HOME over user.Current due to glibc bug: golang.org/issue/13470
-	if v := os.Getenv("HOME"); v != "" {
-		return v
-	}
-	// Else, fall back to user.Current:
-	if u, err := user.Current(); err == nil {
-		return u.HomeDir
-	}
-	return ""
+	return activeUser, nil
 }
