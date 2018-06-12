22d2777688fca89e373e46ae6485067bd898eba1
Andres <andres@bitnami.com>
2018-06-12 12:33:12 +0200

    Proxy logic to communicate with tiller

---
+++ cmd/tiller-proxy/proxy/proxy.go
@@ -0,0 +1,309 @@
+/*
+Copyright (c) 2018 Bitnami
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package proxy
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/helm/pkg/helm"
+	"k8s.io/helm/pkg/proto/hapi/chart"
+	"k8s.io/helm/pkg/proto/hapi/release"
+
+	chartUtils "github.com/kubeapps/kubeapps/cmd/tiller-proxy/chart"
+)
+
+const (
+	defaultNamespace      = metav1.NamespaceSystem
+	defaultRepoURL        = "https://kubernetes-charts.storage.googleapis.com"
+	defaultTimeoutSeconds = 180
+)
+
+var (
+	appMutex map[string]*sync.Mutex
+)
+
+func init() {
+	appMutex = make(map[string]*sync.Mutex)
+}
+
+// Proxy contains all the elements to contact Tiller and the K8s API
+type Proxy struct {
+	kubeClient kubernetes.Interface
+	helmClient helm.Interface
+	netClient  *chartUtils.HTTPClient
+	loadChart  chartUtils.LoadChart
+}
+
+type helmRelease struct {
+	// RepoURL is the URL of the repository. Defaults to stable repo.
+	RepoURL string `json:"repoUrl,omitempty"`
+	// ChartName is the name of the chart within the repo.
+	ChartName string `json:"chartName"`
+	// ReleaseName is the Name of the release given to Tiller.
+	ReleaseName string `json:"releaseName"`
+	// Version is the chart version.
+	Version string `json:"version"`
+	// Auth is the authentication.
+	Auth helmReleaseAuth `json:"auth,omitempty"`
+	// Values is a string containing (unparsed) YAML values.
+	Values string `json:"values,omitempty"`
+}
+
+type helmReleaseAuth struct {
+	// Header is header based Authorization
+	Header *helmReleaseAuthHeader `json:"header,omitempty"`
+}
+
+type helmReleaseAuthHeader struct {
+	// Selects a key of a secret in the pod's namespace
+	SecretKeyRef corev1.SecretKeySelector `json:"secretKeyRef,omitempty"`
+}
+
+func isNotFound(err error) bool {
+	// Ideally this would be `grpc.Code(err) == codes.NotFound`,
+	// but it seems helm doesn't return grpc codes
+	return strings.Contains(grpc.ErrorDesc(err), "not found")
+}
+
+// NewProxy creates a Proxy
+func NewProxy(kubeClient kubernetes.Interface, helmClient helm.Interface, netClient chartUtils.HTTPClient, loadChart chartUtils.LoadChart) *Proxy {
+	return &Proxy{
+		kubeClient: kubeClient,
+		helmClient: helmClient,
+		netClient:  &netClient,
+		loadChart:  loadChart,
+	}
+}
+
+// AppOverview represents the basics of a release
+type AppOverview struct {
+	ReleaseName string `json:"releaseName"`
+	Version     string `json:"version"`
+	Namespace   string `json:"namespace"`
+}
+
+func (p *Proxy) getChart(rel *helmRelease) (*chart.Chart, error) {
+	repoURL := rel.RepoURL
+	if repoURL == "" {
+		// FIXME: Make configurable
+		repoURL = defaultRepoURL
+	}
+	repoURL = strings.TrimSuffix(strings.TrimSpace(repoURL), "/") + "/index.yaml"
+
+	authHeader := ""
+	if rel.Auth.Header != nil {
+		namespace := os.Getenv("POD_NAMESPACE")
+		if namespace == "" {
+			namespace = defaultNamespace
+		}
+
+		secret, err := p.kubeClient.Core().Secrets(namespace).Get(rel.Auth.Header.SecretKeyRef.Name, metav1.GetOptions{})
+		if err != nil {
+			return nil, err
+		}
+		authHeader = string(secret.Data[rel.Auth.Header.SecretKeyRef.Key])
+	}
+
+	log.Printf("Downloading repo %s index...", repoURL)
+	repoIndex, err := chartUtils.FetchRepoIndex(p.netClient, repoURL, authHeader)
+	if err != nil {
+		return nil, err
+	}
+
+	chartURL, err := chartUtils.FindChartInRepoIndex(repoIndex, repoURL, rel.ChartName, rel.Version)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Downloading %s ...", chartURL)
+	chartRequested, err := chartUtils.FetchChart(p.netClient, chartURL, authHeader, p.loadChart)
+	if err != nil {
+		return nil, err
+	}
+	return chartRequested, nil
+}
+
+func (p *Proxy) get(name, namespace string) (*release.Release, error) {
+	list, err := p.helmClient.ListReleases()
+	if err != nil {
+		return nil, fmt.Errorf("Unable to list helm releases: %v", err)
+	}
+	var rel *release.Release
+	for _, r := range list.Releases {
+		if (namespace == "" || namespace == r.Namespace) && r.Name == name {
+			rel = r
+			break
+		}
+	}
+	if rel == nil {
+		return nil, fmt.Errorf("Release %s not found in namespace %s", name, namespace)
+	}
+	return rel, nil
+}
+
+func (p *Proxy) deploy(name, namespace, values string, ch *chart.Chart) (*release.Release, error) {
+	log.Printf("Installing release %s into namespace %s", name, namespace)
+	res, err := p.helmClient.InstallReleaseFromChart(
+		ch,
+		namespace,
+		helm.ValueOverrides([]byte(values)),
+		helm.ReleaseName(name),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return res.GetRelease(), nil
+}
+
+func (p *Proxy) update(name, namespace, values string, ch *chart.Chart) (*release.Release, error) {
+	log.Printf("Updating release %s", name)
+	res, err := p.helmClient.UpdateReleaseFromChart(
+		name,
+		ch,
+		helm.UpdateValueOverrides([]byte(values)),
+		//helm.UpgradeForce(true), ?
+	)
+	if err != nil {
+		return nil, err
+	}
+	return res.GetRelease(), nil
+}
+
+// LogReleaseStatus prints the status of the given release if exists
+func (p *Proxy) LogReleaseStatus(relName string) {
+	status, err := p.helmClient.ReleaseStatus(relName)
+	if err == nil {
+		if status.Info != nil && status.Info.Status != nil {
+			log.Printf("Release status: %s", status.Info.Status.Code)
+		}
+	} else {
+		log.Printf("Unable to fetch release status for %s: %v", relName, err)
+	}
+}
+
+// ListReleases list releases in a specific namespace if given
+func (p *Proxy) ListReleases(namespace string) ([]AppOverview, error) {
+	list, err := p.helmClient.ListReleases()
+	if err != nil {
+		return []AppOverview{}, fmt.Errorf("Unable to list helm releases: %v", err)
+	}
+	appList := []AppOverview{}
+	for _, r := range list.Releases {
+		if namespace == "" || namespace == r.Namespace {
+			appList = append(appList, AppOverview{r.Name, r.Chart.Metadata.Version, r.Namespace})
+		}
+	}
+	return appList, nil
+}
+
+func lock(name string) {
+	if appMutex[name] == nil {
+		appMutex[name] = &sync.Mutex{}
+	}
+	appMutex[name].Lock()
+}
+
+func unlock(name string) {
+	appMutex[name].Unlock()
+}
+
+// CreateRelease creates a tiller release
+func (p *Proxy) CreateRelease(namespace string, rawRelease []byte) (*release.Release, error) {
+	hrelease := &helmRelease{}
+	err := json.Unmarshal(rawRelease, hrelease)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse request body: %v", err)
+	}
+	lock(hrelease.ReleaseName)
+	defer unlock(hrelease.ReleaseName)
+	ch, err := p.getChart(hrelease)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to retrieve chart info: %v", err)
+	}
+	if err != nil {
+		return nil, err
+	}
+	rel, err := p.deploy(hrelease.ReleaseName, namespace, hrelease.Values, ch)
+	if err != nil {
+		return nil, fmt.Errorf("Unable create release: %v", err)
+	}
+	return rel, nil
+}
+
+// UpdateRelease upgrades a tiller release
+func (p *Proxy) UpdateRelease(name, namespace string, rawRelease []byte) (*release.Release, error) {
+	lock(name)
+	defer unlock(name)
+	var rel *release.Release
+	// Check if the release already exists
+	_, err := p.get(name, namespace)
+	if err != nil && isNotFound(err) {
+		return nil, fmt.Errorf("Release %s not found in the namespace %s. Unable to update it", name, namespace)
+	} else if err != nil {
+		return nil, err
+	} else {
+		hrelease := &helmRelease{}
+		err := json.Unmarshal(rawRelease, hrelease)
+		if err != nil {
+			return nil, fmt.Errorf("Unable to parse request body: %v", err)
+		}
+		ch, err := p.getChart(hrelease)
+		if err != nil {
+			return nil, fmt.Errorf("Unable to retrieve chart info: %v", err)
+		}
+		if err != nil {
+			return nil, err
+		}
+		rel, err = p.update(hrelease.ReleaseName, namespace, hrelease.Values, ch)
+		if err != nil {
+			return nil, fmt.Errorf("Unable to update release: %v", err)
+		}
+	}
+	return rel, nil
+}
+
+// GetRelease returns the info of a release
+func (p *Proxy) GetRelease(name, namespace string) (*release.Release, error) {
+	lock(name)
+	defer unlock(name)
+	return p.get(name, namespace)
+}
+
+// DeleteRelease deletes a release
+func (p *Proxy) DeleteRelease(name, namespace string) error {
+	lock(name)
+	defer unlock(name)
+	// Validate that the release actually belongs to the namespace
+	_, err := p.get(name, namespace)
+	if err != nil {
+		return err
+	}
+	_, err = p.helmClient.DeleteRelease(name, helm.DeletePurge(true))
+	if err != nil {
+		return fmt.Errorf("Unable to delete release: %v", err)
+	}
+	return nil
+}
---
+++ cmd/tiller-proxy/proxy/proxy_test.go
@@ -0,0 +1,364 @@
+/*
+Copyright (c) 2018 Bitnami
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package proxy
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/helm/pkg/helm"
+	"k8s.io/helm/pkg/proto/hapi/chart"
+	"k8s.io/helm/pkg/proto/hapi/release"
+	"k8s.io/helm/pkg/repo"
+)
+
+// Fake server for repositories and charts
+type fakeHTTPClient struct {
+	repoURLs  []string
+	chartURLs []string
+	index     *repo.IndexFile
+}
+
+func (f *fakeHTTPClient) Do(h *http.Request) (*http.Response, error) {
+	for _, repoURL := range f.repoURLs {
+		if h.URL.String() == fmt.Sprintf("%sindex.yaml", repoURL) {
+			// Return fake chart index (not customizable per repo)
+			body, err := json.Marshal(*f.index)
+			if err != nil {
+				fmt.Printf("Error! %v", err)
+			}
+			return &http.Response{StatusCode: 200, Body: ioutil.NopCloser(bytes.NewReader(body))}, nil
+		}
+	}
+	for _, chartURL := range f.chartURLs {
+		if h.URL.String() == chartURL {
+			// Simulate download time
+			time.Sleep(100 * time.Millisecond)
+			// Fake chart response
+			return &http.Response{StatusCode: 200, Body: ioutil.NopCloser(bytes.NewReader([]byte{}))}, nil
+		}
+	}
+	// Unexpected path
+	return &http.Response{StatusCode: 404}, fmt.Errorf("Unexpected path")
+}
+
+func fakeLoadChart(in io.Reader) (*chart.Chart, error) {
+	// Fake I/O time
+	time.Sleep(100 * time.Millisecond)
+	return &chart.Chart{}, nil
+}
+
+func prepareTestProxy(hrs []helmRelease, existingTillerReleases []AppOverview) *Proxy {
+	var repoURLs []string
+	var chartURLs []string
+	entries := map[string]repo.ChartVersions{}
+	// Populate Chart registry with content of the given helmReleases
+	for _, hr := range hrs {
+		repoURLs = append(repoURLs, hr.RepoURL)
+		chartMeta := chart.Metadata{Name: hr.ChartName, Version: hr.Version}
+		chartURL := fmt.Sprintf("%s%s-%s.tgz", hr.RepoURL, hr.ChartName, hr.Version)
+		chartURLs = append(chartURLs, chartURL)
+		chartVersion := repo.ChartVersion{Metadata: &chartMeta, URLs: []string{chartURL}}
+		chartVersions := []*repo.ChartVersion{&chartVersion}
+		entries[hr.ChartName] = chartVersions
+	}
+	index := &repo.IndexFile{APIVersion: "v1", Generated: time.Now(), Entries: entries}
+	netClient := fakeHTTPClient{repoURLs, chartURLs, index}
+	helmClient := helm.FakeClient{}
+	// Populate Fake helm client with releases
+	for _, r := range existingTillerReleases {
+		helmClient.Rels = append(helmClient.Rels, &release.Release{
+			Name:      r.ReleaseName,
+			Namespace: r.Namespace,
+			Chart: &chart.Chart{
+				Metadata: &chart.Metadata{
+					Version: r.Version,
+				},
+			},
+		})
+	}
+	kubeClient := fake.NewSimpleClientset()
+	return NewProxy(kubeClient, &helmClient, &netClient, fakeLoadChart)
+}
+
+func TestListAllReleases(t *testing.T) {
+	app1 := AppOverview{"foo", "1.0.0", "my_ns"}
+	app2 := AppOverview{"bar", "1.0.0", "other_ns"}
+	proxy := prepareTestProxy([]helmRelease{}, []AppOverview{app1, app2})
+
+	// Should return all the releases if no namespace is given
+	releases, err := proxy.ListReleases("")
+	if err != nil {
+		t.Fatalf("Unexpected error %v", err)
+	}
+	if len(releases) != 2 {
+		t.Errorf("It should return both releases")
+	}
+	if !reflect.DeepEqual([]AppOverview{app1, app2}, releases) {
+		t.Errorf("Unexpected list of releases %v", releases)
+	}
+}
+
+func TestListNamespacedRelease(t *testing.T) {
+	app1 := AppOverview{"foo", "1.0.0", "my_ns"}
+	app2 := AppOverview{"bar", "1.0.0", "other_ns"}
+	proxy := prepareTestProxy([]helmRelease{}, []AppOverview{app1, app2})
+
+	// Should return all the releases if no namespace is given
+	releases, err := proxy.ListReleases(app1.Namespace)
+	if err != nil {
+		t.Fatalf("Unexpected error %v", err)
+	}
+	if len(releases) != 1 {
+		t.Errorf("It should return both releases")
+	}
+	if !reflect.DeepEqual([]AppOverview{app1}, releases) {
+		t.Errorf("Unexpected list of releases %v", releases)
+	}
+}
+
+func TestCreateHelmRelease(t *testing.T) {
+	ns := "myns"
+	h := helmRelease{
+		ReleaseName: "not-foo",
+		RepoURL:     "http://charts.example.com/repo/",
+		ChartName:   "foo",
+		Version:     "v1.0.0",
+	}
+	rawRelease, _ := json.Marshal(h)
+	proxy := prepareTestProxy([]helmRelease{h}, []AppOverview{})
+
+	result, err := proxy.CreateRelease(ns, rawRelease)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	if result.Name != h.ReleaseName {
+		t.Errorf("Expected release named %s received %s", h.ReleaseName, result.Name)
+	}
+	if result.Namespace != ns {
+		t.Errorf("Expected release in namespace %s received %s", ns, result.Namespace)
+	}
+	// We cannot check that the rest of the chart properties as properly set
+	// because the fake InstallReleaseFromChart ignores the given chart
+}
+
+func TestCreateConflictingHelmRelease(t *testing.T) {
+	ns1 := "myns"
+	h := helmRelease{
+		ReleaseName: "not-foo",
+		RepoURL:     "http://charts.example.com/repo/",
+		ChartName:   "foo",
+		Version:     "v1.0.0",
+	}
+	ns2 := "other_ns"
+	app := AppOverview{h.ReleaseName, h.Version, ns2}
+	rawRelease, _ := json.Marshal(h)
+	proxy := prepareTestProxy([]helmRelease{h}, []AppOverview{app})
+
+	_, err := proxy.CreateRelease(ns1, rawRelease)
+	if err == nil {
+		t.Error("Release should fail, an existing release in a different namespace already exists")
+	}
+	if !strings.Contains(err.Error(), "name that is still in use") {
+		t.Errorf("Unexpected error %v", err)
+	}
+}
+
+func TestHelmReleaseUpdated(t *testing.T) {
+	ns := "myns"
+	h := helmRelease{
+		ReleaseName: "not-foo",
+		RepoURL:     "http://charts.example.com/repo/",
+		ChartName:   "foo",
+		Version:     "v1.0.0",
+	}
+	rawRelease, _ := json.Marshal(h)
+	app := AppOverview{h.ReleaseName, h.Version, ns}
+	proxy := prepareTestProxy([]helmRelease{h}, []AppOverview{app})
+
+	result, err := proxy.UpdateRelease(h.ReleaseName, ns, rawRelease)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	if result.Name != h.ReleaseName {
+		t.Errorf("Expected release named %s received %s", h.ReleaseName, result.Name)
+	}
+	if result.Namespace != ns {
+		t.Errorf("Expected release in namespace %s received %s", ns, result.Namespace)
+	}
+	rels, err := proxy.helmClient.ListReleases()
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	// We cannot test that the release content changes because fake UpdateReleaseResponse
+	// does not modify the release
+	if len(rels.Releases) != 1 {
+		t.Errorf("Unexpected amount of releases %d, it should update the existing one", len(rels.Releases))
+	}
+}
+
+func TestUpdateMissingHelmRelease(t *testing.T) {
+	ns := "myns"
+	h := helmRelease{
+		ReleaseName: "not-foo",
+		RepoURL:     "http://charts.example.com/repo/",
+		ChartName:   "foo",
+		Version:     "v1.0.0",
+	}
+	rawRelease, _ := json.Marshal(h)
+	// Simulate the same app but in a different namespace
+	ns2 := "other_ns"
+	app := AppOverview{h.ReleaseName, h.Version, ns2}
+	proxy := prepareTestProxy([]helmRelease{h}, []AppOverview{app})
+
+	_, err := proxy.UpdateRelease(h.ReleaseName, ns, rawRelease)
+	if err == nil {
+		t.Error("Update should fail, there is not a release in the namespace specified")
+	}
+	if !strings.Contains(err.Error(), "not found") {
+		t.Errorf("Unexpected error %v", err)
+	}
+}
+
+func TestGetHelmRelease(t *testing.T) {
+	app1 := AppOverview{"foo", "1.0.0", "my_ns"}
+	app2 := AppOverview{"bar", "1.0.0", "other_ns"}
+	type testStruct struct {
+		existingApps    []AppOverview
+		shouldFail      bool
+		targetApp       string
+		tartegNamespace string
+		expectedResult  string
+	}
+	tests := []testStruct{
+		{[]AppOverview{app1, app2}, false, "foo", "my_ns", "foo"},
+		{[]AppOverview{app1, app2}, true, "bar", "my_ns", ""},
+		{[]AppOverview{app1, app2}, true, "foobar", "my_ns", ""},
+		{[]AppOverview{app1, app2}, false, "foo", "", "foo"},
+	}
+	for _, test := range tests {
+		proxy := prepareTestProxy([]helmRelease{}, test.existingApps)
+		res, err := proxy.GetRelease(test.targetApp, test.tartegNamespace)
+		if test.shouldFail && err == nil {
+			t.Errorf("Get %s/%s should fail", test.tartegNamespace, test.targetApp)
+		}
+		if !test.shouldFail {
+			if err != nil {
+				t.Errorf("Unexpected error %v", err)
+			}
+			if res.Name != test.expectedResult {
+				t.Errorf("Expecting app %s, received %s", test.expectedResult, res.Name)
+			}
+		}
+	}
+}
+
+func TestHelmReleaseDeleted(t *testing.T) {
+	app := AppOverview{"foo", "1.0.0", "my_ns"}
+	proxy := prepareTestProxy([]helmRelease{}, []AppOverview{app})
+
+	err := proxy.DeleteRelease(app.ReleaseName, app.Namespace)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	rels, err := proxy.helmClient.ListReleases()
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	if len(rels.Releases) != 0 {
+		t.Errorf("Unexpected amount of releases %d, it should be empty", len(rels.Releases))
+	}
+}
+
+func TestDeleteMissingHelmRelease(t *testing.T) {
+	app := AppOverview{"foo", "1.0.0", "my_ns"}
+	proxy := prepareTestProxy([]helmRelease{}, []AppOverview{app})
+
+	err := proxy.DeleteRelease(app.ReleaseName, "other_ns")
+	if err == nil {
+		t.Error("Delete should fail, there is not a release in the namespace specified")
+	}
+	rels, err := proxy.helmClient.ListReleases()
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	if len(rels.Releases) != 1 {
+		t.Errorf("Unexpected amount of releases %d, it contain a release", len(rels.Releases))
+	}
+}
+
+func TestEnsureThreadSafety(t *testing.T) {
+	ns := "myns"
+	h := helmRelease{
+		ReleaseName: "not-foo",
+		RepoURL:     "http://charts.example.com/repo/",
+		ChartName:   "foo",
+		Version:     "v1.0.0",
+	}
+	rawRelease, _ := json.Marshal(h)
+	proxy := prepareTestProxy([]helmRelease{h}, []AppOverview{})
+	finish := make(chan struct{})
+	type test func()
+	phases := []test{
+		func() {
+			// Create first element
+			result, err := proxy.CreateRelease(ns, rawRelease)
+			if err != nil {
+				t.Errorf("Unexpected error %v", err)
+			}
+			if result.Name != h.ReleaseName {
+				t.Errorf("Expected release named %s received %s", h.ReleaseName, result.Name)
+			}
+		},
+		func() {
+			// Try to create it again
+			_, err := proxy.CreateRelease(ns, rawRelease)
+			if err == nil {
+				t.Errorf("Should fail with 'already exists'")
+			}
+		},
+		func() {
+			_, err := proxy.UpdateRelease(h.ReleaseName, ns, rawRelease)
+			if err != nil {
+				t.Errorf("Unexpected error %v", err)
+			}
+		},
+		func() {
+			err := proxy.DeleteRelease(h.ReleaseName, ns)
+			if err != nil {
+				t.Errorf("Unexpected error %v", err)
+			}
+			finish <- struct{}{}
+		},
+	}
+	for _, phase := range phases {
+		// Run all phases in parallel
+		go phase()
+		// Give minimum time for phase to block
+		time.Sleep(1 * time.Millisecond)
+	}
+	<-finish
+}
