b13c5912688df3bdd94d8a8933958c13501f9a27
Miguel Ángel Martínez Triviño <migmartri@gmail.com>
2018-08-07 10:05:51 -0700

    Custom namespace support for local development (#440)
    Namespace customization for dashboard local dev environment

--- dashboard/src/shared/Config.ts
+++ dashboard/src/shared/Config.ts
@@ -9,6 +9,15 @@ export default class Config {
   public static async getConfig() {
     const url = Config.APIEndpoint;
     const { data } = await axios.get<IConfig>(url);
+
+    // Development environment config overrides
+    // TODO(miguel) Rename env variable to KUBEAPPS_NAMESPACE once/if we eject create-react-app
+    // Currently we are using REACT_APP_* because it's the only way to inject env variables in a sealed setup.
+    // Please note that this env variable gets mapped in the run command in the package.json file
+    if (process.env.NODE_ENV !== "production" && process.env.REACT_APP_KUBEAPPS_NS) {
+      data.namespace = process.env.REACT_APP_KUBEAPPS_NS;
+    }
+
     return data;
   }
 
