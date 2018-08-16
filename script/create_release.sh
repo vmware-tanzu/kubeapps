8421f8e1f07bde64367b637142395e660df9f086
Andres <andres.mgotor@gmail.com>
2018-08-16 10:58:44 +0200

    Add end-to-end tests using CircleCI (#443)
    * Circle-ci file
    
    Add release scripts
    
    * Minor review
    
    * GKE e2e tests
    
    * Apply review
    
    * Minor review
    
    * Remove GKE integration
    
    * Remove kubeapps cli generation
    
    * Add GKE e2e tests to CircleCI (#473)
    
    * Re-enable GKE integration
    
    * Parametrize params

---
+++ script/create_release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+TAG=${1:?}
+
+PROJECT_DIR=$(cd $(dirname $0)/.. && pwd)
+
+source $(dirname $0)/release_utils.sh
+
+if [[ -z "$REPO_NAME" || -z "$REPO_DOMAIN" ]]; then
+  echo "Github repository not specified" > /dev/stderr
+  exit 1
+fi
+
+if [[ -z "$ACCESS_TOKEN" ]]; then
+  echo "Unable to release: Github Token not specified" > /dev/stderr
+  exit 1
+fi
+
+repo_check=`curl -H "Authorization: token $ACCESS_TOKEN" -s https://api.github.com/repos/$REPO_DOMAIN/$REPO_NAME`
+if [[ $repo_check == *"Not Found"* ]]; then
+  echo "Not found a Github repository for $REPO_DOMAIN/$REPO_NAME, it is not possible to publish it" > /dev/stderr
+  exit 1
+else
+  RELEASE_ID=$(release_tag $TAG $REPO_DOMAIN $REPO_NAME | jq '.id') 
+fi
+
+echo "RELEASE ID: $RELEASE_ID"
