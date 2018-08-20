dad8d39423127772ad9b914ed80ba902ab930aa4
Miguel Martinez <migmartri@gmail.com>
2018-08-20 15:56:36 -0700

    use mock-socket

--- dashboard/src/setupTests.ts
+++ dashboard/src/setupTests.ts
@@ -3,5 +3,16 @@ import "raf/polyfill"; // polyfill for requestAnimationFrame
 
 import { configure } from "enzyme";
 import * as Adapter from "enzyme-adapter-react-16";
+import { WebSocket } from "mock-socket";
 
 configure({ adapter: new Adapter() });
+
+// Mock browser specific APIs like localstorage or Websocket
+const localStorageMock = {
+  clear: jest.fn(),
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+};
+
+(global as any).localStorage = localStorageMock;
+(global as any).WebSocket = WebSocket;
