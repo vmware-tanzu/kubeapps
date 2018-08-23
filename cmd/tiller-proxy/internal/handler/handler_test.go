316138a3294e2e907898e50d36fa346f84198d65
Andres <andres@bitnami.com>
2018-08-23 11:23:28 +0200

    Review

--- cmd/tiller-proxy/internal/handler/handler_test.go
+++ cmd/tiller-proxy/internal/handler/handler_test.go
@@ -45,34 +45,14 @@ func TestErrorCodeWithDefault(t *testing.T) {
 		{fmt.Errorf("release foo not found"), http.StatusInternalServerError, http.StatusNotFound},
 		{fmt.Errorf("Unauthorized to get release foo"), http.StatusInternalServerError, http.StatusForbidden},
 		{fmt.Errorf("release \"Foo \" failed"), http.StatusInternalServerError, http.StatusUnprocessableEntity},
-		{fmt.Errorf("This is a unexpected error"), http.StatusInternalServerError, http.StatusInternalServerError},
-		{fmt.Errorf("This is a unexpected error"), http.StatusUnprocessableEntity, http.StatusUnprocessableEntity},
+		{fmt.Errorf("This is an unexpected error"), http.StatusInternalServerError, http.StatusInternalServerError},
+		{fmt.Errorf("This is an unexpected error"), http.StatusUnprocessableEntity, http.StatusUnprocessableEntity},
 	}
 	for _, s := range tests {
 		code := errorCodeWithDefault(s.err, s.defaultCode)
 		if code != s.expectedCode {
 			t.Errorf("Expected '%v' to return code %v got %v", s.err, s.expectedCode, code)
 		}
-	}
-}
-
-type fakeResponseWriter struct {
-	header http.Header
-	Body   string
-}
-
-func (fw *fakeResponseWriter) Header() http.Header {
-	return fw.header
-}
-
-func (fw *fakeResponseWriter) Write(s []byte) (int, error) {
-	fw.Body = fmt.Sprintf("%s%s", fw.Body, string(s))
-	return len(s), nil
-}
-
-func (fw *fakeResponseWriter) WriteHeader(statusCode int) {
-	fw.header = http.Header{
-		"Status-Code": []string{fmt.Sprintf("%v", statusCode)},
 	}
 }
 
@@ -88,12 +68,12 @@ func TestActions(t *testing.T) {
 		Action      string
 		Params      map[string]string
 		// Expected result
-		StatusCode        string
+		StatusCode        int
 		RemainingReleases []release.Release
 		ResponseBody      string // Optional
 	}
 	tests := []testScenario{
-		testScenario{
+		{
 			// Scenario params
 			Description:      "Create a simple release without auth",
 			ExistingReleases: []release.Release{},
@@ -104,13 +84,13 @@ func TestActions(t *testing.T) {
 			Action: "create",
 			Params: map[string]string{"namespace": "default"},
 			// Expected result
-			StatusCode: "200",
+			StatusCode: 200,
 			RemainingReleases: []release.Release{
 				release.Release{Name: "foobar", Namespace: "default"},
 			},
 			ResponseBody: "",
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description:      "Create a simple release with auth",
 			ExistingReleases: []release.Release{},
@@ -121,13 +101,13 @@ func TestActions(t *testing.T) {
 			Action: "create",
 			Params: map[string]string{"namespace": "default"},
 			// Expected result
-			StatusCode: "200",
+			StatusCode: 200,
 			RemainingReleases: []release.Release{
 				release.Release{Name: "foobar", Namespace: "default"},
 			},
 			ResponseBody: "",
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description:      "Create a conflicting release",
 			ExistingReleases: []release.Release{release.Release{Name: "foobar", Namespace: "default"}},
@@ -138,13 +118,13 @@ func TestActions(t *testing.T) {
 			Action: "create",
 			Params: map[string]string{"namespace": "default"},
 			// Expected result
-			StatusCode: "409",
+			StatusCode: 409,
 			RemainingReleases: []release.Release{
 				release.Release{Name: "foobar", Namespace: "default"},
 			},
 			ResponseBody: "",
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description:      "Create a simple release with forbidden actions",
 			ExistingReleases: []release.Release{},
@@ -157,11 +137,11 @@ func TestActions(t *testing.T) {
 			Action: "create",
 			Params: map[string]string{"namespace": "default"},
 			// Expected result
-			StatusCode:        "403",
+			StatusCode:        403,
 			RemainingReleases: []release.Release{},
 			ResponseBody:      `{"code":403,"message":"[{\"apiGroup\":\"v1\",\"resource\":\"pods\",\"namespace\":\"default\",\"clusterWide\":false,\"verbs\":[\"create\"]}]"}`,
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description:      "Upgrade a simple release",
 			ExistingReleases: []release.Release{release.Release{Name: "foobar", Namespace: "default"}},
@@ -172,11 +152,11 @@ func TestActions(t *testing.T) {
 			Action: "upgrade",
 			Params: map[string]string{"namespace": "default", "releaseName": "foobar"},
 			// Expected result
-			StatusCode:        "200",
+			StatusCode:        200,
 			RemainingReleases: []release.Release{release.Release{Name: "foobar", Namespace: "default"}},
 			ResponseBody:      "",
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description:      "Upgrade a missing release",
 			ExistingReleases: []release.Release{},
@@ -187,11 +167,11 @@ func TestActions(t *testing.T) {
 			Action: "upgrade",
 			Params: map[string]string{"namespace": "default", "releaseName": "foobar"},
 			// Expected result
-			StatusCode:        "404",
+			StatusCode:        404,
 			RemainingReleases: []release.Release{},
 			ResponseBody:      "",
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description:      "Upgrade a simple release with forbidden actions",
 			ExistingReleases: []release.Release{release.Release{Name: "foobar", Namespace: "default"}},
@@ -204,11 +184,11 @@ func TestActions(t *testing.T) {
 			Action: "upgrade",
 			Params: map[string]string{"namespace": "default"},
 			// Expected result
-			StatusCode:        "403",
+			StatusCode:        403,
 			RemainingReleases: []release.Release{release.Release{Name: "foobar", Namespace: "default"}},
 			ResponseBody:      `{"code":403,"message":"[{\"apiGroup\":\"v1\",\"resource\":\"pods\",\"namespace\":\"default\",\"clusterWide\":false,\"verbs\":[\"upgrade\"]}]"}`,
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description:      "Delete a simple release",
 			ExistingReleases: []release.Release{release.Release{Name: "foobar", Namespace: "default"}},
@@ -219,11 +199,11 @@ func TestActions(t *testing.T) {
 			Action:      "delete",
 			Params:      map[string]string{"namespace": "default", "releaseName": "foobar"},
 			// Expected result
-			StatusCode:        "200",
+			StatusCode:        200,
 			RemainingReleases: []release.Release{},
 			ResponseBody:      "",
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description:      "Delete a missing release",
 			ExistingReleases: []release.Release{},
@@ -234,11 +214,11 @@ func TestActions(t *testing.T) {
 			Action: "delete",
 			Params: map[string]string{"namespace": "default", "releaseName": "foobar"},
 			// Expected result
-			StatusCode:        "404",
+			StatusCode:        404,
 			RemainingReleases: []release.Release{},
 			ResponseBody:      "",
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description:      "Delete a release with forbidden actions",
 			ExistingReleases: []release.Release{release.Release{Name: "foobar", Namespace: "default", Config: &chart.Config{Raw: ""}}},
@@ -251,11 +231,11 @@ func TestActions(t *testing.T) {
 			Action: "delete",
 			Params: map[string]string{"namespace": "default", "releaseName": "foobar"},
 			// Expected result
-			StatusCode:        "403",
+			StatusCode:        403,
 			RemainingReleases: []release.Release{release.Release{Name: "foobar", Namespace: "default", Config: &chart.Config{Raw: ""}}},
 			ResponseBody:      `{"code":403,"message":"[{\"apiGroup\":\"v1\",\"resource\":\"pods\",\"namespace\":\"default\",\"clusterWide\":false,\"verbs\":[\"delete\"]}]"}`,
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description:      "Get a simple release",
 			ExistingReleases: []release.Release{release.Release{Name: "foobar", Namespace: "default"}},
@@ -266,11 +246,11 @@ func TestActions(t *testing.T) {
 			Action:      "get",
 			Params:      map[string]string{"namespace": "default", "releaseName": "foobar"},
 			// Expected result
-			StatusCode:        "200",
+			StatusCode:        200,
 			RemainingReleases: []release.Release{release.Release{Name: "foobar", Namespace: "default"}},
 			ResponseBody:      `{"data":{"name":"foobar","namespace":"default"}}`,
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description:      "Get a missing release",
 			ExistingReleases: []release.Release{},
@@ -281,11 +261,11 @@ func TestActions(t *testing.T) {
 			Action:      "get",
 			Params:      map[string]string{"namespace": "default", "releaseName": "foobar"},
 			// Expected result
-			StatusCode:        "404",
+			StatusCode:        404,
 			RemainingReleases: []release.Release{},
 			ResponseBody:      "",
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description:      "Get a release with forbidden actions",
 			ExistingReleases: []release.Release{release.Release{Name: "foobar", Namespace: "default", Config: &chart.Config{Raw: ""}}},
@@ -298,11 +278,11 @@ func TestActions(t *testing.T) {
 			Action:      "get",
 			Params:      map[string]string{"namespace": "default", "releaseName": "foobar"},
 			// Expected result
-			StatusCode:        "403",
+			StatusCode:        403,
 			RemainingReleases: []release.Release{release.Release{Name: "foobar", Namespace: "default", Config: &chart.Config{Raw: ""}}},
 			ResponseBody:      `{"code":403,"message":"[{\"apiGroup\":\"v1\",\"resource\":\"pods\",\"namespace\":\"default\",\"clusterWide\":false,\"verbs\":[\"get\"]}]"}`,
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description: "List all releases",
 			ExistingReleases: []release.Release{
@@ -316,14 +296,14 @@ func TestActions(t *testing.T) {
 			Action:      "listall",
 			Params:      map[string]string{},
 			// Expected result
-			StatusCode: "200",
+			StatusCode: 200,
 			RemainingReleases: []release.Release{
 				release.Release{Name: "foobar", Namespace: "default"},
 				release.Release{Name: "foo", Namespace: "not-default"},
 			},
 			ResponseBody: `{"data":[{"releaseName":"foobar","version":"","namespace":"default","status":"DEPLOYED"},{"releaseName":"foo","version":"","namespace":"not-default","status":"DEPLOYED"}]}`,
 		},
-		testScenario{
+		{
 			// Scenario params
 			Description: "List releases in a namespace",
 			ExistingReleases: []release.Release{
@@ -337,7 +317,7 @@ func TestActions(t *testing.T) {
 			Action:      "list",
 			Params:      map[string]string{"namespace": "default"},
 			// Expected result
-			StatusCode: "200",
+			StatusCode: 200,
 			RemainingReleases: []release.Release{
 				release.Release{Name: "foobar", Namespace: "default"},
 				release.Release{Name: "foo", Namespace: "not-default"},
@@ -364,10 +344,7 @@ func TestActions(t *testing.T) {
 			ctx := context.WithValue(req.Context(), userKey, fauth)
 			req = req.WithContext(ctx)
 		}
-		response := &fakeResponseWriter{
-			header: http.Header{},
-			Body:   "",
-		}
+		response := httptest.NewRecorder()
 		// Perform request
 		t.Log(test.Description)
 		switch test.Action {
@@ -387,14 +364,14 @@ func TestActions(t *testing.T) {
 			t.Errorf("Unexpected action %s", test.Action)
 		}
 		// Check result
-		if response.Header().Get("Status-Code") != test.StatusCode {
-			t.Errorf("Expecting a StatusCode %s, received %s", test.StatusCode, response.Header().Get("Status-Code"))
+		if response.Code != test.StatusCode {
+			t.Errorf("Expecting a StatusCode %d, received %d", test.StatusCode, response.Code)
 		}
 		if !reflect.DeepEqual(proxy.Releases, test.RemainingReleases) {
 			t.Errorf("Unexpected remaining releases. Expecting %v, found %v", test.RemainingReleases, proxy.Releases)
 		}
 		if test.ResponseBody != "" {
-			if test.ResponseBody != response.Body {
+			if test.ResponseBody != response.Body.String() {
 				t.Errorf("Unexpected body response. Expecting %s, found %s", test.ResponseBody, response.Body)
 			}
 		}
