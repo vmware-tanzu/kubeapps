94ed02a180eaeaafbe37d3a2a1b3604cdb6036e7
Andres <andres@bitnami.com>
2018-09-04 15:22:38 +0200

    Push and use temporary images

--- script/e2e-test.sh
+++ script/e2e-test.sh
@@ -17,6 +17,7 @@
 set -e
 
 ROOT_DIR=`cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd`
+DEV_TAG=${1:?}
 
 source $ROOT_DIR/script/libtest.sh
 
@@ -29,9 +30,19 @@ k8s_wait_for_pod_ready kube-system app=helm,name=tiller
 wait_for_tiller
 
 # Install Kubeapps
-# TODO: Use just built images
 helm dep up $ROOT_DIR/chart/kubeapps/
-helm install --name kubeapps-ci --namespace kubeapps $ROOT_DIR/chart/kubeapps
+helm install --name kubeapps-ci --namespace kubeapps $ROOT_DIR/chart/kubeapps \
+  --set apprepository.image.tag=$DEV_TAG \
+  --set apprepository.syncImage.tag=$DEV_TAG \
+  --set chartsvc.image.tag=$DEV_TAG \
+  --set dashboard.image.tag=$DEV_TAG \
+  --set tillerProxy.image.tag=$DEV_TAG
+
+# Ensure that we are testing the correct image
+k8s_ensure_image kubeapps kubeapps-ci-apprepository-controller $DEV_TAG
+k8s_ensure_image kubeapps kubeapps-ci-chartsvc $DEV_TAG
+k8s_ensure_image kubeapps kubeapps-ci-dashboard $DEV_TAG
+k8s_ensure_image kubeapps kubeapps-ci-tiller-proxy $DEV_TAG
 
 # Wait for Kubeapps Pods
 k8s_wait_for_pod_ready kubeapps app=kubeapps-ci
--- script/libtest.sh
+++ script/libtest.sh
@@ -45,11 +45,20 @@ k8s_wait_for_pod_completed() {
     k8s_wait_for_pod $namespace $labelSelector Completed
 }
 
+k8s_ensure_image() {
+    namespace=${1:?}
+    deployment=${2:?}
+    expectedPattern=${3:?}
+    jsonpath=${4:-'{.spec.template.spec.containers[0].image}'}
+    echo "Checking image for $deployment"
+    kubectl get deployment -n $namespace $deployment -o jsonpath="$jsonpath" | grep $expectedPattern
+}
+
 ## helm specific helper functions
 wait_for_tiller() {
     echo "Waiting for Tiller to be ready ... "
     local -i cnt=${TEST_MAX_WAIT_SEC:?}
-    until helm version "${@}"; do
+    until helm version --tiller-connection-timeout 5 "${@}"; do
         ((cnt=cnt-1)) || return 1
         sleep 1
     done
