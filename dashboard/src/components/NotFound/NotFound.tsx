792a928b49ea0a188847fd8aaa87f4e6732b1fa5
Andres <andres@bitnami.com>
2018-09-14 11:42:42 +0200

    Move 404 image

--- dashboard/src/components/NotFound/NotFound.tsx
+++ dashboard/src/components/NotFound/NotFound.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import logo404 from "./404.svg";
+import logo404 from "../../img/404.svg";
 
 class NotFound extends React.Component {
   public render() {
     return (
       <div className="text-c align-center margin-t-huge">
-        <h3> The page you are looking for can't be found. </h3>
+        <h3>The page you are looking for can't be found.</h3>
         <img src={logo404} alt="Kubeapps logo" />
       </div>
     );
