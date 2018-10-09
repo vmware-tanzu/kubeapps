74f3fa668958e18168f9773563ad1fc6d58e91d3
Andres <andres.mgotor@gmail.com>
2018-10-09 12:44:31 +0200

    Add "id" to headings (#709)
    * Parses heading links
    
    * Move code to a different file

---
+++ dashboard/src/components/ChartView/HeadingRenderer.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+// Code from https://github.com/rexxars/react-markdown/issues/69
+function flatten(text: string, child: any): any {
+  return typeof child === "string"
+    ? text + child
+    : React.Children.toArray(child.props.children).reduce(flatten, text);
+}
+
+const HeadingRenderer: React.SFC<{}> = (props: any) => {
+  const children = React.Children.toArray(props.children);
+  const text = children.reduce(flatten, "");
+  const slug = text
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9_\s]/g, "") // remove punctuation
+    .replace(/\s/g, "-"); // replace spaces with dash
+  return React.createElement("h" + props.level, { id: slug }, props.children);
+};
+
+export default HeadingRenderer;
