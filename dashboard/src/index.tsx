59c305a27e38b79e464d6b294e37da08c9de92f3
Andres <andres@bitnami.com>
2018-10-10 11:38:12 +0200

    Add the comment for enabling the service worker again

--- dashboard/src/index.tsx
+++ dashboard/src/index.tsx
@@ -7,10 +7,14 @@ import { axios } from "./shared/Auth";
 import Root from "./containers/Root";
 import "./index.css";
 import store from "./store";
+// import registerServiceWorker from "./registerServiceWorker";
 
 createAxiosInterceptors(axios, store);
 
 ReactDOM.render(<Root />, document.getElementById("root") as HTMLElement);
+
+// TODO: Look into re-enabling service worker
+// registerServiceWorker();
 
 // Set App Element for accessibilty
 Modal.setAppElement("#root");
