08a4f1f3a4938a08c51fe8a32c83f800c2783295
Andres <andres.mgotor@gmail.com>
2018-11-12 11:06:34 +0100

    Add link for repo tags (#802)
    * Add link for repo tags
    
    * Move Link import

--- dashboard/src/components/InfoCard/InfoCard.test.tsx
+++ dashboard/src/components/InfoCard/InfoCard.test.tsx
@@ -41,3 +41,26 @@ it("should avoid tags if they are not defined", () => {
   );
   expect(wrapper.find(".ListItem__content__info_tag")).not.toExist();
 });
+
+it("should parse JSX elements in the tags", () => {
+  const tag1 = <span className="tag1">tag1</span>;
+  const tag2 = <span className="tag2">tag2</span>;
+  const wrapper = shallow(
+    <InfoCard
+      title="foo"
+      info="foobar"
+      link="/a/link/somewhere"
+      icon="an-icon.png"
+      tag1Class="blue"
+      tag1Content={tag1}
+      tag2Class="red"
+      tag2Content={tag2}
+    />,
+  );
+  const tag1Elem = wrapper.find(".tag1");
+  expect(tag1Elem).toExist();
+  expect(tag1Elem.text()).toBe("tag1");
+  const tag2Elem = wrapper.find(".tag2");
+  expect(tag2Elem).toExist();
+  expect(tag2Elem.text()).toBe("tag2");
+});
