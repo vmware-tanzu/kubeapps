e1f9c23c3aa3c3243c976e8ee625ba49eb9463a6
Adnan Abdulhussein <adnan@bitnami.com>
2018-11-14 10:22:29 -0800

    rewrite as React.SFC components
    also only display paragraph for svcPlan free parameter if defined

--- dashboard/src/components/ServiceInstanceView/ServiceInstanceInfo.tsx
+++ dashboard/src/components/ServiceInstanceView/ServiceInstanceInfo.tsx
@@ -12,66 +12,64 @@ interface IServiceInstanceInfoProps {
   plan?: IServicePlan;
 }
 
-class ServiceInstanceInfo extends React.Component<IServiceInstanceInfoProps> {
-  public render() {
-    const { instance, svcClass, plan } = this.props;
-    const name = instance.metadata.name;
-    const externalMetadata = svcClass && svcClass.spec.externalMetadata;
-    const imageUrl = (externalMetadata && externalMetadata.imageUrl) || placeholder;
+const ServiceInstanceInfo: React.SFC<IServiceInstanceInfoProps> = props => {
+  const { instance, svcClass, plan } = props;
+  const name = instance.metadata.name;
+  const externalMetadata = svcClass && svcClass.spec.externalMetadata;
+  const imageUrl = (externalMetadata && externalMetadata.imageUrl) || placeholder;
 
-    return (
-      <CardGrid className="ServiceInstanceInfo">
-        <Card>
-          <CardIcon icon={imageUrl} />
-          <CardContent>
-            <h5>{name}</h5>
-            {svcClass && this.renderSvcClassInfo(svcClass)}
-            {plan && this.renderPlanInfo(plan)}
-          </CardContent>
-        </Card>
-      </CardGrid>
-    );
-  }
+  return (
+    <CardGrid className="ServiceInstanceInfo">
+      <Card>
+        <CardIcon icon={imageUrl} />
+        <CardContent>
+          <h5>{name}</h5>
+          {svcClass && renderSvcClassInfo(svcClass)}
+          {plan && renderPlanInfo(plan)}
+        </CardContent>
+      </Card>
+    </CardGrid>
+  );
+};
 
-  private renderSvcClassInfo(svcClass: IClusterServiceClass) {
-    const { spec } = svcClass;
-    const { externalMetadata } = spec;
-    const svcName = externalMetadata ? externalMetadata.displayName : spec.externalName;
-    const description = externalMetadata ? externalMetadata.longDescription : spec.description;
+const renderSvcClassInfo: React.SFC<IClusterServiceClass> = svcClass => {
+  const { spec } = svcClass;
+  const { externalMetadata } = spec;
+  const svcName = externalMetadata ? externalMetadata.displayName : spec.externalName;
+  const description = externalMetadata ? externalMetadata.longDescription : spec.description;
 
-    return (
-      <React.Fragment>
-        <strong>Class:</strong> {svcName}
-        <p>{description}</p>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <strong>Class:</strong> {svcName}
+      <p>{description}</p>
+    </React.Fragment>
+  );
+};
 
-  private renderPlanInfo(svcPlan: IServicePlan) {
-    const { spec } = svcPlan;
-    const { externalMetadata } = spec;
-    const planName = externalMetadata ? externalMetadata.displayName : spec.externalName;
-    const description =
-      externalMetadata && externalMetadata.bullets ? externalMetadata.bullets : [spec.description];
-    const free = svcPlan.spec.free ? <span>Free ✓</span> : undefined;
-    const bullets = (
-      <div>
-        <ul>
-          {description.map(bullet => (
-            <li key={bullet}>{bullet}</li>
-          ))}
-        </ul>
-      </div>
-    );
+const renderPlanInfo: React.SFC<IServicePlan> = svcPlan => {
+  const { spec } = svcPlan;
+  const { externalMetadata } = spec;
+  const planName = externalMetadata ? externalMetadata.displayName : spec.externalName;
+  const description =
+    externalMetadata && externalMetadata.bullets ? externalMetadata.bullets : [spec.description];
+  const free = svcPlan.spec.free ? <span>Free ✓</span> : undefined;
+  const bullets = (
+    <div>
+      <ul>
+        {description.map(bullet => (
+          <li key={bullet}>{bullet}</li>
+        ))}
+      </ul>
+    </div>
+  );
 
-    return (
-      <React.Fragment>
-        <strong>Plan:</strong> {planName}
-        <p className="type-small margin-reset margin-b-big type-color-light-blue">{free}</p>
-        {bullets}
-      </React.Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      <strong>Plan:</strong> {planName}
+      {free && <p className="type-small margin-reset margin-b-big type-color-light-blue">{free}</p>}
+      {bullets}
+    </React.Fragment>
+  );
+};
 
 export default ServiceInstanceInfo;
--- dashboard/src/components/ServiceInstanceView/ServiceInstanceStatus.tsx
+++ dashboard/src/components/ServiceInstanceView/ServiceInstanceStatus.tsx
@@ -10,68 +10,66 @@ interface IServiceInstanceStatusProps {
   instance: IServiceInstance;
 }
 
-class ServiceInstanceStatus extends React.Component<IServiceInstanceStatusProps> {
-  public render() {
-    const { instance } = this.props;
-    const status = instance.status.conditions[0];
-    if (status) {
-      if (status.reason.match(/Provisioning/)) {
-        return this.renderProvisioningStatus();
-      }
-      if (status.reason.match(/Provisioned|Success/)) {
-        return this.renderProvisionedStatus();
-      }
-      if (status.reason.match(/Failed|Error/)) {
-        return this.renderFailedStatus();
-      }
-      if (status.reason.match(/Deprovisioning/)) {
-        return this.renderDeprovisioningStatus();
-      }
+const ServiceInstanceStatus: React.SFC<IServiceInstanceStatusProps> = props => {
+  const { instance } = props;
+  const status = instance.status.conditions[0];
+  if (status) {
+    if (status.reason.match(/Provisioning/)) {
+      return renderProvisioningStatus();
     }
-    return this.renderUnknownStatus();
+    if (status.reason.match(/Provisioned|Success/)) {
+      return renderProvisionedStatus();
+    }
+    if (status.reason.match(/Failed|Error/)) {
+      return renderFailedStatus();
+    }
+    if (status.reason.match(/Deprovisioning/)) {
+      return renderDeprovisioningStatus();
+    }
   }
+  return renderUnknownStatus();
+};
 
-  private renderProvisionedStatus() {
-    return (
-      <span className="DeploymentStatus DeploymentStatus--success">
-        <Check className="icon padding-t-tiny" /> Provisioned
-      </span>
-    );
-  }
+const renderProvisionedStatus = () => {
+  return (
+    <span className="DeploymentStatus DeploymentStatus--success">
+      <Check className="icon padding-t-tiny" /> Provisioned
+    </span>
+  );
+};
 
-  private renderProvisioningStatus() {
-    return (
-      <span className="DeploymentStatus DeploymentStatus--pending">
-        <Compass className="icon padding-t-tiny" /> Provisioning
-      </span>
-    );
-  }
+const renderProvisioningStatus = () => {
+  return (
+    <span className="DeploymentStatus DeploymentStatus--pending">
+      <Compass className="icon padding-t-tiny" /> Provisioning
+    </span>
+  );
+};
 
-  private renderDeprovisioningStatus() {
-    return (
-      <span className="DeploymentStatus DeploymentStatus--pending">
-        <Compass className="icon padding-t-tiny" /> Deprovisioning
-      </span>
-    );
-  }
+const renderDeprovisioningStatus = () => {
+  return (
+    <span className="DeploymentStatus DeploymentStatus--pending">
+      <Compass className="icon padding-t-tiny" /> Deprovisioning
+    </span>
+  );
+};
 
-  private renderFailedStatus() {
-    return (
-      <span className="DeploymentStatus">
-        {/* TODO(prydonius): move style to CSS once we switch all icons to feather icons */}
-        <AlertTriangle className="icon" style={{ bottom: "-0.3em" }} /> Failed
-      </span>
-    );
-  }
+const renderFailedStatus = () => {
+  return (
+    <span className="DeploymentStatus">
+      {/* TODO(prydonius): move style to CSS once we switch all icons to feather icons */}
+      <AlertTriangle className="icon" style={{ bottom: "-0.3em" }} /> Failed
+    </span>
+  );
+};
 
-  private renderUnknownStatus() {
-    return (
-      <span className="DeploymentStatus">
-        {/* TODO(prydonius): move style to CSS once we switch all icons to feather icons */}
-        <AlertTriangle className="icon" style={{ bottom: "-0.3em" }} /> Unknown
-      </span>
-    );
-  }
-}
+const renderUnknownStatus = () => {
+  return (
+    <span className="DeploymentStatus">
+      {/* TODO(prydonius): move style to CSS once we switch all icons to feather icons */}
+      <AlertTriangle className="icon" style={{ bottom: "-0.3em" }} /> Unknown
+    </span>
+  );
+};
 
 export default ServiceInstanceStatus;
