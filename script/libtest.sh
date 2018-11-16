2fdbc6ffe7856504b469e9568c8f316dd186a8eb
Miguel Martínez Triviño <migmartri@gmail.com>
2018-11-16 13:21:48 -0800

    K8s helpers improvements (#833)

--- script/libtest.sh
+++ script/libtest.sh
@@ -15,35 +15,6 @@
 export TEST_MAX_WAIT_SEC=300
 
 ## k8s specific Helper functions
-k8s_wait_for_pod() {
-    namespace=${1:?}
-    labelSelector=${2:?}
-    condition=${3:?}
-    echo "Waiting for pod '${@:2}' to be ${condition} ... "
-    local -i cnt=${TEST_MAX_WAIT_SEC:?}
-
-    # Retries just in case it is not stable
-    local -i successCount=0
-    while [ "$successCount" -lt "3" ]; do
-        if kubectl get pod -a -n "$namespace" -l "$labelSelector" | grep -q "$condition"; then
-            ((successCount=successCount+1))
-        fi
-        ((cnt=cnt-1)) || return 1
-        sleep 1
-    done
-
-}
-k8s_wait_for_pod_ready() {
-    namespace=${1:?}
-    labelSelector=${2:?}
-    k8s_wait_for_pod $namespace $labelSelector Running
-}
-
-k8s_wait_for_pod_completed() {
-    namespace=${1:?}
-    labelSelector=${2:?}
-    k8s_wait_for_pod $namespace $labelSelector Completed
-}
 
 k8s_ensure_image() {
     namespace=${1:?}
@@ -57,4 +28,45 @@ k8s_ensure_image() {
         echo "Failed to found $expectedPattern"
         return 1
     fi
+}
+
+# Waits for the rollout of all the deployments in a provided namespace 
+k8s_wait_for_deployments_rollout() {
+    namespace=${1:?}
+    # Check three times and return the latest result
+    for i in {1..3}; do
+        res=
+        kubectl get deployment -o name --namespace $namespace \
+            | xargs -n1 kubectl rollout status --namespace $namespace || res=$?
+    done
+
+    return $res
+}
+
+# Waits for a set of jobs matching the provided tag to be Completed.
+# It retries up to $TEST_MAX_WAIT_SEC
+k8s_wait_for_job_completed() {
+    namespace=${1:?}
+    labelSelector=${2:?}
+
+    local -i retryTimeSeconds=${TEST_MAX_WAIT_SEC:?}
+    local -i retryTimeStepSeconds=5
+
+
+    while [ "$retryTimeSeconds" -gt 0 ]; do
+        res=$(kubectl get jobs -n $namespace -l $labelSelector \
+        -o jsonpath='{.items[*].status.conditions[?(@.type=="Complete")].status}' | grep "True")
+        # There is a job that finished
+        if [[ $res ]]; then
+            return 0
+        fi
+        # It did not finished so we reduce the remaining time and wait for next retry cycle
+        echo "Waiting for job '${@:2}' to be completed, will retry in $retryTimeStepSeconds seconds ... "
+        retryTimeSeconds=retryTimeSeconds-$retryTimeStepSeconds
+        sleep $retryTimeStepSeconds
+    done
+
+    echo "Job '${@:2}' did not complete"
+
+    return 1
 }
