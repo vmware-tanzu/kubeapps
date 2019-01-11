79781703763f5702064b3220ab250cc640f24fcf
Adnan Abdulhussein <adnan@bitnami.com>
2019-01-11 18:48:15 +0000

    fix tests

--- dashboard/src/components/AppView/ServicesTable/ServicesTable.test.tsx
+++ dashboard/src/components/AppView/ServicesTable/ServicesTable.test.tsx
@@ -1,87 +1,11 @@
 import { shallow } from "enzyme";
-import context from "jest-plugin-context";
 import * as React from "react";
 
-import itBehavesLike from "../../../shared/specs";
-
-import LoadingWrapper from "../../../components/LoadingWrapper";
 import ServiceItem from "./ServiceItem";
 import ServiceTable from "./ServicesTable";
 
-context("when fetching ingresses or services", () => {
-  itBehavesLike("aLoadingComponent", {
-    component: ServiceTable,
-    props: {
-      services: [{ isFetching: true }],
-    },
-  });
-});
-
 it("renders a message if there are no services or ingresses", () => {
   const wrapper = shallow(<ServiceTable serviceRefs={[]} />);
-  expect(
-    wrapper
-      .find(LoadingWrapper)
-      .shallow()
-      .find(ServiceItem),
-  ).not.toExist();
-  expect(
-    wrapper
-      .find(LoadingWrapper)
-      .shallow()
-      .text(),
-  ).toContain("The current application does not contain any service");
+  expect(wrapper.find(ServiceItem)).not.toExist();
+  expect(wrapper.text()).toContain("The current application does not contain any Service objects");
 });
-
-// these tests are no longer relevant as ServicesTable doesn't have the Service
-// resources anymore
-// TODO: update or remove
-
-// it("renders a table with a service with a LoadBalancer", () => {
-//   const service = {
-//     metadata: {
-//       name: "foo",
-//     },
-//     spec: {
-//       type: "LoadBalancer",
-//     },
-//   } as IResource;
-//   const services = [{ isFetching: false, item: service }];
-//   const wrapper = shallow(<ServiceTable serviceRefs={services} />);
-//   expect(wrapper.find(ServiceItem).props()).toMatchObject({
-//     service: { metadata: { name: "foo" }, spec: { type: "LoadBalancer" } },
-//   });
-// });
-
-// it("renders a table with a service with two services", () => {
-//   const service1 = {
-//     metadata: {
-//       name: "foo",
-//     },
-//     spec: {
-//       type: "LoadBalancer",
-//     },
-//   } as IResource;
-//   const service2 = {
-//     metadata: {
-//       name: "bar",
-//     },
-//     spec: {
-//       type: "ClusterIP",
-//     },
-//   } as IResource;
-//   const services = [{ isFetching: false, item: service1 }, { isFetching: false, item: service2 }];
-//   const wrapper = shallow(<ServiceTable services={services} />);
-//   expect(
-//     wrapper
-//       .find(ServiceItem)
-//       .at(0)
-//       .props(),
-//   ).toMatchObject({ service: service1 });
-//   expect(
-//     wrapper
-//       .find(ServiceItem)
-//       .at(1)
-//       .props(),
-//   ).toMatchObject({ service: service2 });
-// });
