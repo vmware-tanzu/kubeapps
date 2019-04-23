ea67941e62e463b79e4f4730758bbe47590b6f7f
Andres Martinez Gotor <andres@bitnami.com>
2019-04-23 15:00:24 -0700

    Draft

--- script/e2e-test.sh
+++ script/e2e-test.sh
@@ -31,7 +31,10 @@ echo "IMAGE_REPO_SUFFIX: $IMG_MODIFIER"
 kubectl version
 
 # Install Tiller with TLS support
+kubectl -n kube-system create sa tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 helm init \
+  --service-account tiller \
   --tiller-tls \
   --tiller-tls-cert ${CERTS_DIR}/tiller.cert.pem \
   --tiller-tls-key ${CERTS_DIR}/tiller.key.pem \
