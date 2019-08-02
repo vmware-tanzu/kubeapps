54127bed7d7788ccab308410840ba0cc5a467e30
Michael Nelson <mnelson@bitnami.com>
2019-08-02 18:39:47 +1000

    State not set to authenticated when validateToken fails with non-401 error (#1100)
    * State is not set to authenticated when validateToken fails with non-401 error
    
    * A 403 during validateToken indicates successful authn (though not authz)

--- dashboard/src/shared/Auth.ts
+++ dashboard/src/shared/Auth.ts
@@ -1,6 +1,7 @@
 import Axios, { AxiosResponse } from "axios";
 const AuthTokenKey = "kubeapps_auth_token";
 const AuthTokenOIDCKey = "kubeapps_auth_token_oidc";
+import { APIBase } from "./Kube";
 
 export class Auth {
   public static getAuthToken() {
@@ -43,11 +44,19 @@ export class Auth {
   // Throws an error if the token is invalid
   public static async validateToken(token: string) {
     try {
-      await Axios.get("api/kube/", { headers: { Authorization: `Bearer ${token}` } });
+      await Axios.get(APIBase + "/", { headers: { Authorization: `Bearer ${token}` } });
     } catch (e) {
       const res = e.response as AxiosResponse;
       if (res.status === 401) {
         throw new Error("invalid token");
+      }
+      // A 403 authorization error only occurs if the token resulted in
+      // successful authentication. We don't make any assumptions over RBAC
+      // for the root "/" nonResourceURL or other required authz permissions
+      // until operations on those resources are attempted (though we may
+      // want to revisit this in the future).
+      if (res.status !== 403) {
+        throw new Error(`${res.status}: ${res.data}`);
       }
     }
   }
