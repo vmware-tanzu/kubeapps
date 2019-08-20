70ce929881c52598b3def3e243487cbd346cd00d
Andres Martinez Gotor <andres@bitnami.com>
2019-08-20 12:52:40 +0200

    Refactor Modals style

--- dashboard/src/components/PrivateRoute/PrivateRoute.tsx
+++ dashboard/src/components/PrivateRoute/PrivateRoute.tsx
@@ -22,19 +22,7 @@ class PrivateRoute extends React.Component<IPrivateRouteProps> {
     }
     if (sessionExpired) {
       return (
-        <Modal
-          style={{
-            content: {
-              bottom: "auto",
-              left: "50%",
-              marginRight: "-50%",
-              right: "auto",
-              top: "50%",
-              transform: "translate(-50%, -50%)",
-            },
-          }}
-          isOpen={true}
-        >
+        <Modal className="centered-modal" isOpen={true}>
           <div>
             <div className="margin-b-normal">
               Your session has expired or the connection has been lost, please reload the page.
