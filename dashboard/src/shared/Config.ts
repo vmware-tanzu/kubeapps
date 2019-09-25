9ff2187f4002658cb84235e797fd52d388b53a34
Andres Martinez Gotor <andres@bitnami.com>
2019-09-25 16:42:15 +0200

    Add feature flag for basic form (#1180)

--- dashboard/src/shared/Config.ts
+++ dashboard/src/shared/Config.ts
@@ -5,6 +5,7 @@ export interface IConfig {
   namespace: string;
   appVersion: string;
   error?: Error;
+  enableBasicForm: boolean;
 }
 
 export default class Config {
@@ -16,9 +17,14 @@ export default class Config {
     // TODO(miguel) Rename env variable to KUBEAPPS_NAMESPACE once/if we eject create-react-app
     // Currently we are using REACT_APP_* because it's the only way to inject env variables in a sealed setup.
     // Please note that this env variable gets mapped in the run command in the package.json file
-    if (process.env.NODE_ENV !== "production" && process.env.REACT_APP_KUBEAPPS_NS) {
-      data.namespace = process.env.REACT_APP_KUBEAPPS_NS;
+    if (process.env.NODE_ENV !== "production") {
       data.appVersion = "DEVEL";
+      if (process.env.REACT_APP_KUBEAPPS_NS) {
+        data.namespace = process.env.REACT_APP_KUBEAPPS_NS;
+      }
+      if (process.env.REACT_APP_KUBEAPPS_ENABLE_BASIC_FORM === "true") {
+        data.enableBasicForm = true;
+      }
     }
 
     return data;
