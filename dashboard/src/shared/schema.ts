e076f9b6b6014f552f126161d598f5106c0dde63
Andres Martinez Gotor <andres@bitnami.com>
2019-10-03 11:43:42 +0200

    Replace logic within lookForParams

--- dashboard/src/shared/schema.ts
+++ dashboard/src/shared/schema.ts
@@ -15,12 +15,39 @@ nullOptions.nullStr = "";
 // It returns a key:value map for easier handling.
 export function retrieveBasicFormParams(
   defaultValues: string,
-  schema: jsonSchema.JSONSchema4,
+  schema?: jsonSchema.JSONSchema4,
+  parentPath?: string,
 ): { [key: string]: IBasicFormParam } {
+  let params = {};
   if (schema && schema.properties) {
-    return lookForFormParams(defaultValues, schema.properties);
+    const properties = schema.properties!;
+    Object.keys(properties).map(propertyKey => {
+      // The param path is its parent path + the object key
+      const itemPath = `${parentPath || ""}${propertyKey}`;
+      // If the property has the key "form", it's a basic parameter
+      if (properties[propertyKey].form) {
+        // Use the default value either from the JSON schema or the default values
+        const value = getValue(defaultValues, itemPath, properties[propertyKey].default);
+        params = {
+          ...params,
+          // The key of the param is the value of the form tag
+          [properties[propertyKey].form]: { path: itemPath, value },
+        };
+      }
+      // If the property is an object, iterate recursively
+      if (schema.properties![propertyKey].type === "object") {
+        params = {
+          ...params,
+          ...retrieveBasicFormParams(
+            defaultValues,
+            schema.properties![propertyKey],
+            `${itemPath}.`,
+          ),
+        };
+      }
+    });
   }
-  return {};
+  return params;
 }
 
 // setValue modifies the current values (text) based on a path
@@ -36,39 +63,4 @@ export function getValue(values: string, path: string, defaultValue?: any) {
   const doc = YAML.parseDocument(values);
   const splittedPath = path.split(".");
   return (doc as any).getIn(splittedPath) || defaultValue;
-}
-
-// lookForFormParams iterates recursively over JSON Schema properties and returns IBasicFormParams
-function lookForFormParams(
-  defaultValues: string,
-  schemaProperties: jsonSchema.JSONSchema4,
-  parentPath?: string,
-): { [key: string]: IBasicFormParam } {
-  let params = {};
-  Object.keys(schemaProperties).map(propertyKey => {
-    // The param path is its parent path + the object key
-    const itemPath = `${parentPath || ""}${propertyKey}`;
-    // If the property has the key "form", it's a basic parameter
-    if (schemaProperties[propertyKey].form) {
-      // Use the default value either from the JSON schema or the default values
-      const value = getValue(defaultValues, itemPath, schemaProperties[propertyKey].default);
-      params = {
-        ...params,
-        // The key of the param is the value of the form tag
-        [schemaProperties[propertyKey].form]: { path: itemPath, value },
-      };
-    }
-    // If the property is an object, iterate recursively
-    if (schemaProperties[propertyKey].type === "object") {
-      params = {
-        ...params,
-        ...lookForFormParams(
-          defaultValues,
-          schemaProperties[propertyKey].properties,
-          `${itemPath}.`,
-        ),
-      };
-    }
-  });
-  return params;
 }
