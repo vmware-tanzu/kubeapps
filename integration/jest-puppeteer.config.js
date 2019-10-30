67fa388600a445c21b41f0e6d19a40036d0f9b53
Andres Martinez Gotor <andresmgot@vmware.com>
2019-10-30 11:14:53 +0100

    Additional end-to-end tests (#1261)

--- integration/jest-puppeteer.config.js
+++ integration/jest-puppeteer.config.js
@@ -4,5 +4,6 @@ module.exports = {
   launch: {
     headless,
     args: ["--no-sandbox"]
-  }
+  },
+  browserContext: "incognito"
 };
---
+++ integration/use-cases/create-registry.js
@@ -0,0 +1,41 @@
+jest.setTimeout(120000);
+
+test("Deploys an application with the values by default", async () => {
+  page.setDefaultTimeout(4000);
+  await page.goto(getUrl("/#/login"));
+
+  await expect(page).toFillForm("form", {
+    token: process.env.ADMIN_TOKEN
+  });
+
+  await expect(page).toClick("button", { text: "Login" });
+
+  // Double click to show configuration
+  await expect(page).toClick("a", { text: "Configuration" });
+  await expect(page).toClick("a", { text: "Configuration" });
+
+  await expect(page).toClick("a", { text: "App Repositories" });
+
+  await expect(page).toClick("button", { text: "Add App Repository" });
+
+  await page.type("#kubeapps-repo-name", "my-repo");
+
+  await page.type("#kubeapps-repo-url", "https://charts.gitlab.io/");
+
+  await expect(page).toClick("button", { text: "Install Repo" });
+
+  await expect(page).toClick("a", { text: "my-repo" });
+
+  let retries = 3;
+  while (retries > 0) {
+    try {
+      await expect(page).toMatch("gitlab", { timeout: 2000 });
+      break;
+    } catch (e) {
+      // Refresh since the chart will get a bit of time to populate
+      await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
+    } finally {
+      retries--;
+    }
+  }
+});
