6024b152b72580f52c6315589b2bf18b0bc7ab79
Simon Alling <alling.simon@gmail.com>
2019-12-02 12:42:49 +0100

    Refactor token extraction

--- pkg/auth/authgate.go
+++ pkg/auth/authgate.go
@@ -15,15 +15,18 @@ type contextKey int
 // UserKey is the context key for the User data in the request context
 const UserKey contextKey = 0
 
+// tokenPrefix is the string preceding the token in the Authorization header.
+const tokenPrefix = "Bearer "
+
 // AuthGate implements middleware to check if the user is logged in before continuing
 func AuthGate() negroni.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
-		authHeader := strings.Split(req.Header.Get("Authorization"), "Bearer ")
-		if len(authHeader) != 2 {
+		token := ExtractToken(req.Header.Get("Authorization"))
+		if token == "" {
 			response.NewErrorResponse(http.StatusUnauthorized, "Unauthorized").Write(w)
 			return
 		}
-		userAuth, err := NewAuth(authHeader[1])
+		userAuth, err := NewAuth(token)
 		if err != nil {
 			response.NewErrorResponse(http.StatusInternalServerError, err.Error()).Write(w)
 			return
@@ -35,5 +38,14 @@ func AuthGate() negroni.HandlerFunc {
 		}
 		ctx := context.WithValue(req.Context(), UserKey, userAuth)
 		next(w, req.WithContext(ctx))
+	}
+}
+
+// ExtractToken extracts the token from a correctly formatted Authorization header.
+func ExtractToken(headerValue string) string {
+	if strings.HasPrefix(headerValue, tokenPrefix) {
+		return headerValue[len(tokenPrefix):]
+	} else {
+		return ""
 	}
 }
