b1352104adf8df4b0b8070722b629cd1331647bb
Andres Martinez Gotor <andresmgot@vmware.com>
2019-12-11 12:18:10 +0100

    Use arguments interpolation from postgres (#1345)

--- cmd/asset-syncer/postgresql_utils_test.go
+++ cmd/asset-syncer/postgresql_utils_test.go
@@ -35,12 +35,13 @@ func (d *mockDB) Close() error {
 func Test_DeletePGRepo(t *testing.T) {
 	repoName := "test"
 	m := &mockDB{&mock.Mock{}}
-	tables := []string{chartTable, repositoryTable, chartFilesTable}
+	tables := []string{chartTable, chartFilesTable}
 	for _, table := range tables {
-		q := fmt.Sprintf("DELETE FROM %s WHERE info -> 'repo' ->> 'name' = '%s'", table, repoName)
+		q := fmt.Sprintf("DELETE FROM %s WHERE info -> 'repo' ->> 'name' = $1", table)
 		// Since we are not specifying any argument, Query is called with []interface{}(nil)
-		m.On("Query", q, []interface{}(nil))
+		m.On("Query", q, []interface{}{repoName})
 	}
+	m.On("Query", "DELETE FROM repos WHERE name = $1", []interface{}{repoName})
 
 	pgManager := &postgresAssetManager{"", m}
 	err := pgManager.Delete(repoName)
@@ -52,8 +53,8 @@ func Test_DeletePGRepo(t *testing.T) {
 
 func Test_PGRepoAlreadyPropcessed(t *testing.T) {
 	m := &mockDB{&mock.Mock{}}
-	pgManager := &postgresAssetManager{m}
-	m.On("QueryRow", "SELECT checksum FROM repos WHERE name = 'foo'", []interface{}(nil))
+	pgManager := &postgresAssetManager{"", m}
+	m.On("QueryRow", "SELECT checksum FROM repos WHERE name = $1", []interface{}{"foo"})
 	pgManager.RepoAlreadyProcessed("foo", "123")
 	m.AssertExpectations(t)
 }
@@ -63,13 +64,13 @@ func Test_PGUpdateLastCheck(t *testing.T) {
 	repoName := "foo"
 	checksum := "bar"
 	now := time.Now()
-	pgManager := &postgresAssetManager{m}
-	expectedQuery := fmt.Sprintf(`INSERT INTO %s (name, checksum, last_update)
-	VALUES ('%s', '%s', '%s')
+	pgManager := &postgresAssetManager{"", m}
+	expectedQuery := `INSERT INTO repos (name, checksum, last_update)
+	VALUES ($1, $2, $3)
 	ON CONFLICT (name) 
-	DO UPDATE SET last_update = '%s', checksum = '%s'
-	`, repositoryTable, repoName, checksum, now.String(), now.String(), checksum)
-	m.On("Query", expectedQuery, []interface{}(nil))
+	DO UPDATE SET last_update = $3, checksum = $2
+	`
+	m.On("Query", expectedQuery, []interface{}{repoName, checksum, now.String()})
 	pgManager.UpdateLastCheck(repoName, checksum, now)
 	m.AssertExpectations(t)
 }
@@ -77,7 +78,7 @@ func Test_PGUpdateLastCheck(t *testing.T) {
 func Test_PGremoveMissingCharts(t *testing.T) {
 	charts := []chart{{ID: "foo"}, {ID: "bar"}}
 	m := &mockDB{&mock.Mock{}}
-	pgManager := &postgresAssetManager{m}
+	pgManager := &postgresAssetManager{"", m}
 	m.On("Query", "DELETE FROM charts WHERE info ->> 'ID' NOT IN ('foo', 'bar')", []interface{}(nil))
 	pgManager.removeMissingCharts(charts)
 	m.AssertExpectations(t)
