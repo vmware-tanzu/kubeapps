15d5b2d4c9f226f05361d397fc4aa5f355c7b179
Andres Martinez Gotor <andres@bitnami.com>
2020-01-23 10:39:32 +0100

    Add explanatory comment

--- dashboard/src/components/ChartView/LinkRenderer.tsx
+++ dashboard/src/components/ChartView/LinkRenderer.tsx
@@ -5,6 +5,8 @@ const LinkRenderer: React.SFC<{}> = (props: any) => {
   if (props.href.startsWith("#")) {
     return <Link to={props.href}>{props.children}</Link>;
   }
+  // If it's not a hash link it's an external link since it's rendering
+  // the chart README. Because of that, render it as a normal anchor
   return <a href={props.href}>{props.children}</a>;
 };
 
