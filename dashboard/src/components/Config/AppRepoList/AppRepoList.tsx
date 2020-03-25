07dda1e40b6d326ca5f218aa90b0ebecbad93952
Michael Nelson <mnelson@bitnami.com>
2020-03-25 15:35:22 +1100

    Update docs for private app repos.

--- dashboard/src/components/Config/AppRepoList/AppRepoList.tsx
+++ dashboard/src/components/Config/AppRepoList/AppRepoList.tsx
@@ -142,7 +142,7 @@ class AppRepoList extends React.Component<IAppRepoListProps> {
                 Kubeapps now enables you to create App Repositories in your own namespace that will
                 be available in your own namespace and, in the future, optionally available in other
                 namespaces to which you have access. You can read more information in the{" "}
-                <a href="https://github.com/kubeapps/kubeapps/blob/master/docs/user/private-app-repository.md#per-namespace-app-repositories">
+                <a href="https://github.com/kubeapps/kubeapps/blob/master/docs/user/private-app-repository.md">
                   Private App Repository docs
                 </a>
                 .
