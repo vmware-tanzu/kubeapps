83d39ad6f99ea3f69729761a28fb455e24d220c0
Michael Nelson <mnelson@bitnami.com>
2020-04-16 15:27:58 +1000

    Implement adding imagePullSecrets to Pod resources.

--- pkg/agent/docker_secrets_postrenderer.go
+++ pkg/agent/docker_secrets_postrenderer.go
@@ -2,7 +2,9 @@ package agent
 
 import (
 	"bytes"
+	"fmt"
 	"io"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 )
@@ -39,7 +41,27 @@ func (r *DockerSecretsPostRenderer) Run(renderedManifests *bytes.Buffer) (modifi
 		resourceList = append(resourceList, resource)
 	}
 
-	// TODO: Update relevant container images with image pull secrets
+	// TODO(mnelson): If re-rendering the entire manifest creates issues, we
+	// could instead find the correct byte position and insert the image pull
+	// secret into the byte stream at the relevant points, but this will be
+	// more complex.
+	for _, resourceItem := range resourceList {
+		resource, ok := resourceItem.(map[interface{}]interface{})
+		if !ok {
+			continue
+		}
+		podSpec, err := getResourcePodSpec(resource)
+		if err != nil {
+			return nil, err
+		}
+		if podSpec == nil {
+			continue
+		}
+		err = r.updatePodSpecWithPullSecrets(podSpec)
+		if err != nil {
+			return nil, err
+		}
+	}
 
 	modifiedManifests = bytes.NewBuffer([]byte{})
 	encoder := yaml.NewEncoder(modifiedManifests)
@@ -53,4 +75,106 @@ func (r *DockerSecretsPostRenderer) Run(renderedManifests *bytes.Buffer) (modifi
 	}
 
 	return modifiedManifests, nil
+}
+
+// updatePodSpecWithPullSecrets updates the podSpec inline with the relevant pull secrets.
+// We do not parse the yaml into actual Kubernetes objects since we want to be
+// independent of api versions. This requires special care and limitations, so
+// we limit our assumptions of the untyped handling to the following, with
+// clear errors when the assumption fails:
+// - The pod spec includes a 'containers' key with a slice value (with a clear error if this is not the case)
+// - Each container value is a map with an 'image' key and string value.
+func (r *DockerSecretsPostRenderer) updatePodSpecWithPullSecrets(podSpec map[interface{}]interface{}) error {
+	containersObject, ok := podSpec["containers"]
+	if !ok {
+		return fmt.Errorf("pod spec did not include a containers key: %+v", podSpec)
+	}
+	containers, ok := containersObject.([]interface{})
+	if !ok {
+		return fmt.Errorf("pod spec did not include a slice for the containers value: %+v", podSpec)
+	}
+
+	// If there are existing pull secrets, initialise our slice with that value
+	// and additionally initialize a map keyed by secret name which we can
+	// use to test existence more easily.
+	var imagePullSecrets []map[string]interface{}
+	existingNames := map[string]bool{}
+	if existingPullSecrets, ok := podSpec["imagePullSecrets"]; ok {
+		imagePullSecrets = existingPullSecrets.([]map[string]interface{})
+		for _, s := range imagePullSecrets {
+			if name, ok := s["name"]; ok {
+				if n, ok := name.(string); ok {
+					existingNames[n] = true
+				}
+			}
+		}
+	}
+
+	for _, c := range containers {
+		container, ok := c.(map[interface{}]interface{})
+		if !ok {
+			return fmt.Errorf("pod containers did not contain maps: %+v", c)
+		}
+		image, ok := container["image"].(string)
+		if !ok {
+			// NOTE: in https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.18/#container-v1-core
+			// the image is optional to allow higher level config management to default or override (such as
+			// deployments or statefulsets), but both only define pod templates which in turn define containers?
+			return fmt.Errorf("pod container did not contain an image reference: %+v", container)
+		}
+
+		// Ignore (dockerhub) image refs without a domain.
+		domainSplit := strings.Split(image, "/")
+		if len(domainSplit) == 1 {
+			continue
+		}
+		imageDomain := domainSplit[0]
+
+		secretName, ok := r.secrets[imageDomain]
+		if !ok {
+			continue
+		}
+		// Only add the secret if it's not already included in the image pull secrets.
+		if _, ok := existingNames[secretName]; !ok {
+			imagePullSecrets = append(imagePullSecrets, map[string]interface{}{"name": secretName})
+			existingNames[secretName] = true
+		}
+	}
+
+	if len(imagePullSecrets) > 0 {
+		podSpec["imagePullSecrets"] = imagePullSecrets
+	}
+
+	return nil
+}
+
+// getResourcePodSpec checks the kind of the resource and extracts the pod spec accordingly.
+// We do not parse the yaml into actual Kubernetes objects since we want to be
+// independent of api versions. This requires special care and limitations, so
+// we limit our assumptions of the untyped handling to:
+// - A resource doc is a map with a "kind" key with a string value (with a clear error if this is not the case)
+// - A pod resource doc has a "spec" key containing a map (with a clear error if this is not the case).
+func getResourcePodSpec(resource map[interface{}]interface{}) (map[interface{}]interface{}, error) {
+	kindValue, ok := resource["kind"]
+	if !ok {
+		return nil, fmt.Errorf("resource did not specify resource kind: %+v", resource)
+	}
+
+	kind, ok := kindValue.(string)
+	if !ok {
+		return nil, fmt.Errorf("resource kind was not a string: %+v", resource)
+	}
+
+	// TODO: Update to support other kinds with pod specs as part of pod templates:
+	// https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.18/#podtemplatespec-v1-core
+	switch kind {
+	case "Pod":
+		podSpec, ok := resource["spec"].(map[interface{}]interface{})
+		if !ok {
+			return nil, fmt.Errorf("pod's spec was not a map: %+v", resource)
+		}
+		return podSpec, nil
+	}
+
+	return nil, nil
 }
--- pkg/agent/docker_secrets_postrenderer_test.go
+++ pkg/agent/docker_secrets_postrenderer_test.go
@@ -27,6 +27,15 @@ func TestDockerSecretsPostRenderer(t *testing.T) {
 			expectErr: true,
 		},
 		{
+			name: "it returns an error if a document has no kind",
+			input: bytes.NewBuffer([]byte(`apiVersion: v1
+nokind: Pod
+metadata: somemetadata
+`)),
+			secrets:   map[string]string{"foo.example.com": "secret-name"},
+			expectErr: true,
+		},
+		{
 			name: "it re-renders the yaml with ordering and indent changes only",
 			input: bytes.NewBuffer([]byte(`apiVersion: v1
 kind: Pod
@@ -47,6 +56,7 @@ spec:
       name: container-name
   restartPolicy: Never
 ---
+kind: Unknown
 other: doc
 `)),
 			output: bytes.NewBuffer([]byte(`apiVersion: v1
@@ -68,9 +78,60 @@ spec:
     name: container-name
   restartPolicy: Never
 ---
+kind: Unknown
 other: doc
 `)),
 			secrets: map[string]string{"foo.example.com": "secret-name"},
+		},
+		{
+			name: "it appends relevant image pull secrets to pod specs",
+			input: bytes.NewBuffer([]byte(`apiVersion: v1
+kind: Pod
+metadata:
+  annotations:
+    annotation-1: some-annotation
+  name: image-secret-test
+spec:
+  containers:
+  - command:
+    - sh
+    - -c
+    - echo 'foo'
+    env:
+    - name: SOME_ENV
+      value: env_value
+    image: example.com/bitnami/nginx:1.16.1-debian-10-r42
+    name: container-name
+  restartPolicy: Never
+---
+kind: Unknown
+other: doc
+`)),
+			output: bytes.NewBuffer([]byte(`apiVersion: v1
+kind: Pod
+metadata:
+  annotations:
+    annotation-1: some-annotation
+  name: image-secret-test
+spec:
+  containers:
+  - command:
+    - sh
+    - -c
+    - echo 'foo'
+    env:
+    - name: SOME_ENV
+      value: env_value
+    image: example.com/bitnami/nginx:1.16.1-debian-10-r42
+    name: container-name
+  imagePullSecrets:
+  - name: secret-1
+  restartPolicy: Never
+---
+kind: Unknown
+other: doc
+`)),
+			secrets: map[string]string{"example.com": "secret-1"},
 		},
 	}
 
@@ -84,6 +145,246 @@ other: doc
 			}
 
 			if got, want := renderedManifests.String(), tc.output.String(); !cmp.Equal(got, want) {
+				t.Errorf("mismatch (-want +got):\n%s", cmp.Diff(want, got))
+			}
+		})
+	}
+}
+
+func TestUpdatePodSpecWithPullSecrets(t *testing.T) {
+	testCases := []struct {
+		name                string
+		podSpec             map[interface{}]interface{}
+		secrets             map[string]string
+		expectedPullSecrets interface{}
+		expectErr           bool
+	}{
+		{
+			name: "it does not add image pull secrets when no secret matches",
+			podSpec: map[interface{}]interface{}{
+				"containers": []interface{}{
+					map[interface{}]interface{}{
+						"image": "example.com/foobar:v1",
+					},
+				},
+			},
+			secrets: map[string]string{
+				"other.com": "secret-1",
+			},
+			expectedPullSecrets: nil,
+		},
+		{
+			name: "it adds an image pull secret when one secret matches",
+			podSpec: map[interface{}]interface{}{
+				"containers": []interface{}{
+					map[interface{}]interface{}{
+						"image": "example.com/foobar:v1",
+					},
+				},
+			},
+			secrets: map[string]string{
+				"example.com": "secret-1",
+			},
+			expectedPullSecrets: []map[string]interface{}{
+				{"name": "secret-1"},
+			},
+		},
+		{
+			name: "it adds multiple image pull secrets when multiple secrets matches",
+			podSpec: map[interface{}]interface{}{
+				"containers": []interface{}{
+					map[interface{}]interface{}{
+						"image": "example.com/foobar:v1",
+					},
+					map[interface{}]interface{}{
+						"image": "otherexample.com/foobar:v1",
+					},
+				},
+			},
+			secrets: map[string]string{
+				"example.com":      "secret-1",
+				"otherexample.com": "secret-2",
+			},
+			expectedPullSecrets: []map[string]interface{}{
+				{"name": "secret-1"},
+				{"name": "secret-2"},
+			},
+		},
+		{
+			name: "it appends to existing image pull secrets",
+			podSpec: map[interface{}]interface{}{
+				"containers": []interface{}{
+					map[interface{}]interface{}{
+						"image": "example.com/foobar:v1",
+					},
+					map[interface{}]interface{}{
+						"image": "otherexample.com/foobar:v1",
+					},
+				},
+				"imagePullSecrets": []map[string]interface{}{
+					map[string]interface{}{
+						"name": "secret-1",
+					},
+				},
+			},
+			secrets: map[string]string{
+				"example.com":      "secret-2",
+				"otherexample.com": "secret-3",
+			},
+			expectedPullSecrets: []map[string]interface{}{
+				{"name": "secret-1"},
+				{"name": "secret-2"},
+				{"name": "secret-3"},
+			},
+		},
+		{
+			name: "it does not duplicate existing image pull secrets",
+			podSpec: map[interface{}]interface{}{
+				"containers": []interface{}{
+					map[interface{}]interface{}{
+						"image": "example.com/foobar:v1",
+					},
+					map[interface{}]interface{}{
+						"image": "otherexample.com/foobar:v1",
+					},
+				},
+				"imagePullSecrets": []map[string]interface{}{
+					map[string]interface{}{
+						"name": "secret-1",
+					},
+				},
+			},
+			secrets: map[string]string{
+				"example.com":      "secret-1",
+				"otherexample.com": "secret-2",
+			},
+			expectedPullSecrets: []map[string]interface{}{
+				{"name": "secret-1"},
+				{"name": "secret-2"},
+			},
+		},
+		{
+			name: "it does not mistake domainless image refs from dockerhub with a badly-named secret",
+			podSpec: map[interface{}]interface{}{
+				"containers": []interface{}{
+					map[interface{}]interface{}{
+						"image": "wordpress",
+					},
+				},
+			},
+			secrets: map[string]string{
+				"wordpress": "secret-1",
+			},
+			expectedPullSecrets: nil,
+		},
+		{
+			name: "it returns an error if a containers key does not exist",
+			podSpec: map[interface{}]interface{}{
+				"notcontainers": []interface{}{
+					map[interface{}]interface{}{
+						"image": "example.com/foobar:v1",
+					},
+				},
+			},
+			expectErr: true,
+		},
+		{
+			name: "it returns an error if a containers value is not a map",
+			podSpec: map[interface{}]interface{}{
+				"containers": "not a slice",
+			},
+			expectErr: true,
+		},
+		{
+			name: "it returns an error if values within the containers slice are not maps",
+			podSpec: map[interface{}]interface{}{
+				"containers": []interface{}{
+					"not a map",
+				},
+			},
+			expectErr: true,
+		},
+		{
+			name: "it returns an error if a container value does not include an image key",
+			podSpec: map[interface{}]interface{}{
+				"containers": []interface{}{
+					map[interface{}]interface{}{
+						"notimage": "example.com/foobar:v1",
+					},
+				},
+			},
+			expectErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := NewDockerSecretsPostRenderer(tc.secrets)
+
+			err := r.updatePodSpecWithPullSecrets(tc.podSpec)
+			if got, want := err != nil, tc.expectErr; got != want {
+				t.Fatalf("got: %t, want: %t. err: %+v", got, want, err)
+			}
+
+			if got, want := tc.podSpec["imagePullSecrets"], tc.expectedPullSecrets; !cmp.Equal(got, want) {
+				t.Errorf("mismatch (-want +got):\n%s", cmp.Diff(want, got))
+			}
+		})
+	}
+}
+
+func TestGetResourcePodSpec(t *testing.T) {
+	testCases := []struct {
+		name      string
+		resource  map[interface{}]interface{}
+		result    map[interface{}]interface{}
+		expectErr bool
+	}{
+		{
+			name: "it returns an error if the resource does not have a kind",
+			resource: map[interface{}]interface{}{
+				"notkind": "Pod",
+				"spec":    map[string]interface{}{"some": "spec"},
+			},
+			expectErr: true,
+		},
+		{
+			name: "it returns an error if the kind is not a string",
+			resource: map[interface{}]interface{}{
+				"kind": map[string]interface{}{"not": "string"},
+				"spec": map[interface{}]interface{}{"some": "spec"},
+			},
+			expectErr: true,
+		},
+		{
+			name: "it returns an error if the spec is not a map",
+			resource: map[interface{}]interface{}{
+				"kind": "Pod",
+				"spec": "not a map",
+			},
+			expectErr: true,
+		},
+		{
+			name: "it returns the pod spec from a pod",
+			resource: map[interface{}]interface{}{
+				"kind": "Pod",
+				"spec": map[interface{}]interface{}{"some": "spec"},
+			},
+			result: map[interface{}]interface{}{
+				"some": "spec",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := getResourcePodSpec(tc.resource)
+
+			if got, want := err != nil, tc.expectErr; got != want {
+				t.Fatalf("got: %t, want: %t. err: %+v", got, want, err)
+			}
+
+			if got, want := result, tc.result; !cmp.Equal(got, want) {
 				t.Errorf("mismatch (-want +got):\n%s", cmp.Diff(want, got))
 			}
 		})
