4c9db995cd637c40f0b6b934e4b16ed3481cb124
Michael Nelson <minelson@vmware.com>
2020-05-22 11:29:55 +1000

    Switch to nodeport for dev env on kind cluster. (#1749)
    * Switch to nodeport for dev env on kind cluster.

--- script/test-certs/gen-certs.sh
+++ script/test-certs/gen-certs.sh
@@ -14,7 +14,6 @@ subjectAltName = @alt_names
 
 [alt_names]
 DNS.1 = localhost
-DNS.2 = dex.dex
 EOF
 
 openssl genrsa -out ./ca.key.pem 4096
@@ -24,16 +23,12 @@ openssl req -key ca.key.pem -new -x509 -days 7300 -sha256 -out ca.cert.pem -subj
 openssl genrsa -out ./tiller.key.pem 4096
 ## helm client key
 openssl genrsa -out ./helm.key.pem 4096
-## dex server key
-openssl genrsa -out ./dex.key.pem 4096
 
 openssl req -key tiller.key.pem -new -sha256 -out tiller.csr.pem -config tls_config -subj "/CN=kubeapps-ca"
-openssl req -key dex.key.pem    -new -sha256 -out dex.csr.pem    -config tls_config -subj "/CN=dex.dex"
 openssl req -key helm.key.pem   -new -sha256 -out helm.csr.pem   -config tls_config -subj "/CN=kubeapps-ca"
 
 openssl x509 -days 7300 -req -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial -in tiller.csr.pem -out tiller.cert.pem
 openssl x509 -days 7300 -req -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial -in helm.csr.pem -out helm.cert.pem
-openssl x509 -days 7300 -req -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial -in dex.csr.pem -out dex.cert.pem
 
 # clean up unnecessary files
-rm ca.key.pem ca.srl dex.csr.pem helm.csr.pem tiller.csr.pem tls_config
+rm ca.key.pem ca.cert.srl helm.csr.pem tiller.csr.pem tls_config
