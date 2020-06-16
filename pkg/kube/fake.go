007f57c7fa75e631a0ba830e8e3f8cb7982667fb
Michael Nelson <minelson@vmware.com>
2020-06-16 11:24:30 +1000

    Don't ignore errors when creating clientset with AsUser helper. (#1792)

--- pkg/kube/fake.go
+++ pkg/kube/fake.go
@@ -36,8 +36,8 @@ type FakeHandler struct {
 }
 
 // AsUser fakes user auth
-func (c *FakeHandler) AsUser(token, cluster string) handler {
-	return c
+func (c *FakeHandler) AsUser(token, cluster string) (handler, error) {
+	return c, nil
 }
 
 // AsSVC fakes using current svcaccount
