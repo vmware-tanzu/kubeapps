6e760a76e0461ca8fcbc18ce6c0809e803f85fcd
absoludity <absoludity@gmail.com>
2020-06-18 20:07:42 +1000

    Use two separate types for IChartCatalogItem and IOperatorCatalogItem.
    It's not perfect - there's more that could be DRY'd up and encapsulated,
    but with this change you can actually use TypeScript's type system to
    ensure the correct fields are present.
    
    I've not done an IRL test, this is just to demo the thought.
    
    Fixes #1803 (maybe)

--- dashboard/src/components/Catalog/CatalogItem.tsx
+++ dashboard/src/components/Catalog/CatalogItem.tsx
@@ -8,22 +8,26 @@ import * as url from "../../shared/url";
 import InfoCard from "../InfoCard";
 import "./CatalogItem.css";
 
-
-
-interface ICatalogItemProps {
-  item: ICatalogItem;
-}
-
 export interface ICatalogItem {
   id: string;
   name: string;
   version: string;
   description: string;
-  type: "chart" | "operator";
   namespace: string;
   icon?: string;
-  repo?: IRepo;
-  csv?: string;
+}
+
+export interface IChartCatalogItem extends ICatalogItem {
+  repo: IRepo;
+}
+
+export interface IOperatorCatalogItem extends ICatalogItem {
+  csv: string;
+}
+
+export interface ICatalogItemProps {
+  type: string;
+  item: IChartCatalogItem | IOperatorCatalogItem;
 }
 
 // 3 lines description max
@@ -38,32 +42,55 @@ function trimDescription(desc: string): string {
 }
 
 const CatalogItem: React.SFC<ICatalogItemProps> = props => {
-  const { item } = props;
-  const { icon, name, repo, version, description, type, namespace, id, csv } = item;
-  const iconSrc = icon || placeholder;
-  let link;
-  let tag1;
-  let subIcon;
-  if (type === "chart") {
-    tag1 = (
-      // TODO(#1803): See #1803 regarding the work-arounds below for the fact
-      // that repo is required if we're dealing with a chart here.
-      <Link
-        className="ListItem__content__info_tag_link"
-        to={url.app.repo(repo?.name || "", namespace)}
-      >
-        {repo?.name}
-      </Link>
-    );
-    link = url.app.charts.get(name, repo || {} as IRepo, namespace);
-    subIcon = helmIcon;
+  if (props.type === "operator") {
+    const item = props.item as IOperatorCatalogItem;
+    return OperatorCatalogItem(item); 
   } else {
-    // Cosmetic change, remove the version from the csv name
-    const csvName = csv?.split(".v")[0];
-    tag1 = <span>{csvName}</span>;
-    link = `/ns/${namespace}/operators-instances/new/${csv}/${id}`;
-    subIcon = operatorIcon;
+    const item = props.item as IChartCatalogItem;
+    return ChartCatalogItem(item);
   }
+};
+
+const OperatorCatalogItem: React.SFC<IOperatorCatalogItem> = props => {
+  const { icon, name, csv, version, description, namespace, id } = props;
+  const iconSrc = icon || placeholder;
+  // Cosmetic change, remove the version from the csv name
+  const csvName = props.csv.split(".v")[0];
+  const tag1 = <span>{csvName}</span>;
+  const link = `/ns/${namespace}/operators-instances/new/${csv}/${id}`;
+  const subIcon = operatorIcon;
+  const descriptionC = (
+    <div className="ListItem__content__description">{trimDescription(description)}</div>
+  );
+  return (
+    <InfoCard
+      key={id}
+      title={name}
+      link={link}
+      info={version || "-"}
+      icon={iconSrc}
+      description={descriptionC}
+      tag1Content={tag1}
+      tag1Class={""}
+      subIcon={subIcon}
+    />
+  );
+};
+
+const ChartCatalogItem: React.SFC<IChartCatalogItem> = props => {
+  const { icon, name, repo, version, description, namespace, id } = props;
+  const iconSrc = icon || placeholder;
+  const tag1 = (
+    <Link
+      className="ListItem__content__info_tag_link"
+      to={url.app.repo(repo.name, namespace)}
+    >
+      {repo.name}
+    </Link>
+  );
+  const link = url.app.charts.get(name, repo || {} as IRepo, namespace);
+  const subIcon = helmIcon;
+
   const descriptionC = (
     <div className="ListItem__content__description">{trimDescription(description)}</div>
   );
