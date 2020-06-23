7b58fabb1d55583b38bd4051d1d8e7f4aac96bf9
Andres Martinez Gotor <andres@bitnami.com>
2020-06-23 17:58:48 +0200

    Use only bitnami apps in e2e tests

--- integration/use-cases/default-deployment.js
+++ integration/use-cases/default-deployment.js
@@ -2,14 +2,14 @@ test("Deploys an application with the values by default", async () => {
   await page.goto(getUrl("/#/login"));
 
   await expect(page).toFillForm("form", {
-    token: process.env.ADMIN_TOKEN
+    token: process.env.ADMIN_TOKEN,
   });
 
   await expect(page).toClick("button", { text: "Login" });
 
   await expect(page).toClick("a", { text: "Catalog" });
 
-  await expect(page).toClick("a", { text: "aerospike", timeout: 60000 });
+  await expect(page).toClick("a", { text: "apache", timeout: 60000 });
 
   await expect(page).toClick("button", { text: "Deploy" });
 
