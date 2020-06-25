dd56a3e62031150d46de40ae21cf5890dc10d29b
Andres Martinez Gotor <andres@bitnami.com>
2020-06-25 11:12:13 +0200

    Remove unused code

--- dashboard/src/containers/LayoutContainer/LayoutContainer.ts
+++ dashboard/src/containers/LayoutContainer/LayoutContainer.ts
@@ -1,14 +1,9 @@
 import { connect } from "react-redux";
-import { RouteComponentProps } from "react-router";
 
 import Layout from "../../components/Layout";
 import { IStoreState } from "../../shared/types";
 
-interface IState extends IStoreState {
-  router: RouteComponentProps<{}>;
-}
-
-function mapStateToProps({ config: { featureFlags } }: IState) {
+function mapStateToProps({ config: { featureFlags } }: IStoreState) {
   return {
     UI: featureFlags.ui,
   };
