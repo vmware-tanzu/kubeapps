f153931c61e9295b5b13617c9b23d147355f730f
Andres Martinez Gotor <andres@bitnami.com>
2020-07-15 17:26:57 +0200

    Application Status v2

--- dashboard/src/components/ApplicationStatus/index.tsx
+++ dashboard/src/components/ApplicationStatus/index.tsx
@@ -1,3 +1,18 @@
-import ApplicationStatus from "./ApplicationStatus";
+import * as React from "react";
 
-export default ApplicationStatus;
+import { IApplicationStatusProps } from "./ApplicationStatus";
+
+const ApplicationStatus = React.lazy(() => import("./ApplicationStatus"));
+const ApplicationStatusV2 = React.lazy(() => import("./ApplicationStatus.v2"));
+
+interface IApplicationStatusSelectorProps extends IApplicationStatusProps {
+  UI: string;
+}
+
+const ApplicationStatusSelector: React.FC<IApplicationStatusSelectorProps> = props => (
+  <React.Suspense fallback={null}>
+    {props.UI === "clarity" ? <ApplicationStatusV2 {...props} /> : <ApplicationStatus {...props} />}
+  </React.Suspense>
+);
+
+export default ApplicationStatusSelector;
