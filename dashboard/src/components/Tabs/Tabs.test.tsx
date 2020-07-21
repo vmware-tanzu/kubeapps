e550e82587494aa352d89b3d0e393771420f85a2
Andres Martinez Gotor <andresmgot@vmware.com>
2020-07-21 16:57:33 +0200

    Resource Tabs (#1873)

---
+++ dashboard/src/components/Tabs/Tabs.test.tsx
@@ -0,0 +1,36 @@
+import { mount } from "enzyme";
+import * as React from "react";
+
+import Tabs from "./Tabs";
+
+it("renders several tabs", () => {
+  const wrapper = mount(<Tabs id="tabs" columns={["foo", "bar"]} data={["FOO", "BAR"]} />);
+  expect(wrapper.find("button")).toHaveLength(2);
+  expect(wrapper.find("section")).toHaveLength(2);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it("changes content between tabs", () => {
+  const wrapper = mount(<Tabs id="tabs" columns={["foo", "bar"]} data={["FOO", "BAR"]} />);
+  expect(wrapper.find(".active").text()).toEqual("foo");
+  expect(
+    wrapper
+      .find("section")
+      .findWhere(s => s.prop("aria-hidden") === "false")
+      .text(),
+  ).toEqual("FOO");
+
+  const otherTab = wrapper.find("#tabs-tab1");
+  otherTab.simulate("click");
+  wrapper.update();
+
+  const newSelectedTab = wrapper.find("#tabs-tab1");
+  expect(newSelectedTab).toHaveClassName("active");
+  expect(newSelectedTab.text()).toEqual("bar");
+  expect(
+    wrapper
+      .find("section")
+      .findWhere(s => s.prop("aria-hidden") === "false")
+      .text(),
+  ).toEqual("BAR");
+});
