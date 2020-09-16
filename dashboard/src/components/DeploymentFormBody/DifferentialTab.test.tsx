40bfd00e978a4ba678b2b8c6d615fdac334c8599
Andres Martinez Gotor <andresmgot@vmware.com>
2020-09-16 12:57:35 +0200

    Add an icon when there are changes to review (#2032)
    * Add an icon when there are changes to review
    
    * Restoring defaults use case
    
    * Fix typos

---
+++ dashboard/src/components/DeploymentFormBody/DifferentialTab.test.tsx
@@ -0,0 +1,91 @@
+import { CdsIcon } from "@clr/react/icon";
+import { mount } from "enzyme";
+import * as React from "react";
+import { act } from "react-dom/test-utils";
+import DifferentialTab from "./DifferentialTab";
+
+describe("when installing", () => {
+  it("should show the changes icon if the values change", () => {
+    const wrapper = mount(
+      <DifferentialTab
+        deploymentEvent="install"
+        deployedValues=""
+        defaultValues="foo"
+        appValues="bar"
+      />,
+    );
+    expect(wrapper.find(CdsIcon).prop("hidden")).toBe(false);
+  });
+
+  it("should hide the changes icon if the values are the same", () => {
+    const wrapper = mount(
+      <DifferentialTab
+        deploymentEvent="install"
+        deployedValues=""
+        defaultValues="foo"
+        appValues="foo"
+      />,
+    );
+    expect(wrapper.find(CdsIcon).prop("hidden")).toBe(true);
+  });
+
+  it("clicking the tab removes the icon", () => {
+    const wrapper = mount(
+      <DifferentialTab
+        deploymentEvent="install"
+        deployedValues=""
+        defaultValues="foo"
+        appValues="bar"
+      />,
+    );
+    expect(wrapper.find(CdsIcon).prop("hidden")).toBe(false);
+    act(() => {
+      wrapper.find("div").simulate("click");
+    });
+    wrapper.update();
+    expect(wrapper.find(CdsIcon).prop("hidden")).toBe(true);
+  });
+
+  it("setting default values removes the icon", () => {
+    const wrapper = mount(
+      <DifferentialTab
+        deploymentEvent="install"
+        deployedValues=""
+        defaultValues="foo"
+        appValues="bar"
+      />,
+    );
+    expect(wrapper.find(CdsIcon).prop("hidden")).toBe(false);
+    act(() => {
+      wrapper.setProps({ appValues: "foo" });
+    });
+    wrapper.update();
+    expect(wrapper.find(CdsIcon).prop("hidden")).toBe(true);
+  });
+});
+
+describe("when upgrading", () => {
+  it("should show the changes icon if the values change", () => {
+    const wrapper = mount(
+      <DifferentialTab
+        deploymentEvent="upgrade"
+        deployedValues="foo"
+        defaultValues="foo"
+        appValues="bar"
+      />,
+    );
+    expect(wrapper.find(CdsIcon).prop("hidden")).toBe(false);
+  });
+
+  it("should hide the changes icon if the values are the same", () => {
+    const wrapper = mount(
+      <DifferentialTab
+        deploymentEvent="upgrade"
+        deployedValues="foo"
+        defaultValues="foo"
+        appValues="foo"
+      />,
+    );
+    expect(wrapper.find(CdsIcon).prop("hidden")).toBe(true);
+  });
+});
