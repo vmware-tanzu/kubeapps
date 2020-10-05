3f5b0d90cc2993265bff7105f172b924da096eff
Andres Martinez Gotor <andres@bitnami.com>
2020-10-05 12:59:10 +0200

    Fix e2e issue for Operators. Added debug info

--- integration/jest.environment.js
+++ integration/jest.environment.js
@@ -8,7 +8,7 @@ const {
   retryAttempts,
   endpoint,
   waitTimeout,
-  screenshotsFolder
+  screenshotsFolder,
 } = require("./args");
 
 // Create an environment to store a screenshot of the page if the current test
@@ -30,12 +30,13 @@ class ScreenshotOnFailureEnvironment extends PuppeteerEnvironment {
     try {
       // Check the server is up before running the test suite
       console.log(
-        `Waiting ${endpoint} to be ready before running the tests (${waitTimeout /
-          1000}s)`
+        `Waiting ${endpoint} to be ready before running the tests (${
+          waitTimeout / 1000
+        }s)`
       );
       await waitOn({
         resources: [endpoint],
-        timeout: waitTimeout
+        timeout: waitTimeout,
       });
       console.log(`${endpoint} is ready!`);
     } catch (err) {
@@ -52,9 +53,33 @@ class ScreenshotOnFailureEnvironment extends PuppeteerEnvironment {
     await this.global.page.setViewport({
       width: 1200,
       height: 780,
-      deviceScaleFactor: 1
+      deviceScaleFactor: 1,
     });
     await this.global.page.setDefaultTimeout(8000);
+    this.global.page
+      .on("console", async (message) => {
+        if (message.type() === "error") {
+          console.error(`${message.type().toUpperCase()} ${message.text()}`);
+          // Code from https://github.com/puppeteer/puppeteer/issues/3397#issuecomment-429325514
+          const args = await message.args();
+          args.forEach(async (arg) => {
+            const val = await arg.jsonValue();
+            // value is serializable
+            if (JSON.stringify(val) !== JSON.stringify({})) console.log(val);
+            // value is unserializable (or an empty oject)
+            else {
+              const { type, subtype, description } = arg._remoteObject;
+              console.log(
+                `type: ${type}, subtype: ${subtype}, description:\n ${description}`
+              );
+            }
+          });
+        }
+      })
+      .on("pageerror", ({ message }) => console.log(message))
+      .on("requestfailed", (request) =>
+        console.log(`${request.failure().errorText} ${request.url()}`)
+      );
   }
 
   async teardown() {
@@ -72,7 +97,7 @@ class ScreenshotOnFailureEnvironment extends PuppeteerEnvironment {
           .replace(/ /g, "-");
         // Take a screenshot at the point of failure
         await this.global.page.screenshot({
-          path: path.join(__dirname, `${screenshotsFolder}/${testName}.png`)
+          path: path.join(__dirname, `${screenshotsFolder}/${testName}.png`),
         });
       }
     }
