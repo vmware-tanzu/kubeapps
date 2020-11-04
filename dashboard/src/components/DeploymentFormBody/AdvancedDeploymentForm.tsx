47d6922f8c43f6533eb33930cbe396080ef53998
Antonio Gamez Diaz <agamez@vmware.com>
2020-11-04 11:15:19 +0100

    Add search (crtl+F) in ACE editors
    Crtl+F will now trigger a searchbox to appear.

--- dashboard/src/components/DeploymentFormBody/AdvancedDeploymentForm.tsx
+++ dashboard/src/components/DeploymentFormBody/AdvancedDeploymentForm.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import AceEditor from "react-ace";
 
+import "ace-builds/src-noconflict/ext-searchbox";
 import "ace-builds/src-noconflict/mode-yaml";
 import "ace-builds/src-noconflict/theme-xcode";
 
