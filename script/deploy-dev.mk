52ccfc695c028b93b0eca4a9e5b5036b903f243c
Antonio Gamez Diaz <agamez@vmware.com>
2020-11-25 16:40:40 +0100

    Update devel mk files

--- script/deploy-dev.mk
+++ script/deploy-dev.mk
@@ -5,32 +5,35 @@
 # that has been setup with OIDC support (see ./cluster-kind.mk)
 
 deploy-dex: devel/dex.crt devel/dex.key
-	kubectl create namespace dex
-	kubectl -n dex create secret tls dex-web-server-tls \
+	kubectl --kubeconfig=${CLUSTER_CONFIG} create namespace dex || true
+	kubectl --kubeconfig=${CLUSTER_CONFIG} -n dex create secret tls dex-web-server-tls \
 		--key ./devel/dex.key \
-		--cert ./devel/dex.crt
-	helm install dex stable/dex --namespace dex --version 2.13.0 \
-		--values ./docs/user/manifests/kubeapps-local-dev-dex-values.yaml
+		--cert ./devel/dex.crt || true
+	helm --kubeconfig=${CLUSTER_CONFIG} repo add stable https://charts.helm.sh/stable || true
+	helm --kubeconfig=${CLUSTER_CONFIG} install dex stable/dex --namespace dex  --values ./docs/user/manifests/kubeapps-local-dev-dex-values.yaml || true
 
 deploy-openldap:
-	kubectl create namespace ldap
-	helm install ldap stable/openldap --namespace ldap \
-		--values ./docs/user/manifests/kubeapps-local-dev-openldap-values.yaml
+	kubectl --kubeconfig=${CLUSTER_CONFIG} create namespace ldap || true
+	helm --kubeconfig=${CLUSTER_CONFIG} install ldap stable/openldap --namespace ldap \
+		--values ./docs/user/manifests/kubeapps-local-dev-openldap-values.yaml || true
 
+# Get mkcert from https://github.com/FiloSottile/mkcert/releases
 devel/localhost-cert.pem:
-	mkcert -key-file ./devel/localhost-key.pem -cert-file ./devel/localhost-cert.pem localhost 172.18.0.2
+	mkcert -key-file ./devel/localhost-key.pem -cert-file ./devel/localhost-cert.pem localhost 172.19.0.2 || true
 
 deploy-dependencies: deploy-dex deploy-openldap devel/localhost-cert.pem
-	kubectl create namespace kubeapps
-	kubectl -n kubeapps create secret tls localhost-tls \
+	kubectl --kubeconfig=${CLUSTER_CONFIG} create namespace kubeapps || true
+	kubectl --kubeconfig=${CLUSTER_CONFIG} -n kubeapps create secret tls localhost-tls \
 		--key ./devel/localhost-key.pem \
-		--cert ./devel/localhost-cert.pem
+		--cert ./devel/localhost-cert.pem || true
 
-deploy-dev: deploy-dependencies
-	helm install kubeapps ./chart/kubeapps --namespace kubeapps \
+deploy-dev-kubeapps: 
+	helm --kubeconfig=${CLUSTER_CONFIG} install kubeapps ./chart/kubeapps --namespace kubeapps --create-namespace \
 		--values ./docs/user/manifests/kubeapps-local-dev-values.yaml \
 		--values ./docs/user/manifests/kubeapps-local-dev-auth-proxy-values.yaml \
-		--values ./docs/user/manifests/kubeapps-local-dev-additional-kind-cluster.yaml
+		--values ./docs/user/manifests/kubeapps-local-dev-additional-kind-cluster.yaml || true
+
+deploy-dev: deploy-dependencies deploy-dev-kubeapps
 	@echo "\nYou can now simply open your browser at https://localhost/ to access Kubeapps!"
 	@echo "When logging in, you will be redirected to dex (with a self-signed cert) and can login with email as either of"
 	@echo "  kubeapps-operator@example.com:password"
@@ -40,10 +43,16 @@ deploy-dev: deploy-dependencies
 	@echo "  kubeapps-user-ldap@example.org:password"
 	@echo "to authenticate with the corresponding permissions."
 
+reset-dev-kubeapps:
+	helm --kubeconfig=${CLUSTER_CONFIG} -n kubeapps delete kubeapps || true
+	kubectl -kubeconfig=${CLUSTER_CONFIG} delete jobs -n kubeapps --all || true
+	kubectl delete namespace --wait kubeapps || true
+
 reset-dev:
-	helm -n kubeapps delete kubeapps || true
-	helm -n dex delete dex || true
-	helm -n ldap delete ldap || true
+	helm --kubeconfig=${CLUSTER_CONFIG} -n kubeapps delete kubeapps || true
+	kubectl -kubeconfig=${CLUSTER_CONFIG} delete jobs -n kubeapps --all || true
+	helm --kubeconfig=${CLUSTER_CONFIG} -n dex delete dex || true
+	helm --kubeconfig=${CLUSTER_CONFIG} -n ldap delete ldap || true
 	kubectl delete namespace --wait dex ldap kubeapps || true
 
-.PHONY: deploy-dex deploy-dependencies deploy-dev deploy-openldap reset-dev update-apiserver-etc-hosts
+.PHONY: deploy-dex deploy-dependencies deploy-dev deploy-openldap reset-dev
