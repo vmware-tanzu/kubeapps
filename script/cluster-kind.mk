05d44fc853306cd412d69baf9179ced39cfd9c9b
Antonio Gamez Diaz <agamez@vmware.com>
2020-11-26 17:32:45 +0100

    Remove '|| true'

--- script/cluster-kind.mk
+++ script/cluster-kind.mk
@@ -15,15 +15,15 @@ ${CLUSTER_CONFIG}:
 		--name ${CLUSTER_NAME} \
 		--config=./docs/user/manifests/kubeapps-local-dev-apiserver-config.yaml \
 		--retain \
-		--wait 10s || true
-	kubectl apply --kubeconfig=${CLUSTER_CONFIG} -f ./docs/user/manifests/kubeapps-local-dev-users-rbac.yaml || true
-	kubectl apply --kubeconfig=${CLUSTER_CONFIG} -f ./docs/user/manifests/ingress-nginx-kind-with-large-proxy-buffers.yaml || true
+		--wait 10s
+	kubectl apply --kubeconfig=${CLUSTER_CONFIG} -f ./docs/user/manifests/kubeapps-local-dev-users-rbac.yaml
+	kubectl apply --kubeconfig=${CLUSTER_CONFIG} -f ./docs/user/manifests/ingress-nginx-kind-with-large-proxy-buffers.yaml
 	# TODO: need to add wait for condition=exists or similar - https://github.com/kubernetes/kubernetes/issues/83242
 	sleep 5
 	kubectl wait --kubeconfig=${CLUSTER_CONFIG} --namespace ingress-nginx \
 		--for=condition=ready pod \
 		--selector=app.kubernetes.io/component=controller \
-		--timeout=120s || true
+		--timeout=120s
 
 cluster-kind: ${CLUSTER_CONFIG}
 
@@ -31,10 +31,10 @@ cluster-kind: ${CLUSTER_CONFIG}
 # reuse the key and cert from the apiserver, which already includes v3 extensions
 # for the correct alternative name (using the IP address).
 devel/dex.crt:
-	docker cp kubeapps-control-plane:/etc/kubernetes/pki/apiserver.crt ./devel/dex.crt || true
+	docker cp kubeapps-control-plane:/etc/kubernetes/pki/apiserver.crt ./devel/dex.crt
 
 devel/dex.key:
-	docker cp kubeapps-control-plane:/etc/kubernetes/pki/apiserver.key ./devel/dex.key || true
+	docker cp kubeapps-control-plane:/etc/kubernetes/pki/apiserver.key ./devel/dex.key
 
 ${ADDITIONAL_CLUSTER_CONFIG}: devel/dex.crt
 	kind create cluster \
@@ -42,9 +42,9 @@ ${ADDITIONAL_CLUSTER_CONFIG}: devel/dex.crt
 		--name ${ADDITIONAL_CLUSTER_NAME} \
 		--config=./docs/user/manifests/kubeapps-local-dev-additional-apiserver-config.yaml \
 		--retain \
-		--wait 10s || true
-	kubectl apply --kubeconfig=${ADDITIONAL_CLUSTER_CONFIG} -f ./docs/user/manifests/kubeapps-local-dev-users-rbac.yaml || true
-	kubectl apply --kubeconfig=${ADDITIONAL_CLUSTER_CONFIG} -f ./docs/user/manifests/kubeapps-local-dev-namespace-discovery-rbac.yaml || true
+		--wait 10s
+	kubectl apply --kubeconfig=${ADDITIONAL_CLUSTER_CONFIG} -f ./docs/user/manifests/kubeapps-local-dev-users-rbac.yaml
+	kubectl apply --kubeconfig=${ADDITIONAL_CLUSTER_CONFIG} -f ./docs/user/manifests/kubeapps-local-dev-namespace-discovery-rbac.yaml
 
 additional-cluster-kind: ${ADDITIONAL_CLUSTER_CONFIG}
 
--- script/deploy-dev.mk
+++ script/deploy-dev.mk
@@ -5,33 +5,34 @@
 # that has been setup with OIDC support (see ./cluster-kind.mk)
 
 deploy-dex: devel/dex.crt devel/dex.key
-	kubectl --kubeconfig=${CLUSTER_CONFIG} create namespace dex || true
+	kubectl --kubeconfig=${CLUSTER_CONFIG} create namespace dex
 	kubectl --kubeconfig=${CLUSTER_CONFIG} -n dex create secret tls dex-web-server-tls \
 		--key ./devel/dex.key \
-		--cert ./devel/dex.crt || true
-	helm --kubeconfig=${CLUSTER_CONFIG} repo add stable https://charts.helm.sh/stable || true
-	helm --kubeconfig=${CLUSTER_CONFIG} install dex stable/dex --namespace dex  --values ./docs/user/manifests/kubeapps-local-dev-dex-values.yaml || true
+		--cert ./devel/dex.crt
+	helm --kubeconfig=${CLUSTER_CONFIG} repo add stable https://charts.helm.sh/stable
+	helm --kubeconfig=${CLUSTER_CONFIG} install dex stable/dex --namespace dex  --values ./docs/user/manifests/kubeapps-local-dev-dex-values.yaml
 
 deploy-openldap:
-	kubectl --kubeconfig=${CLUSTER_CONFIG} create namespace ldap || true
+	kubectl --kubeconfig=${CLUSTER_CONFIG} create namespace ldap
+	helm --kubeconfig=${CLUSTER_CONFIG} repo add stable https://charts.helm.sh/stable
 	helm --kubeconfig=${CLUSTER_CONFIG} install ldap stable/openldap --namespace ldap \
-		--values ./docs/user/manifests/kubeapps-local-dev-openldap-values.yaml || true
+		--values ./docs/user/manifests/kubeapps-local-dev-openldap-values.yaml
 
 # Get mkcert from https://github.com/FiloSottile/mkcert/releases
 devel/localhost-cert.pem:
-	mkcert -key-file ./devel/localhost-key.pem -cert-file ./devel/localhost-cert.pem localhost 172.19.0.2 || true
+	mkcert -key-file ./devel/localhost-key.pem -cert-file ./devel/localhost-cert.pem localhost 172.19.0.2
 
 deploy-dependencies: deploy-dex deploy-openldap devel/localhost-cert.pem
-	kubectl --kubeconfig=${CLUSTER_CONFIG} create namespace kubeapps || true
+	kubectl --kubeconfig=${CLUSTER_CONFIG} create namespace kubeapps
 	kubectl --kubeconfig=${CLUSTER_CONFIG} -n kubeapps create secret tls localhost-tls \
 		--key ./devel/localhost-key.pem \
-		--cert ./devel/localhost-cert.pem || true
+		--cert ./devel/localhost-cert.pem
 
 deploy-dev-kubeapps: 
 	helm --kubeconfig=${CLUSTER_CONFIG} install kubeapps ./chart/kubeapps --namespace kubeapps --create-namespace \
 		--values ./docs/user/manifests/kubeapps-local-dev-values.yaml \
 		--values ./docs/user/manifests/kubeapps-local-dev-auth-proxy-values.yaml \
-		--values ./docs/user/manifests/kubeapps-local-dev-additional-kind-cluster.yaml || true
+		--values ./docs/user/manifests/kubeapps-local-dev-additional-kind-cluster.yaml
 
 deploy-dev: deploy-dependencies deploy-dev-kubeapps
 	@echo "\nYou can now simply open your browser at https://localhost/ to access Kubeapps!"
@@ -44,14 +45,14 @@ deploy-dev: deploy-dependencies deploy-dev-kubeapps
 	@echo "to authenticate with the corresponding permissions."
 
 reset-dev-kubeapps:
-	helm --kubeconfig=${CLUSTER_CONFIG} -n kubeapps delete kubeapps || true
-	kubectl -kubeconfig=${CLUSTER_CONFIG} delete jobs -n kubeapps --all || true
-	kubectl delete namespace --wait kubeapps || true
+	helm --kubeconfig=${CLUSTER_CONFIG} -n kubeapps delete kubeapps
+	kubectl -kubeconfig=${CLUSTER_CONFIG} delete jobs -n kubeapps --all
+	kubectl delete namespace --wait kubeapps
 
 reset-dev:
-	helm --kubeconfig=${CLUSTER_CONFIG} -n kubeapps delete kubeapps || true
+	helm --kubeconfig=${CLUSTER_CONFIG} -n kubeapps delete kubeapps  || true
 	kubectl -kubeconfig=${CLUSTER_CONFIG} delete jobs -n kubeapps --all || true
-	helm --kubeconfig=${CLUSTER_CONFIG} -n dex delete dex || true
+	helm --kubeconfig=${CLUSTER_CONFIG} -n dex delete dex  || true
 	helm --kubeconfig=${CLUSTER_CONFIG} -n ldap delete ldap || true
 	kubectl delete namespace --wait dex ldap kubeapps || true
 
