01621c5bc7d420501630f8bc457db91731a25159
Michael Nelson <mnelson@bitnami.com>
2020-12-01 21:56:54 +1100

    Add logging helper.

--- cmd/pinniped-proxy/src/service.rs
+++ cmd/pinniped-proxy/src/service.rs
@@ -1,7 +1,17 @@
 use std::convert::Infallible;
 
 use hyper::{Body, Request, Response};
+use log::info;
 
-pub async fn proxy(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("pinniped-proxy stub\n")))
+use crate::logging;
+
+pub async fn proxy(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let log_data = logging::request_log_data(&req);
+
+    // TODO: actual proxying to happen here.
+    let response = Response::new(Body::from("pinniped-proxy stub\n"));
+
+    info!("{}", logging::response_log_data(&response, log_data));
+
+    Ok(response)
 }
