17c2ee3f6863d4f3e7167194f5fe1f34b077ae7a
Michael Nelson <mnelson@bitnami.com>
2020-12-03 15:43:50 +1100

    Add actual proxying (without credential exchange yet)

--- cmd/pinniped-proxy/src/service.rs
+++ cmd/pinniped-proxy/src/service.rs
@@ -2,34 +2,65 @@ use std::convert::Infallible;
 
 use anyhow::Error;
 use hyper::{Body, Request, Response, StatusCode};
-use log::info;
+use log::{error, info};
+use native_tls::TlsConnector;
 
 use crate::logging;
 use crate::https;
 
-pub async fn proxy(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+pub async fn proxy(mut req: Request<Body>) -> Result<Response<Body>, Infallible> {
+
+    let k8s_api_server_url = match https::get_api_server_url(req.headers()) {
+        Ok(u) => u,
+        Err(e) => return handle_error(e, logging::request_log_data(&req)),
+    };
+    req = https::rewrite_request(req, k8s_api_server_url).unwrap();
     let log_data = logging::request_log_data(&req);
 
-    let _ = match https::get_api_server_url(req.headers()) {
-        Ok(u) => u,
-        Err(e) => return handle_error(e, log_data),
-    };
     // TODO: don't call this if we're using https://kubernetes.local, instead
     // grab the data from the file system.
     let cert_auth_data = match https::get_api_server_cert_auth_data(req.headers()) {
         Ok(c) => c,
         Err(e) => return handle_error(e, log_data),
     };
-    let _ = match https::cert_for_cert_data(cert_auth_data) {
+    let k8s_api_cert = match https::cert_for_cert_data(cert_auth_data) {
         Ok(c) => c,
         Err(e) => return handle_error(e, log_data),
     };
-    // TODO: actual proxying to happen here.
-    let response = Response::new(Body::from("pinniped-proxy stub\n"));
 
-    info!("{}", logging::response_log_data(&response, log_data));
+    // Create an https client with which to proxy the request.
+    // We need to construct the TlsConnector for each request so that we can set
+    // the client cert. It'd be nice if we could do the construction once and just
+    // clone to add the client cert?
+    let mut tls_builder = &mut TlsConnector::builder();
+    tls_builder = tls_builder.add_root_certificate(k8s_api_cert.clone());
 
-    Ok(response)
+    // TODO: Call credential exchange fn to set the client auth certs.
+
+    let client = match https::make_https_client(tls_builder) {
+        Ok(c) => c,
+        Err(e) => {
+            error!("{:#?}", e);
+            let response = Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body(Body::from(e.to_string())).unwrap();
+            return Ok(response);
+        },
+    };
+
+    match client.request(req).await {
+        Ok(r) => {
+            info!("{}", logging::response_log_data(&r, log_data));
+            Ok(r)
+        },
+        Err(e) => {
+            error!("{:#?}", e);
+            let response = Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body(Body::from(e.to_string())).unwrap();
+            Ok(response)
+        },
+    }
 }
 
 /// handle_error converts an error into a BAD_REQUEST response.
