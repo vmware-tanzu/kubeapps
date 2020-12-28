316c901cb2767792f416db2bbf8f7843cebbb8bb
Antonio Gamez Diaz <agamez@vmware.com>
2020-12-28 16:13:09 +0100

    Add generateWhereClause unit test

--- cmd/assetsvc/postgresql_utils_test.go
+++ cmd/assetsvc/postgresql_utils_test.go
@@ -433,3 +433,236 @@ func Test_getPaginatedChartList(t *testing.T) {
 		})
 	}
 }
+
+func Test_generateWhereClause(t *testing.T) {
+	tests := []struct {
+		name           string
+		namespace      string
+		chartName      string
+		version        string
+		appVersion     string
+		repos          []string
+		categories     []string
+		query          string
+		expectedClause string
+		expectedParams []interface{}
+	}{
+		{
+			name:           "returns where clause - no params",
+			namespace:      "",
+			chartName:      "",
+			version:        "",
+			appVersion:     "",
+			repos:          []string{""},
+			categories:     []string{""},
+			query:          "",
+			expectedClause: "WHERE (repo_namespace = $1 OR repo_namespace = $2)",
+			expectedParams: []interface{}{string(""), string("kubeapps")},
+		},
+		{
+			name:           "returns where clause - single param - namespace",
+			namespace:      "my-ns",
+			chartName:      "",
+			version:        "",
+			appVersion:     "",
+			repos:          []string{""},
+			categories:     []string{""},
+			query:          "",
+			expectedClause: "WHERE (repo_namespace = $1 OR repo_namespace = $2)",
+			expectedParams: []interface{}{string("my-ns"), string("kubeapps")},
+		},
+		{
+			name:           "returns where clause - single param - name",
+			namespace:      "",
+			chartName:      "my-chart",
+			version:        "",
+			appVersion:     "",
+			repos:          []string{""},
+			categories:     []string{""},
+			query:          "",
+			expectedClause: "WHERE (repo_namespace = $1 OR repo_namespace = $2) AND (info->>'name' = $3)",
+			expectedParams: []interface{}{string(""), string("kubeapps"), string("my-chart")},
+		},
+		{
+			name:           "returns where clause - single param - version",
+			namespace:      "",
+			chartName:      "",
+			version:        "1.0.0",
+			appVersion:     "",
+			repos:          []string{""},
+			categories:     []string{""},
+			query:          "",
+			expectedClause: "WHERE (repo_namespace = $1 OR repo_namespace = $2)", //needs both version and appVersion
+			expectedParams: []interface{}{string(""), string("kubeapps")},
+		},
+		{
+			name:           "returns where clause - single param - appVersion",
+			namespace:      "",
+			chartName:      "",
+			version:        "",
+			appVersion:     "0.1.0",
+			repos:          []string{""},
+			categories:     []string{""},
+			query:          "",
+			expectedClause: "WHERE (repo_namespace = $1 OR repo_namespace = $2)", //needs both version and appVersion
+			expectedParams: []interface{}{string(""), string("kubeapps")},
+		},
+		{
+			name:           "returns where clause - single param - version AND appVersion",
+			namespace:      "",
+			chartName:      "",
+			version:        "1.0.0",
+			appVersion:     "0.1.0",
+			repos:          []string{""},
+			categories:     []string{""},
+			query:          "",
+			expectedClause: `WHERE (repo_namespace = $1 OR repo_namespace = $2) AND (info->'chartVersions' @> '[{"version":"1.0.0","app_version":"0.1.0"}]'::jsonb)`,
+			expectedParams: []interface{}{string(""), string("kubeapps")},
+		},
+		{
+			name:           "returns where clause - no params",
+			namespace:      "",
+			chartName:      "",
+			version:        "",
+			appVersion:     "",
+			repos:          []string{""},
+			categories:     []string{""},
+			query:          "",
+			expectedClause: "WHERE (repo_namespace = $1 OR repo_namespace = $2)",
+			expectedParams: []interface{}{string(""), string("kubeapps")},
+		},
+		{
+			name:           "returns where clause - single param - single repo",
+			namespace:      "",
+			chartName:      "",
+			version:        "",
+			appVersion:     "",
+			repos:          []string{"my-repo1"},
+			categories:     []string{""},
+			query:          "",
+			expectedClause: `WHERE (repo_namespace = $1 OR repo_namespace = $2) AND ((repo_name = $3))`,
+			expectedParams: []interface{}{string(""), string("kubeapps"), string("my-repo1")},
+		},
+		{
+			name:           "returns where clause - single param - multiple repos",
+			namespace:      "",
+			chartName:      "",
+			version:        "",
+			appVersion:     "",
+			repos:          []string{"my-repo1", "my-repo2"},
+			categories:     []string{""},
+			query:          "",
+			expectedClause: `WHERE (repo_namespace = $1 OR repo_namespace = $2) AND ((repo_name = $3) OR (repo_name = $4))`,
+			expectedParams: []interface{}{string(""), string("kubeapps"), string("my-repo1"), string("my-repo2")},
+		},
+		{
+			name:           "returns where clause - single param - single category",
+			namespace:      "",
+			chartName:      "",
+			version:        "",
+			appVersion:     "",
+			repos:          []string{""},
+			categories:     []string{"my-category1"},
+			query:          "",
+			expectedClause: `WHERE (repo_namespace = $1 OR repo_namespace = $2) AND (info->>'category' = $3)`,
+			expectedParams: []interface{}{string(""), string("kubeapps"), string("my-category1")},
+		},
+		{
+			name:           "returns where clause - single param - multiple categories",
+			namespace:      "",
+			chartName:      "",
+			version:        "",
+			appVersion:     "",
+			repos:          []string{""},
+			categories:     []string{"my-category1", "my-category2"},
+			query:          "",
+			expectedClause: `WHERE (repo_namespace = $1 OR repo_namespace = $2) AND (info->>'category' = $3 OR info->>'category' = $4)`,
+			expectedParams: []interface{}{string(""), string("kubeapps"), string("my-category1"), string("my-category2")},
+		},
+		{
+			name:           "returns where clause - single param - query (one word)",
+			namespace:      "",
+			chartName:      "",
+			version:        "",
+			appVersion:     "",
+			repos:          []string{""},
+			categories:     []string{""},
+			query:          "chart",
+			expectedClause: `WHERE (repo_namespace = $1 OR repo_namespace = $2) AND ((info ->> 'name' ILIKE $3) OR (info ->> 'description' ILIKE $3) OR (info -> 'repo' ->> 'name' ILIKE $3) OR (info ->> 'keywords' ILIKE $3) OR (info ->> 'sources' ILIKE $3) OR (info -> 'maintainers' ->> 'name' ILIKE $3))`,
+			expectedParams: []interface{}{string(""), string("kubeapps"), string("%chart%")},
+		},
+		{
+			name:           "returns where clause - single param - query (two words)",
+			namespace:      "",
+			chartName:      "",
+			version:        "",
+			appVersion:     "",
+			repos:          []string{""},
+			categories:     []string{""},
+			query:          "my chart",
+			expectedClause: `WHERE (repo_namespace = $1 OR repo_namespace = $2) AND ((info ->> 'name' ILIKE $3) OR (info ->> 'description' ILIKE $3) OR (info -> 'repo' ->> 'name' ILIKE $3) OR (info ->> 'keywords' ILIKE $3) OR (info ->> 'sources' ILIKE $3) OR (info -> 'maintainers' ->> 'name' ILIKE $3))`,
+			expectedParams: []interface{}{string(""), string("kubeapps"), string("%my chart%")},
+		},
+		{
+			name:           "returns where clause - single param - query (with slash)",
+			namespace:      "",
+			chartName:      "",
+			version:        "",
+			appVersion:     "",
+			repos:          []string{""},
+			categories:     []string{""},
+			query:          "my/chart",
+			expectedClause: `WHERE (repo_namespace = $1 OR repo_namespace = $2) AND ((info ->> 'name' ILIKE $3) OR (info ->> 'description' ILIKE $3) OR (info -> 'repo' ->> 'name' ILIKE $3) OR (info ->> 'keywords' ILIKE $3) OR (info ->> 'sources' ILIKE $3) OR (info -> 'maintainers' ->> 'name' ILIKE $3))`,
+			expectedParams: []interface{}{string(""), string("kubeapps"), string("%my/chart%")},
+		},
+		{
+			name:           "returns where clause - single param - query (encoded)",
+			namespace:      "",
+			chartName:      "",
+			version:        "",
+			appVersion:     "",
+			repos:          []string{""},
+			categories:     []string{""},
+			query:          "my%2Fchart",
+			expectedClause: `WHERE (repo_namespace = $1 OR repo_namespace = $2) AND ((info ->> 'name' ILIKE $3) OR (info ->> 'description' ILIKE $3) OR (info -> 'repo' ->> 'name' ILIKE $3) OR (info ->> 'keywords' ILIKE $3) OR (info ->> 'sources' ILIKE $3) OR (info -> 'maintainers' ->> 'name' ILIKE $3))`,
+			expectedParams: []interface{}{string(""), string("kubeapps"), string("%my%2Fchart%")},
+		},
+		{
+			name:           "returns where clause - every param",
+			namespace:      "my-ns",
+			chartName:      "my-chart",
+			version:        "1.0.0",
+			appVersion:     "0.1.0",
+			repos:          []string{"my-repo1", "my-repo2"},
+			categories:     []string{"my-category1", "my-category2"},
+			query:          "best chart",
+			expectedClause: `WHERE (repo_namespace = $1 OR repo_namespace = $2) AND (info->>'name' = $3) AND (info->'chartVersions' @> '[{"version":"1.0.0","app_version":"0.1.0"}]'::jsonb) AND ((repo_name = $4) OR (repo_name = $5)) AND (info->>'category' = $6 OR info->>'category' = $7) AND ((info ->> 'name' ILIKE $8) OR (info ->> 'description' ILIKE $8) OR (info -> 'repo' ->> 'name' ILIKE $8) OR (info ->> 'keywords' ILIKE $8) OR (info ->> 'sources' ILIKE $8) OR (info -> 'maintainers' ->> 'name' ILIKE $8))`,
+			expectedParams: []interface{}{string("my-ns"), string("kubeapps"), string("my-chart"), string("my-repo1"), string("my-repo2"), string("my-category1"), string("my-category2"), string("%best chart%")},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pgManager, _, cleanup := getMockManager(t)
+			defer cleanup()
+
+			cq := ChartQuery{
+				namespace:   tt.namespace,
+				chartName:   tt.chartName,
+				version:     tt.version,
+				appVersion:  tt.appVersion,
+				searchQuery: tt.query,
+				repos:       tt.repos,
+				categories:  tt.categories,
+			}
+			whereQuery, whereQueryParams := pgManager.generateWhereClause(cq)
+
+			if tt.expectedClause != whereQuery {
+				t.Errorf("Expecting query:\n'%s'\nreceived query:\n'%s'\nin '%s'", tt.expectedClause, whereQuery, tt.name)
+			}
+
+			if !cmp.Equal(tt.expectedParams, whereQueryParams) {
+				t.Errorf("Param mismatch in '%s': %s", tt.name, cmp.Diff(tt.expectedParams, whereQueryParams))
+			}
+		})
+	}
+}
