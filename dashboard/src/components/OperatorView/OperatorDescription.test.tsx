c37ece36e5d2ba0d8a1cf33931026dfdd058c15c
Antonio Gamez Diaz <agamez@vmware.com>
2021-02-02 17:32:09 +0100

    Fix problems when bumping up react-markdown version. Update deprecated .SFC by .FunctionComponent
    Now it decouples github flavor from the main package,  more at https://github.com/remarkjs/react-markdown/blob/main/changelog.md 81b2e3

--- dashboard/src/components/OperatorView/OperatorDescription.test.tsx
+++ dashboard/src/components/OperatorView/OperatorDescription.test.tsx
@@ -1,6 +1,7 @@
 import { shallow } from "enzyme";
 import * as React from "react";
 import ReactMarkdown from "react-markdown";
+import { defaultStore, mountWrapper } from "shared/specs/mountWrapper";
 import OperatorDescription from "./OperatorDescription";
 
 it("renders a description", () => {
@@ -8,4 +9,36 @@ it("renders a description", () => {
   const markdown = wrapper.find(ReactMarkdown);
   expect(markdown).toExist();
   expect(markdown.html()).toContain('<h1 id="title">Title!</h1>');
+});
+
+it("renders the ReactMarkdown content with github flavored markdown (table)", () => {
+  const props = {
+    description: "|h1|h2|\n|-|-|\n|foo|bar|",
+  };
+  const wrapper = mountWrapper(defaultStore, <OperatorDescription {...props} />);
+  const component = wrapper.find(ReactMarkdown);
+  expect(
+    component
+      .find("table th")
+      .first()
+      .text(),
+  ).toBe("h1");
+  expect(
+    component
+      .find("table th")
+      .last()
+      .text(),
+  ).toBe("h2");
+  expect(
+    component
+      .find("table td")
+      .first()
+      .text(),
+  ).toBe("foo");
+  expect(
+    component
+      .find("table td")
+      .last()
+      .text(),
+  ).toBe("bar");
 });
