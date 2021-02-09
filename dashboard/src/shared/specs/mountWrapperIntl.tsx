25e5913b4a15fa25607670cc74882001d560c5cd
Antonio Gamez Diaz <agamez@vmware.com>
2021-02-09 18:56:09 +0100

    Add test helper for react-intl

---
+++ dashboard/src/shared/specs/mountWrapperIntl.tsx
@@ -0,0 +1,41 @@
+/**
+ * Components using the react-intl module require access to the intl context.
+ * This is not available when mounting single components in Enzyme.
+ * These helper functions aim to address that and wrap a valid,
+ * English-locale intl context around them.
+ */
+
+import { mount, shallow } from "enzyme";
+import * as React from "react";
+import { IntlProvider } from "react-intl";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { MockStore } from "redux-mock-store";
+import I18n from "../I18n";
+
+// Default to english 1i8nconfiguration
+const messages = I18n.getDefaulI18nConfig().messages;
+const locale = I18n.getDefaulI18nConfig().locale;
+
+export const mountIntl = (node: React.ReactElement) =>
+  mount(
+    <IntlProvider locale={locale} key={locale} messages={messages} defaultLocale={locale}>
+      {node}
+    </IntlProvider>,
+  );
+
+export const mountWrapperIntl = (store: MockStore, children: React.ReactElement) =>
+  mount(
+    <Provider store={store}>
+      <IntlProvider locale={locale} key={locale} messages={messages} defaultLocale={locale}>
+        <Router>{children}</Router>
+      </IntlProvider>
+    </Provider>,
+  );
+
+export const shallowIntl = (node: React.ReactElement) =>
+  shallow(
+    <IntlProvider locale={locale} key={locale} messages={messages} defaultLocale={locale}>
+      {node}
+    </IntlProvider>,
+  );
