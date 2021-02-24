6089714a26ca790380a6cf2f5a72ef8f8f51c8b0
Antonio Gamez Diaz <agamez@vmware.com>
2021-02-24 17:18:26 +0100

    Add tests

--- dashboard/src/components/Header/Menu.test.tsx
+++ dashboard/src/components/Header/Menu.test.tsx
@@ -5,6 +5,8 @@ import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
 
 import { CdsButton } from "@cds/react/button";
+import { SupportedThemes } from "components/ThemeSelector/ThemeSelector";
+import { act } from "react-dom/test-utils";
 import { BrowserRouter, Link } from "react-router-dom";
 import { IClustersState } from "reducers/cluster";
 import Menu from "./Menu";
@@ -58,4 +60,55 @@ it("logs out", () => {
   // Simulate doesn't work with CdsButtons
   (logoutButton.prop("onClick") as any)();
   expect(logout).toHaveBeenCalled();
+});
+
+describe("theme switcher toggle", () => {
+  it("toggle not checked by default", () => {
+    const wrapper = mount(
+      <Provider store={defaultStore}>
+        <BrowserRouter>
+          <Menu {...defaultProps} />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const toggle = wrapper.find("cds-toggle input");
+    expect(toggle.prop("checked")).toBe(false);
+  });
+
+  it("toggle checked if dark theme is configured", () => {
+    localStorage.setItem("theme", SupportedThemes.dark);
+    const wrapper = mount(
+      <Provider store={defaultStore}>
+        <BrowserRouter>
+          <Menu {...defaultProps} />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const toggle = wrapper.find("cds-toggle input");
+    expect(toggle.prop("checked")).toBe(true);
+  });
+
+  it("toggle reloads page after changing theme", () => {
+    localStorage.setItem("theme", SupportedThemes.dark);
+    const wrapper = mount(
+      <Provider store={defaultStore}>
+        <BrowserRouter>
+          <Menu {...defaultProps} />
+        </BrowserRouter>
+      </Provider>,
+    );
+    // After the JSDOM upgrade, window.xxx are read-only properties
+    // https://github.com/facebook/jest/issues/9471
+    Object.defineProperty(window, "location", {
+      configurable: true,
+      writable: true,
+      value: { reload: jest.fn() },
+    });
+    const toggle = wrapper.find("cds-toggle input");
+    act(() => {
+      (toggle.prop("onChange") as any)();
+    });
+    wrapper.update();
+    expect(window.location.reload).toHaveBeenCalled();
+  });
 });
--- dashboard/src/containers/Root.test.tsx
+++ dashboard/src/containers/Root.test.tsx
@@ -1,29 +1,47 @@
+import ThemeSelector, { SupportedThemes } from "components/ThemeSelector/ThemeSelector";
 import { shallow } from "enzyme";
 import { act } from "react-dom/test-utils";
 import { IntlProvider } from "react-intl";
 import I18n, { II18nConfig } from "shared/I18n";
 import Root from "./Root";
 
-it("renders the root componet", () => {
+it("renders the root component", () => {
   const wrapper = shallow(<Root />);
   expect(wrapper).toExist();
 });
 
-it("loads the initial i18n config from getDefaulI18nConfig", async () => {
-  const config: II18nConfig = { locale: "custom", messages: { messageId: "translation" } };
-  const getDefaultConfig = jest.spyOn(I18n, "getDefaultConfig").mockReturnValue(config);
-  act(() => {
-    shallow(<Root />);
+describe("118n configuration", () => {
+  it("loads the initial i18n config from getDefaulI18nConfig", async () => {
+    const config: II18nConfig = { locale: "custom", messages: { messageId: "translation" } };
+    const getDefaultConfig = jest.spyOn(I18n, "getDefaultConfig").mockReturnValue(config);
+    act(() => {
+      shallow(<Root />);
+    });
+    expect(getDefaultConfig).toHaveBeenCalled();
   });
-  expect(getDefaultConfig).toHaveBeenCalled();
+
+  it("loads the async i18n config from getCustomI18nConfig", async () => {
+    const config: II18nConfig = { locale: "custom", messages: { messageId: "translation" } };
+    I18n.getCustomConfig = jest.fn().mockReturnValue({ then: jest.fn((f: any) => f(config)) });
+    act(() => {
+      const wrapper = shallow(<Root />);
+      expect(wrapper.find(IntlProvider).prop("locale")).toBe("custom");
+      expect(wrapper.find(IntlProvider).prop("messages")).toStrictEqual({
+        messageId: "translation",
+      });
+    });
+  });
 });
 
-it("loads the async i18n config from getCustomI18nConfig", async () => {
-  const config: II18nConfig = { locale: "custom", messages: { messageId: "translation" } };
-  I18n.getCustomConfig = jest.fn().mockReturnValue({ then: jest.fn((f: any) => f(config)) });
-  act(() => {
+describe("css initial configuration", () => {
+  it("load the default 'light' theme", () => {
     const wrapper = shallow(<Root />);
-    expect(wrapper.find(IntlProvider).prop("locale")).toBe("custom");
-    expect(wrapper.find(IntlProvider).prop("messages")).toStrictEqual({ messageId: "translation" });
+    expect(wrapper.find(ThemeSelector).prop("theme")).toBe(SupportedThemes.light);
+  });
+
+  it("load the stored theme", () => {
+    localStorage.setItem("theme", SupportedThemes.dark);
+    const wrapper = shallow(<Root />);
+    expect(wrapper.find(ThemeSelector).prop("theme")).toBe(SupportedThemes.dark);
   });
 });
