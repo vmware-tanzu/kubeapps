563e85009f8bc750dd061c3f586fb7b97ecbdcbf
Antonio Gamez Diaz <agamez@vmware.com>
2021-02-25 20:03:19 +0100

    Remove themeSelector

--- dashboard/src/components/ThemeSelector/DarkTheme.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-import "@clr/ui/clr-ui-dark.min.css";
-
-const DarkTheme: React.FC = () => <></>;
-
-export default DarkTheme;
--- dashboard/src/components/ThemeSelector/LightTheme.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-import "@clr/ui/clr-ui.min.css";
-
-const LightTheme: React.FC = () => <></>;
-
-export default LightTheme;
