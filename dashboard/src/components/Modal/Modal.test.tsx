55a1cdf3d132968d77de306f159c680c2b49cc97
Antonio Gamez Diaz <agamez@vmware.com>
2021-03-02 16:34:30 +0100

    Add modal component based on the cds-modal

--- dashboard/src/components/Modal/Modal.test.tsx
+++ dashboard/src/components/Modal/Modal.test.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-import Modal, { modalSizes } from "./Modal";
-
+import { CdsButton } from "@cds/react/button";
+import { CdsModal, CdsModalActions, CdsModalContent, CdsModalHeader } from "@cds/react/modal";
 import { mount } from "enzyme";
+import * as React from "react";
+import Modal from "./Modal";
+
+const modalSizes = ["sm", "default", "lg", "xl"];
 
 const mockCloseFunc = jest.fn();
 
@@ -14,126 +17,88 @@ describe(Modal, () => {
         <span>child</span>
       </Modal>,
     );
-
     expect(wrapper).toExist();
   });
-
   it("hides modal when showModal is set to false", () => {
     const wrapper = mount(
       <Modal showModal={false} onModalClose={mockCloseFunc}>
         <span>child</span>
       </Modal>,
     );
-
     expect(wrapper.isEmptyRender()).toBeTruthy();
   });
-
   it("projects title, body and footer", () => {
     const wrapper = mount(
       <Modal
         showModal={true}
         onModalClose={mockCloseFunc}
+        hideCloseButton={true}
         title={"Title"}
         footer={<section>Footer content</section>}
       >
         <p>Body content</p>
       </Modal>,
     );
-
-    expect(wrapper.find("section")).toHaveText("Footer content");
-    expect(wrapper.find("h3")).toHaveText("Title");
-    expect(wrapper.find("p")).toHaveText("Body content");
+    expect(wrapper.find(CdsModalHeader)).toHaveText("Title");
+    expect(wrapper.find(CdsModalContent)).toHaveText("Body content");
+    expect(wrapper.find(CdsModalActions)).toHaveText("Footer content");
   });
-
   it("matches all modal sizes", () => {
-    Object.keys(modalSizes).forEach(size => {
-      const wrapper = mount(
-        <Modal showModal={true} modalSize={size} onModalClose={mockCloseFunc}>
-          <span>child</span>
-        </Modal>,
-      );
-
-      const modalDialog = wrapper.find(".modal-dialog");
-      expect(modalDialog).toHaveClassName(modalSizes[size]);
-    });
-  });
-
-  it("hides close button when showCLoseButton set to false", () => {
-    Object.keys(modalSizes).forEach(size => {
-      const wrapper = mount(
-        <Modal showModal={true} modalSize={size} onModalClose={mockCloseFunc} hideCloseButton>
-          <span>child</span>
-        </Modal>,
-      );
-
-      const closeButton = wrapper.find(".close");
-      expect(closeButton).not.toExist();
-    });
-  });
-
-  it("shows close button when showCLoseButton set to true", () => {
-    Object.keys(modalSizes).forEach(size => {
+    Object.values(modalSizes).forEach(size => {
       const wrapper = mount(
         <Modal
           showModal={true}
-          showCloseButton={true}
-          modalSize={size}
+          modalSize={size as "sm" | "default" | "lg" | "xl"}
           onModalClose={mockCloseFunc}
         >
           <span>child</span>
         </Modal>,
       );
-
-      const closeButton = wrapper.find(".close");
+      const modalDialog = wrapper.find(CdsModal);
+      expect(modalDialog.prop("size")).toBe(size);
+    });
+  });
+  it("hides close button when hideCloseButton set to true", () => {
+    Object.keys(modalSizes).forEach(size => {
+      const wrapper = mount(
+        <Modal
+          showModal={true}
+          modalSize={size as "sm" | "default" | "lg" | "xl"}
+          onModalClose={mockCloseFunc}
+          hideCloseButton={true}
+        >
+          <span>child</span>
+        </Modal>,
+      );
+      const closeButton = wrapper.find(CdsButton);
+      expect(closeButton).not.toExist();
+    });
+  });
+  it("shows close button when hideCloseButton set to false", () => {
+    Object.keys(modalSizes).forEach(size => {
+      const wrapper = mount(
+        <Modal
+          showModal={true}
+          modalSize={size as "sm" | "default" | "lg" | "xl"}
+          onModalClose={mockCloseFunc}
+          hideCloseButton={false}
+        >
+          <span>child</span>
+        </Modal>,
+      );
+      const closeButton = wrapper.find(CdsButton);
       expect(closeButton).toExist();
     });
   });
-
   it("close modal on close button click", () => {
     const wrapper = mount(
       <Modal showModal={true} onModalClose={mockCloseFunc}>
         <span>child</span>
       </Modal>,
     );
-
-    const closeButton = wrapper.find(".close");
-
+    const closeButton = wrapper.find(CdsButton);
     closeButton.simulate("click");
-
     wrapper.update();
-
     expect(mockCloseFunc).toHaveBeenCalled();
-  });
-
-  it("close modal on backdrop click", () => {
-    const wrapper = mount(
-      <Modal showModal={true} staticBackdrop={false} onModalClose={mockCloseFunc}>
-        <span>child</span>
-      </Modal>,
-    );
-
-    const closeButton = wrapper.find(".modal-backdrop");
-
-    closeButton.simulate("click");
-
-    wrapper.update();
-
-    expect(mockCloseFunc).toHaveBeenCalled();
-  });
-
-  it("does not close modal on backdrop if staticBackdrop is set to true", () => {
-    const wrapper = mount(
-      <Modal showModal={true} onModalClose={mockCloseFunc}>
-        <span>child</span>
-      </Modal>,
-    );
-
-    const closeButton = wrapper.find(".modal-backdrop");
-
-    closeButton.simulate("click");
-
-    wrapper.update();
-
-    expect(mockCloseFunc).not.toHaveBeenCalled();
   });
 });
