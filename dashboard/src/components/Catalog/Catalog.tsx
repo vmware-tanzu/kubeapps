1482cb999add8f0de3abac7ca1f21ebfee3a1554
Antonio Gámez <agamez@vmware.com>
2021-03-04 13:44:32 +0100

    Avoid endless page requesting if when failing requests + manual retry (#2496)
    * Avoid endless page requesting if when failing requests. Add manual retry.
    
    * Refetch the same failing page instead of the next one

--- dashboard/src/components/Catalog/Catalog.tsx
+++ dashboard/src/components/Catalog/Catalog.tsx
@@ -1,5 +1,6 @@
 import { CdsButton } from "@cds/react/button";
 import { CdsIcon } from "@cds/react/icon";
+import actions from "actions";
 import FilterGroup from "components/FilterGroup/FilterGroup";
 import Alert from "components/js/Alert";
 import Column from "components/js/Column";
@@ -256,8 +257,20 @@ function Catalog(props: ICatalogProps) {
 
   // Required to have the latest value of page
   const setPageWithContext = () => {
+    if (!error) {
+      increaseRequestedPage();
+    }
+  };
+
+  const forceRetry = () => {
+    dispatch(actions.charts.clearErrorChart());
+    fetchCharts(cluster, namespace, reposFilter, page, size, searchFilter);
+  };
+
+  const increaseRequestedPage = () => {
     setPage(page + 1);
   };
+
   const observeBorder = (node: any) => {
     // Check if the IntersectionAPI is enabled
     if ("IntersectionObserver" in window && "IntersectionObserverEntry" in window) {
@@ -305,7 +318,15 @@ function Catalog(props: ICatalogProps) {
         }
       />
       {error && (
-        <Alert theme="danger">An error occurred while fetching the catalog: {error.message}</Alert>
+        <Alert theme="danger">
+          An error occurred while fetching the catalog: {error.message}.{" "}
+          {!hasFinishedFetching && (
+            <CdsButton size="sm" action="flat" onClick={forceRetry} type="button">
+              {" "}
+              Try again{" "}
+            </CdsButton>
+          )}
+        </Alert>
       )}
       {isEqual(filters, initialFilterState()) &&
       hasFinishedFetching &&
@@ -426,6 +447,12 @@ function Catalog(props: ICatalogProps) {
                         filters[filterNames.TYPE].find((type: string) => type === "Charts")) && (
                         <div className="endPageMessage">
                           <LoadingWrapper medium={true} loaded={false} />
+                          {error && !hasFinishedFetching && (
+                            <CdsButton size="sm" action="flat" onClick={forceRetry} type="button">
+                              {" "}
+                              Try again{" "}
+                            </CdsButton>
+                          )}
                         </div>
                       )}
                     {!hasFinishedFetching && !isFetching && (
