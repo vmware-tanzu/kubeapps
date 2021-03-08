188a4983c3ec8d850b53f3a8fd2401869ff6a4c9
Andres Martinez Gotor <andresmgot@vmware.com>
2021-03-08 15:15:52 +0100

    Add filter rules to AppRepository spec (#2461)

--- cmd/apprepository-controller/pkg/apis/apprepository/v1alpha1/types.go
+++ cmd/apprepository-controller/pkg/apis/apprepository/v1alpha1/types.go
@@ -47,9 +47,12 @@ type AppRepositorySpec struct {
 	DockerRegistrySecrets []string `json:"dockerRegistrySecrets,omitempty"`
 	// In case of an OCI type, the list of repositories is needed
 	// as there is no API for the index
+	// DEPRECATED: Use FilterRules instead
 	OCIRepositories []string `json:"ociRepositories,omitempty"`
 	// TLSInsecureSkipVerify skips TLS verification
 	TLSInsecureSkipVerify bool `json:"tlsInsecureSkipVerify,omitempty"`
+	// FilterRule allows to filter packages based on a JQuery
+	FilterRule FilterRuleSpec `json:"filterRules,omitempty"`
 }
 
 // AppRepositoryAuth is the auth for an AppRepository resource
@@ -68,6 +71,12 @@ type AppRepositoryAuthHeader struct {
 type AppRepositoryCustomCA struct {
 	// Selects a key of a secret in the pod's namespace
 	SecretKeyRef corev1.SecretKeySelector `json:"secretKeyRef,omitempty"`
+}
+
+// FilterRuleSpec defines a set of rules and aggreagation logic
+type FilterRuleSpec struct {
+	JQ        string            `json:"jq"`
+	Variables map[string]string `json:"variables,omitempty"`
 }
 
 // AppRepositoryStatus is the status for an AppRepository resource
