a3d1bae98e0d0b117766f3f8729dd183ca1e782d
Andres Martinez Gotor <andres@bitnami.com>
2021-03-10 17:09:22 +0100

    More fixes for the compatibility of e2e tests

--- integration/use-cases/create-registry.js
+++ integration/use-cases/create-registry.js
@@ -13,9 +13,9 @@ test("Creates a registry", async () => {
 
   await expect(page).toClick("cds-button", { text: "Add App Repository" });
 
-  await page.type("cds-form-group > cds-input:nth-child(1) > input", "my-repo");
+  await page.type("input[placeholder=\"example\"]", "my-repo");
 
-  await page.type("cds-form-group > cds-input:nth-child(2) > input", "https://charts.gitlab.io/");
+  await page.type("input[placeholder=\"https://charts.example.com/stable\"]", "https://charts.gitlab.io/");
 
   // Similar to the above click for an App Repository, the click on
   // the Install Repo doesn't always register (in fact, from the
