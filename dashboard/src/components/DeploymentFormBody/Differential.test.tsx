831a59168624f43b1cc67e04449a6102ef0c8310
Andres Martinez Gotor <andres@bitnami.com>
2021-03-11 15:12:36 +0100

    Dark mode for diff tab

--- dashboard/src/components/DeploymentFormBody/Differential.test.tsx
+++ dashboard/src/components/DeploymentFormBody/Differential.test.tsx
@@ -1,17 +1,37 @@
-import { shallow } from "enzyme";
+import ReactDiffViewer from "react-diff-viewer";
+import { SupportedThemes } from "shared/Config";
+import { defaultStore, getStore, mountWrapper } from "shared/specs/mountWrapper";
 import Differential from "./Differential";
 
 it("should render a diff between two strings", () => {
-  const wrapper = shallow(
-    <Differential title="test" oldValues="foo" newValues="bar" emptyDiffText="empty" />,
+  const wrapper = mountWrapper(
+    defaultStore,
+    <Differential oldValues="foo" newValues="bar" emptyDiffText="empty" />,
   );
-  expect(wrapper).toMatchSnapshot();
+  expect(wrapper.find(ReactDiffViewer).props()).toMatchObject({ oldValue: "foo", newValue: "bar" });
 });
 
 it("should print the emptyDiffText if there are no changes", () => {
-  const wrapper = shallow(
-    <Differential title="test" oldValues="foo" newValues="foo" emptyDiffText="No differences!" />,
+  const wrapper = mountWrapper(
+    defaultStore,
+    <Differential oldValues="foo" newValues="foo" emptyDiffText="No differences!" />,
   );
   expect(wrapper.text()).toMatch("No differences!");
   expect(wrapper.text()).not.toMatch("foo");
+});
+
+it("sets light theme by default", () => {
+  const wrapper = mountWrapper(
+    defaultStore,
+    <Differential oldValues="foo" newValues="bar" emptyDiffText="empty" />,
+  );
+  expect(wrapper.find(ReactDiffViewer).prop("useDarkTheme")).toBe(false);
+});
+
+it("changes theme", () => {
+  const wrapper = mountWrapper(
+    getStore({ config: { theme: SupportedThemes.dark } }),
+    <Differential oldValues="foo" newValues="bar" emptyDiffText="empty" />,
+  );
+  expect(wrapper.find(ReactDiffViewer).prop("useDarkTheme")).toBe(true);
 });
--- dashboard/src/components/DeploymentFormBody/DifferentialSelector.tsx
+++ dashboard/src/components/DeploymentFormBody/DifferentialSelector.tsx
@@ -15,27 +15,17 @@ export default function DifferentialSelector({
   appValues,
 }: IDifferentialSelectorProps) {
   let oldValues = "";
-  let title = "";
   let emptyDiffText = "";
   if (deploymentEvent === "upgrade") {
     // If there are already some deployed values (upgrade scenario)
     // We compare the values from the old release and the new one
     oldValues = deployedValues;
-    title = "Difference from deployed version";
     emptyDiffText = "The values for the new release are identical to the deployed version.";
   } else {
     // If it's a new deployment, we show the different from the default
     // values for the selected version
     oldValues = defaultValues || "";
-    title = "Difference from chart defaults";
     emptyDiffText = "No changes detected from chart defaults.";
   }
-  return (
-    <Differential
-      title={title}
-      oldValues={oldValues}
-      newValues={appValues}
-      emptyDiffText={emptyDiffText}
-    />
-  );
+  return <Differential oldValues={oldValues} newValues={appValues} emptyDiffText={emptyDiffText} />;
 }
