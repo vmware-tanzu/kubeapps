6287486ceca11d24effa3a0a15d6cbeeb140c876
Antonio Gámez Díaz <agamez@vmware.com>
2021-03-23 19:42:34 +0100

    Update CI config

--- integration/use-cases/create-private-registry.js
+++ integration/use-cases/create-private-registry.js
@@ -3,20 +3,19 @@ const utils = require("./lib/utils");
 
 test("Creates a private registry", async () => {
   // ODIC login
-  await Promise.all([
-    await page.goto(getUrl("/#/c/default/ns/default/config/repos")),
-    await page.waitForNavigation(),
-    await expect(page).toClick("cds-button", { text: "Login via OIDC Provider" }),
-    await page.waitForNavigation(),
-    await expect(page).toClick(".dex-container button", { text: "Log in with Email" }),
-    await page.waitForNavigation(),
-    await page.type("input[id=\"login\"]", "kubeapps-operator@example.com"),
-    await page.type("input[id=\"password\"]", "password"),
-    await page.waitForSelector("#submit-login", {visible: true, timeout: 3000 }),
-    await page.evaluate((selector) => document.querySelector(selector).click(), "#submit-login"),
-  ]);
-  await page.goto(getUrl("/#/c/default/ns/default/config/repos")),
-  await page.waitForNavigation(),
+  await page.goto(getUrl("/#/c/default/ns/default/config/repos"));
+  await page.waitForNavigation();
+  await expect(page).toClick("cds-button", { text: "Login via OIDC Provider" });
+  await page.waitForNavigation();
+  await expect(page).toClick(".dex-container button", { text: "Log in with Email" });
+  await page.waitForNavigation();
+  await page.type("input[id=\"login\"]", "kubeapps-operator@example.com");
+  await page.type("input[id=\"password\"]", "password");
+  await page.waitForSelector("#submit-login", { visible: true, timeout: 3000 });
+  await page.evaluate((selector) => document.querySelector(selector).click(), "#submit-login");
+  await page.waitForSelector(".kubeapps-header-content", { visible: true, timeout: 3000 });
+  await page.goto(getUrl("/#/c/default/ns/default/config/repos"));
+  await page.waitForNavigation();
 
   await expect(page).toClick("cds-button", { text: "Add App Repository" });
   const randomNumber = Math.floor(Math.random() * Math.floor(100));
