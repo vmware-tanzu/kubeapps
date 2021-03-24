3bed24ff6a7cab2b9956c151cd2e5372dae925f7
Antonio Gámez Díaz <agamez@vmware.com>
2021-03-24 10:11:03 +0100

    Update CI config

--- integration/use-cases/create-private-registry.js
+++ integration/use-cases/create-private-registry.js
@@ -12,7 +12,6 @@ test("Creates a private registry", async () => {
   await page.type("input[id=\"login\"]", "kubeapps-operator@example.com");
   await page.type("input[id=\"password\"]", "password");
   await page.waitForSelector("#submit-login", { visible: true, timeout: 3000 });
-  await expect(page).toClick("#submit-login", { text: "Login" });
   await page.evaluate((selector) => document.querySelector(selector).click(), "#submit-login");
   await page.waitForSelector(".kubeapps-header-content", { visible: true, timeout: 3000 });
   await page.goto(getUrl("/#/c/default/ns/default/config/repos"));
