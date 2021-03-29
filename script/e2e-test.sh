696780097c80ade1ae63f6416568d3261636814d
Antonio Gamez Diaz <agamez@vmware.com>
2021-03-29 11:46:09 +0200

    Reduce OLM installation flakiness
    Based upon https://github.com/operator-framework/operator-lifecycle-manager/blob/master/scripts/install.sh

--- script/e2e-test.sh
+++ script/e2e-test.sh
@@ -75,12 +75,34 @@ installOLM() {
     url=https://github.com/operator-framework/operator-lifecycle-manager/releases/download/${release}
     namespace=olm
 
-    kubectl apply -f ${url}/crds.yaml
-    kubectl apply -f ${url}/olm.yaml
+    kubectl apply -f "${url}/crds.yaml"
+    kubectl wait --for=condition=Established -f "${url}/crds.yaml"
+    kubectl apply -f "${url}/olm.yaml"
 
     # wait for deployments to be ready
     kubectl rollout status -w deployment/olm-operator --namespace="${namespace}"
     kubectl rollout status -w deployment/catalog-operator --namespace="${namespace}"
+
+    retries=30
+    until [[ $retries == 0 ]]; do
+        new_csv_phase=$(kubectl get csv -n "${namespace}" packageserver -o jsonpath='{.status.phase}' 2>/dev/null || echo "Waiting for CSV to appear")
+        if [[ $new_csv_phase != "$csv_phase" ]]; then
+            csv_phase=$new_csv_phase
+            echo "CSV \"packageserver\" phase: $csv_phase"
+        fi
+        if [[ "$new_csv_phase" == "Succeeded" ]]; then
+      break
+        fi
+        sleep 10
+        retries=$((retries - 1))
+    done
+
+  if [ $retries == 0 ]; then
+      echo "CSV \"packageserver\" failed to reach phase succeeded"
+      exit 1
+  fi
+
+  kubectl rollout status -w deployment/packageserver --namespace="${namespace}"
 }
 
 ########################
