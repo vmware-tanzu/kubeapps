3320a087d87e84e39234c5bedb28adb578effad6
Antonio Gamez Diaz <agamez@vmware.com>
2021-03-30 23:43:55 +0200

    Fix sequencer

--- integration/jest.sequencer.js
+++ integration/jest.sequencer.js
@@ -1,8 +1,10 @@
 const Sequencer = require("@jest/test-sequencer").default;
 
-export default class CustomSequencer extends Sequencer {
+class CustomSequencer extends Sequencer {
   sort(tests) {
     const copyTests = Array.from(tests);
     return copyTests.sort((testA, testB) => (testA.path > testB.path ? 1 : -1));
   }
 }
+
+module.exports = CustomSequencer;
