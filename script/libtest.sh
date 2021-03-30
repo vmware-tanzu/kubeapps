2775a31374dfa2f2941a13977bebb7d03bb3ecfc
Antonio Gamez Diaz <agamez@vmware.com>
2021-03-30 20:31:01 +0200

    Add watch to k8s_wait_for_deployment

--- script/libtest.sh
+++ script/libtest.sh
@@ -41,15 +41,16 @@ k8s_wait_for_deployment() {
     
     debug "Waiting for deployment ${deployment} to be successfully rolled out..."
     # Avoid to exit the function if the rollout fails
-    silence kubectl rollout status --namespace "$namespace" deployment "$deployment" --timeout=60s || exit_code=$?
+    silence kubectl rollout status --namespace "$namespace" deployment "$deployment" -w --timeout=60s || exit_code=$?
     debug "Rollout exit code: '${exit_code}'"
     if [ ${exit_code} -ne 0 ]
     then
       info "Rollout exit code: '${exit_code}'"
-      info "Retrying after 15s..."
+      exit_code=0;
+      info "Retrying after 60s..."
       kubectl get pods --namespace "$namespace"
       sleep 60
-      silence kubectl rollout status --namespace "$namespace" deployment "$deployment" --timeout=60s || exit_code=$?
+      silence kubectl rollout status --namespace "$namespace" deployment "$deployment" -w --timeout=60s || exit_code=$?
       if [ ${exit_code} -ne 0 ]
       then
         info "Rollout exit code: '${exit_code}'"
