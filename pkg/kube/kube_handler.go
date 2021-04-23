fc74321a4a1ee7ece4e7b4e9dc7a8cd0d9933caa
Antonio Gamez Diaz <agamez@vmware.com>
2021-04-23 13:49:26 +0200

    handle empty cluster in asSVC and others

--- pkg/kube/kube_handler.go
+++ pkg/kube/kube_handler.go
@@ -110,6 +110,14 @@ func NewClusterConfig(inClusterConfig *rest.Config, userToken string, cluster st
 	config.BearerToken = userToken
 	config.BearerTokenFile = ""
 
+	fmt.Printf("Ey,NewClusterConfig '%v': ", cluster)
+
+	// If the cluster is not provided, assume inClusterConfig
+	if cluster == "" {
+		fmt.Printf("Ey, NewClusterConfig return '%v': ", cluster)
+		return config, nil
+	}
+
 	clusterConfig, ok := clustersConfig.Clusters[cluster]
 	if !ok {
 		return nil, fmt.Errorf("cluster %q has no configuration", cluster)
@@ -259,7 +267,7 @@ func (a *kubeHandler) getSvcClientsetForCluster(cluster string, config *rest.Con
 	// cluster, the namespace selector remains unpopulated.
 	var svcClientset combinedClientsetInterface
 	var err error
-	if cluster == a.clustersConfig.KubeappsClusterName {
+	if cluster == "" || cluster == a.clustersConfig.KubeappsClusterName {
 		svcClientset = a.kubeappsSvcClientset
 	} else {
 		additionalCluster, ok := a.clustersConfig.Clusters[cluster]
