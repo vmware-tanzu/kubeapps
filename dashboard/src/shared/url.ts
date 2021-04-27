e33c3cddcc711115c02bd61babd2507808843e55
Antonio Gamez Diaz <agamez@vmware.com>
2021-04-27 12:39:57 +0200

    Revert change in can-i url

--- dashboard/src/shared/url.ts
+++ dashboard/src/shared/url.ts
@@ -97,7 +97,7 @@ export const backend = {
     update: (cluster: string, namespace: string, name: string) =>
       `${backend.apprepositories.base(cluster, namespace)}/${name}`,
   },
-  canI: (cluster: string) => `api/v1/clusters${cluster ? "/" + cluster : ""}/can-i`,
+  canI: (cluster: string) => `api/v1/clusters/${cluster}/can-i`,
 };
 
 export const kubeops = {
