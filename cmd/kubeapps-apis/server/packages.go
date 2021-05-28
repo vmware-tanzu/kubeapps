dcfff7cd87779b5f943ff8b48e7a970a79e817da
Michael Nelson <minelson@vmware.com>
2021-05-28 16:58:56 +1000

    Register plugins that implement core services for aggregation.

---
+++ cmd/kubeapps-apis/server/packages.go
@@ -0,0 +1,58 @@
+/*
+Copyright © 2021 VMware
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package server
+
+import (
+	"context"
+
+	packages "github.com/kubeapps/kubeapps/cmd/kubeapps-apis/gen/core/packages/v1alpha1"
+)
+
+// packagesServer implements the API defined in proto/kubeappsapis/core/packages/v1alpha1/packages.proto
+type packagesServer struct {
+	packages.UnimplementedPackagesServiceServer
+
+	// plugins is a slice of all registered plugins which satisfy the core.packages.v1alpha1
+	// interface.
+	plugins []*pkgsPluginWithServer
+}
+
+func NewPackagesServer(plugins []*pkgsPluginWithServer) *packagesServer {
+	return &packagesServer{
+		plugins: plugins,
+	}
+}
+
+// GetPackageRepositories returns the package repositories based on the request.
+func (s packagesServer) GetPackageRepositories(ctx context.Context, request *packages.GetPackageRepositoriesRequest) (*packages.GetPackageRepositoriesResponse, error) {
+	repos := []*packages.PackageRepository{}
+	for _, p := range s.plugins {
+		response, err := p.server.GetPackageRepositories(ctx, request)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add the plugin for the repos
+		pluginRepos := response.Repositories
+		for _, r := range pluginRepos {
+			r.Plugin = p.plugin
+		}
+
+		repos = append(repos, pluginRepos...)
+	}
+
+	// TODO: Sort via default sort order or that specified in request.
+	return &packages.GetPackageRepositoriesResponse{
+		Repositories: repos,
+	}, nil
+}
--- cmd/kubeapps-apis/server/plugins.go
+++ cmd/kubeapps-apis/server/plugins.go
@@ -20,10 +20,14 @@ import (
 	"path"
 	"path/filepath"
 	"plugin"
+	"reflect"
 	"sort"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	// packages "github.com/kubeapps/kubeapps/cmd/kubeapps-apis/gen/core/packages/v1alpha1"
+	packages "github.com/kubeapps/kubeapps/cmd/kubeapps-apis/gen/core/packages/v1alpha1"
 	plugins "github.com/kubeapps/kubeapps/cmd/kubeapps-apis/gen/core/plugins/v1alpha1"
+
 	"google.golang.org/grpc"
 	log "k8s.io/klog/v2"
 )
@@ -39,12 +43,24 @@ const (
 	pluginDetailFunction    = "GetPluginDetail"
 )
 
+// pkgsPluginWithServer stores the plugin detail together with its implementation.
+type pkgsPluginWithServer struct {
+	plugin *plugins.Plugin
+	server packages.PackagesServiceServer
+}
+
 // coreServer implements the API defined in cmd/kubeapps-api-service/core/core.proto
 type pluginsServer struct {
 	plugins.UnimplementedPluginsServiceServer
 
 	// The slice of plugins is initialised when registering plugins during NewPluginsServer.
 	plugins []*plugins.Plugin
+
+	// packagesPlugins contains plugin server implementations which satisfy
+	// the core server packages.v1alpha1 interface.
+	// TODO: Update the plugins server to be able to register different versions
+	// of core plugins.
+	packagesPlugins []*pkgsPluginWithServer
 }
 
 func NewPluginsServer(pluginDirs []string, registrar grpc.ServiceRegistrar, gwArgs gwHandlerArgs) (*pluginsServer, error) {
@@ -54,16 +70,18 @@ func NewPluginsServer(pluginDirs []string, registrar grpc.ServiceRegistrar, gwAr
 		log.Fatalf("failed to check for plugins: %v", err)
 	}
 
-	pluginDetails, err := registerPlugins(pluginPaths, registrar, gwArgs)
+	ps := &pluginsServer{}
+
+	pluginDetails, err := ps.registerPlugins(pluginPaths, registrar, gwArgs)
 	if err != nil {
 		return nil, fmt.Errorf("failed to register plugins: %w", err)
 	}
 
 	sortPlugins(pluginDetails)
 
-	return &pluginsServer{
-		plugins: pluginDetails,
-	}, nil
+	ps.plugins = pluginDetails
+
+	return ps, nil
 }
 
 // sortPlugins returns a consistently ordered slice.
@@ -81,7 +99,7 @@ func (s *pluginsServer) GetConfiguredPlugins(ctx context.Context, in *plugins.Ge
 }
 
 // registerPlugins opens each plugin, looks up the register function and calls it with the registrar.
-func registerPlugins(pluginPaths []string, grpcReg grpc.ServiceRegistrar, gwArgs gwHandlerArgs) ([]*plugins.Plugin, error) {
+func (s *pluginsServer) registerPlugins(pluginPaths []string, grpcReg grpc.ServiceRegistrar, gwArgs gwHandlerArgs) ([]*plugins.Plugin, error) {
 	pluginDetails := []*plugins.Plugin{}
 	for _, pluginPath := range pluginPaths {
 		p, err := plugin.Open(pluginPath)
@@ -89,18 +107,19 @@ func registerPlugins(pluginPaths []string, grpcReg grpc.ServiceRegistrar, gwArgs
 			return nil, fmt.Errorf("unable to open plugin %q: %w", pluginPath, err)
 		}
 
-		if err = registerGRPC(p, pluginPath, grpcReg); err != nil {
-			return nil, err
-		}
-
-		if err = registerHTTP(p, pluginPath, gwArgs); err != nil {
-			return nil, err
-		}
-
-		if pluginDetail, err := getPluginDetail(p, pluginPath); err != nil {
+		var pluginDetail *plugins.Plugin
+		if pluginDetail, err = getPluginDetail(p, pluginPath); err != nil {
 			return nil, err
 		} else {
 			pluginDetails = append(pluginDetails, pluginDetail)
+		}
+
+		if err = s.registerGRPC(p, pluginDetail, grpcReg); err != nil {
+			return nil, err
+		}
+
+		if err = registerHTTP(p, pluginDetail, gwArgs); err != nil {
+			return nil, err
 		}
 
 		log.Infof("Successfully registered plugin %q", pluginPath)
@@ -109,19 +128,43 @@ func registerPlugins(pluginPaths []string, grpcReg grpc.ServiceRegistrar, gwArgs
 }
 
 // registerGRPC finds and calls the required function for registering the plugin for the GRPC server.
-func registerGRPC(p *plugin.Plugin, pluginPath string, registrar grpc.ServiceRegistrar) error {
+func (s *pluginsServer) registerGRPC(p *plugin.Plugin, pluginDetail *plugins.Plugin, registrar grpc.ServiceRegistrar) error {
 	grpcRegFn, err := p.Lookup(grpcRegisterFunction)
 	if err != nil {
-		return fmt.Errorf("unable to lookup %q for %q: %w", grpcRegisterFunction, pluginPath, err)
+		return fmt.Errorf("unable to lookup %q for %v: %w", grpcRegisterFunction, pluginDetail, err)
 	}
-	type grpcRegisterFunctionType = func(grpc.ServiceRegistrar)
+	type grpcRegisterFunctionType = func(grpc.ServiceRegistrar) interface{}
 	grpcFn, ok := grpcRegFn.(grpcRegisterFunctionType)
 	if !ok {
-		var dummyFn grpcRegisterFunctionType = func(grpc.ServiceRegistrar) {}
-		return fmt.Errorf("unable to use %q in plugin %q due to mismatched signature.\nwant: %T\ngot: %T", grpcRegisterFunction, pluginPath, dummyFn, grpcRegFn)
+		var dummyFn grpcRegisterFunctionType = func(grpc.ServiceRegistrar) interface{} { return nil }
+		return fmt.Errorf("unable to use %q in plugin %v due to mismatched signature.\nwant: %T\ngot: %T", grpcRegisterFunction, pluginDetail, dummyFn, grpcRegFn)
 	}
-	grpcFn(registrar)
+	server := grpcFn(registrar)
 
+	return s.registerPluginsSatisfyingCoreAPIs(server, pluginDetail)
+}
+
+// registerPluginsImplementingCoreAPIs checks a plugin implementation to see
+// if it implements a core api (such as `packages.v1alpha1`) and if so,
+// keeps a (typed) reference to the implementation for use on aggregate APIs.
+func (s *pluginsServer) registerPluginsSatisfyingCoreAPIs(pluginSrv interface{}, pluginDetail *plugins.Plugin) error {
+	// The following heck if the service implements an interface is what
+	// grpc-go itself does, see:
+	// https://github.com/grpc/grpc-go/blob/v1.38.0/server.go#L621
+	serverType := reflect.TypeOf(pluginSrv)
+	corePackagesType := reflect.TypeOf((*packages.PackagesServiceServer)(nil)).Elem()
+
+	if serverType.Implements(corePackagesType) {
+		pkgsSrv, ok := pluginSrv.(packages.PackagesServiceServer)
+		if !ok {
+			return fmt.Errorf("Unable to convert plugin %v to core PackagesServicesServer although it implements the same.", pluginDetail)
+		}
+		s.packagesPlugins = append(s.packagesPlugins, &pkgsPluginWithServer{
+			plugin: pluginDetail,
+			server: pkgsSrv,
+		})
+		log.Errorf("Plugin %v implements core.packages.v1alpha1. Registered for aggregation.", pluginDetail)
+	}
 	return nil
 }
 
@@ -144,10 +187,10 @@ func getPluginDetail(p *plugin.Plugin, pluginPath string) (*plugins.Plugin, erro
 }
 
 // registerHTTP finds and calls the required function for registering the plugin for the HTTP gateway server.
-func registerHTTP(p *plugin.Plugin, pluginPath string, gwArgs gwHandlerArgs) error {
+func registerHTTP(p *plugin.Plugin, pluginDetail *plugins.Plugin, gwArgs gwHandlerArgs) error {
 	gwRegFn, err := p.Lookup(gatewayRegisterFunction)
 	if err != nil {
-		return fmt.Errorf("unable to lookup %q for %q: %w", gatewayRegisterFunction, pluginPath, err)
+		return fmt.Errorf("unable to lookup %q for %v: %w", gatewayRegisterFunction, pluginDetail, err)
 	}
 	type gatewayRegisterFunctionType = func(context.Context, *runtime.ServeMux, string, []grpc.DialOption) error
 	gwfn, ok := gwRegFn.(gatewayRegisterFunctionType)
@@ -155,7 +198,7 @@ func registerHTTP(p *plugin.Plugin, pluginPath string, gwArgs gwHandlerArgs) err
 		// Create a dummyFn only so we can ensure the correct type is shown in case
 		// of an error.
 		var dummyFn gatewayRegisterFunctionType = func(context.Context, *runtime.ServeMux, string, []grpc.DialOption) error { return nil }
-		return fmt.Errorf("unable to use %q in plugin %q due to mismatched signature.\nwant: %T\ngot: %T", gatewayRegisterFunction, pluginPath, dummyFn, gwRegFn)
+		return fmt.Errorf("unable to use %q in plugin %v due to mismatched signature.\nwant: %T\ngot: %T", gatewayRegisterFunction, pluginDetail, dummyFn, gwRegFn)
 	}
 	return gwfn(gwArgs.ctx, gwArgs.mux, gwArgs.addr, gwArgs.dialOptions)
 }
