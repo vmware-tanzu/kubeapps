9bcf61585d185c4063bfd4f46b0e8a262c0c77cd
Greg Fichtenholtz <74032303+gfichtenholt@users.noreply.github.com>
2021-05-30 19:19:37 -0700

    handle namespace in GetAvailablePackages and GetPackageRepositories(), if applicable (#2905)
    * https://github.com/kubeapps/kubeapps/issues/759
    
    Allow adding an optional description for app repositories
    
    * updates based on Michael Nelson's feedback
    
    * missed a test
    
    * add icon url and repo ref to getAvailablePackaes response
    
    * incremental
    
    * clarify comment for readPackagesFromRepoIndex
    
    * clean up
    
    * clean up 2
    
    * add unit tests for fluxv2.GetAvailablePackages
    
    * minor cleanup
    
    * minor cleanup 2
    
    * added a couple more checks
    
    * added unit tests for GetPackageRepositories
    
    * minor changes based on @minelson feedback
    
    * handle namespace in GetAvailablePackages and GetRepositories, if applicable
    
    * multi-cluster not supported yet

--- cmd/kubeapps-apis/plugins/fluxv2/packages/v1alpha1/server.go
+++ cmd/kubeapps-apis/plugins/fluxv2/packages/v1alpha1/server.go
@@ -74,9 +74,12 @@ func NewServer() *Server {
 
 // GetPackageRepositories returns the package repositories based on the request.
 func (s *Server) GetPackageRepositories(ctx context.Context, request *corev1.GetPackageRepositoriesRequest) (*corev1.GetPackageRepositoriesResponse, error) {
-	log.Infof("+GetPackageRepositories(namespace=[%s])", request.Namespace)
+	log.Infof("+GetPackageRepositories(namespace=[%s], cluster=[%s])", request.Namespace, request.Cluster)
+	if request.Cluster != "" {
+		return nil, status.Errorf(codes.Unimplemented, "Not supported yet")
+	}
 
-	repos, err := s.getHelmRepos(ctx)
+	repos, err := s.getHelmRepos(ctx, request.Namespace)
 	if err != nil {
 		return nil, err
 	}
@@ -115,8 +118,12 @@ func (s *Server) GetPackageRepositories(ctx context.Context, request *corev1.Get
 
 // GetAvailablePackages streams the available packages based on the request.
 func (s *Server) GetAvailablePackages(ctx context.Context, request *corev1.GetAvailablePackagesRequest) (*corev1.GetAvailablePackagesResponse, error) {
-	log.Infof("+GetAvailablePackages(namespace=[%s])", request.Namespace)
-	repos, err := s.getHelmRepos(ctx)
+	log.Infof("+GetAvailablePackages(namespace=[%s], cluster=[%s])", request.Namespace, request.Cluster)
+	if request.Cluster != "" {
+		return nil, status.Errorf(codes.Unimplemented, "Not supported yet")
+	}
+
+	repos, err := s.getHelmRepos(ctx, request.Namespace)
 	if err != nil {
 		return nil, err
 	}
@@ -183,7 +190,7 @@ func clientForRequestContext(ctx context.Context) (dynamic.Interface, error) {
 	return client, nil
 }
 
-func (s *Server) getHelmRepos(ctx context.Context) (*unstructured.UnstructuredList, error) {
+func (s *Server) getHelmRepos(ctx context.Context, namespace string) (*unstructured.UnstructuredList, error) {
 	if s.clientGetter == nil {
 		return nil, status.Errorf(codes.Internal, "server not configured with configGetter")
 	}
@@ -197,8 +204,12 @@ func (s *Server) getHelmRepos(ctx context.Context) (*unstructured.UnstructuredLi
 		Version:  fluxVersion,
 		Resource: fluxHelmRepositories}
 
-	// Currently checks globally. Update to handle namespaced requests (?)
-	repos, err := client.Resource(repositoryResource).List(ctx, metav1.ListOptions{})
+	var resource dynamic.NamespaceableResourceInterface = client.Resource(repositoryResource)
+	var resourceIfc dynamic.ResourceInterface = resource
+	if namespace != "" {
+		resourceIfc = resource.Namespace(namespace)
+	}
+	repos, err := resourceIfc.List(ctx, metav1.ListOptions{})
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "unable to list fluxv2 helmrepositories: %v", err)
 	} else {
