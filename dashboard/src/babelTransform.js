f98123c7c0ea0fb47e90d4cbc9f527b56034c7eb
Antonio Gamez Diaz <agamez@vmware.com>
2021-06-30 13:48:04 +0200

    Add more context on the workaround

--- dashboard/src/babelTransform.js
+++ dashboard/src/babelTransform.js
@@ -22,8 +22,14 @@ module.exports = babelJest.createTransformer({
       require.resolve("babel-preset-react-app"),
       {
         runtime: hasJsxRuntime ? "automatic" : "classic",
-        // Temporary workaround until this issue get solved:
+        // Temporary workaround until one of these issues get solved:
         // https://github.com/babel/babel/issues/13520
+        // https://github.com/facebook/create-react-app/issues/11159
+        // Note:
+        // Flow is a static type checker, but we don't have any raw js files requiring it.
+        // It is enabled by default, so we need to explicitly disable it because it is
+        // causing some parsing errors when reading named imports like "import foo as as".
+        // More context here: https://github.com/kubeapps/kubeapps/pull/3042#issuecomment-870053634
         flow: false,
       },
     ],
