a3f478c38af2d2d31528b7ef2cc989f5faf55f9e
Dimitri Laloue <dlaloue@dlaloue-a01.vmware.com>
2021-07-02 14:52:53 +0900

    fixed typo in test code

--- cmd/pinniped-proxy/src/pinniped.rs
+++ cmd/pinniped-proxy/src/pinniped.rs
@@ -386,7 +386,7 @@ mod tests {
                     e.is::<http::uri::InvalidUri>(),
                     "got: {:#?}, want: {}",
                     e,
-                    http::uri::InvalidUri::InvalidUriChar
+                    http::uri::InvalidUri.InvalidUriChar
                 );
                 Ok(())
             }
