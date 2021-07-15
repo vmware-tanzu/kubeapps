194bc109205b2a70dbb512af6bc8813f81ac0dee
Antonio Gamez Diaz <agamez@vmware.com>
2021-07-15 03:50:33 +0200

    Add generated frontend files
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

---
+++ dashboard/src/gen/kubeappsapis/plugins/helm/packages/v1alpha1/helm.ts
@@ -0,0 +1,249 @@
+/* eslint-disable */
+import Long from "long";
+import { grpc } from "@improbable-eng/grpc-web";
+import _m0 from "protobufjs/minimal";
+import {
+  GetAvailablePackageSummariesRequest,
+  GetAvailablePackageDetailRequest,
+  GetAvailablePackageVersionsRequest,
+  GetAvailablePackageSummariesResponse,
+  GetAvailablePackageDetailResponse,
+  GetAvailablePackageVersionsResponse,
+} from "../../../../../kubeappsapis/core/packages/v1alpha1/packages";
+import { BrowserHeaders } from "browser-headers";
+
+export const protobufPackage = "kubeappsapis.plugins.helm.packages.v1alpha1";
+
+export interface HelmPackagesService {
+  /** GetAvailablePackageSummaries returns the available packages managed by the 'helm' plugin */
+  GetAvailablePackageSummaries(
+    request: DeepPartial<GetAvailablePackageSummariesRequest>,
+    metadata?: grpc.Metadata
+  ): Promise<GetAvailablePackageSummariesResponse>;
+  /** GetAvailablePackageDetail returns the package details managed by the 'helm' plugin */
+  GetAvailablePackageDetail(
+    request: DeepPartial<GetAvailablePackageDetailRequest>,
+    metadata?: grpc.Metadata
+  ): Promise<GetAvailablePackageDetailResponse>;
+  /** GetAvailablePackageVersions returns the package versions managed by the 'helm' plugin */
+  GetAvailablePackageVersions(
+    request: DeepPartial<GetAvailablePackageVersionsRequest>,
+    metadata?: grpc.Metadata
+  ): Promise<GetAvailablePackageVersionsResponse>;
+}
+
+export class HelmPackagesServiceClientImpl implements HelmPackagesService {
+  private readonly rpc: Rpc;
+
+  constructor(rpc: Rpc) {
+    this.rpc = rpc;
+    this.GetAvailablePackageSummaries =
+      this.GetAvailablePackageSummaries.bind(this);
+    this.GetAvailablePackageDetail = this.GetAvailablePackageDetail.bind(this);
+    this.GetAvailablePackageVersions =
+      this.GetAvailablePackageVersions.bind(this);
+  }
+
+  GetAvailablePackageSummaries(
+    request: DeepPartial<GetAvailablePackageSummariesRequest>,
+    metadata?: grpc.Metadata
+  ): Promise<GetAvailablePackageSummariesResponse> {
+    return this.rpc.unary(
+      HelmPackagesServiceGetAvailablePackageSummariesDesc,
+      GetAvailablePackageSummariesRequest.fromPartial(request),
+      metadata
+    );
+  }
+
+  GetAvailablePackageDetail(
+    request: DeepPartial<GetAvailablePackageDetailRequest>,
+    metadata?: grpc.Metadata
+  ): Promise<GetAvailablePackageDetailResponse> {
+    return this.rpc.unary(
+      HelmPackagesServiceGetAvailablePackageDetailDesc,
+      GetAvailablePackageDetailRequest.fromPartial(request),
+      metadata
+    );
+  }
+
+  GetAvailablePackageVersions(
+    request: DeepPartial<GetAvailablePackageVersionsRequest>,
+    metadata?: grpc.Metadata
+  ): Promise<GetAvailablePackageVersionsResponse> {
+    return this.rpc.unary(
+      HelmPackagesServiceGetAvailablePackageVersionsDesc,
+      GetAvailablePackageVersionsRequest.fromPartial(request),
+      metadata
+    );
+  }
+}
+
+export const HelmPackagesServiceDesc = {
+  serviceName:
+    "kubeappsapis.plugins.helm.packages.v1alpha1.HelmPackagesService",
+};
+
+export const HelmPackagesServiceGetAvailablePackageSummariesDesc: UnaryMethodDefinitionish =
+  {
+    methodName: "GetAvailablePackageSummaries",
+    service: HelmPackagesServiceDesc,
+    requestStream: false,
+    responseStream: false,
+    requestType: {
+      serializeBinary() {
+        return GetAvailablePackageSummariesRequest.encode(this).finish();
+      },
+    } as any,
+    responseType: {
+      deserializeBinary(data: Uint8Array) {
+        return {
+          ...GetAvailablePackageSummariesResponse.decode(data),
+          toObject() {
+            return this;
+          },
+        };
+      },
+    } as any,
+  };
+
+export const HelmPackagesServiceGetAvailablePackageDetailDesc: UnaryMethodDefinitionish =
+  {
+    methodName: "GetAvailablePackageDetail",
+    service: HelmPackagesServiceDesc,
+    requestStream: false,
+    responseStream: false,
+    requestType: {
+      serializeBinary() {
+        return GetAvailablePackageDetailRequest.encode(this).finish();
+      },
+    } as any,
+    responseType: {
+      deserializeBinary(data: Uint8Array) {
+        return {
+          ...GetAvailablePackageDetailResponse.decode(data),
+          toObject() {
+            return this;
+          },
+        };
+      },
+    } as any,
+  };
+
+export const HelmPackagesServiceGetAvailablePackageVersionsDesc: UnaryMethodDefinitionish =
+  {
+    methodName: "GetAvailablePackageVersions",
+    service: HelmPackagesServiceDesc,
+    requestStream: false,
+    responseStream: false,
+    requestType: {
+      serializeBinary() {
+        return GetAvailablePackageVersionsRequest.encode(this).finish();
+      },
+    } as any,
+    responseType: {
+      deserializeBinary(data: Uint8Array) {
+        return {
+          ...GetAvailablePackageVersionsResponse.decode(data),
+          toObject() {
+            return this;
+          },
+        };
+      },
+    } as any,
+  };
+
+interface UnaryMethodDefinitionishR
+  extends grpc.UnaryMethodDefinition<any, any> {
+  requestStream: any;
+  responseStream: any;
+}
+
+type UnaryMethodDefinitionish = UnaryMethodDefinitionishR;
+
+interface Rpc {
+  unary<T extends UnaryMethodDefinitionish>(
+    methodDesc: T,
+    request: any,
+    metadata: grpc.Metadata | undefined
+  ): Promise<any>;
+}
+
+export class GrpcWebImpl {
+  private host: string;
+  private options: {
+    transport?: grpc.TransportFactory;
+
+    debug?: boolean;
+    metadata?: grpc.Metadata;
+  };
+
+  constructor(
+    host: string,
+    options: {
+      transport?: grpc.TransportFactory;
+
+      debug?: boolean;
+      metadata?: grpc.Metadata;
+    }
+  ) {
+    this.host = host;
+    this.options = options;
+  }
+
+  unary<T extends UnaryMethodDefinitionish>(
+    methodDesc: T,
+    _request: any,
+    metadata: grpc.Metadata | undefined
+  ): Promise<any> {
+    const request = { ..._request, ...methodDesc.requestType };
+    const maybeCombinedMetadata =
+      metadata && this.options.metadata
+        ? new BrowserHeaders({
+            ...this.options?.metadata.headersMap,
+            ...metadata?.headersMap,
+          })
+        : metadata || this.options.metadata;
+    return new Promise((resolve, reject) => {
+      grpc.unary(methodDesc, {
+        request,
+        host: this.host,
+        metadata: maybeCombinedMetadata,
+        transport: this.options.transport,
+        debug: this.options.debug,
+        onEnd: function (response) {
+          if (response.status === grpc.Code.OK) {
+            resolve(response.message);
+          } else {
+            const err = new Error(response.statusMessage) as any;
+            err.code = response.status;
+            err.metadata = response.trailers;
+            reject(err);
+          }
+        },
+      });
+    });
+  }
+}
+
+type Builtin =
+  | Date
+  | Function
+  | Uint8Array
+  | string
+  | number
+  | boolean
+  | undefined;
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Array<infer U>
+  ? Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U>
+  ? ReadonlyArray<DeepPartial<U>>
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>;
+
+if (_m0.util.Long !== Long) {
+  _m0.util.Long = Long as any;
+  _m0.configure();
+}
