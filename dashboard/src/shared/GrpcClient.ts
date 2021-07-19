4172dcb405146dc7f2455bb120682fff94bc13b3
Antonio Gamez Diaz <agamez@vmware.com>
2021-07-19 14:49:20 +0200

    Simplify grpcClient
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- dashboard/src/shared/GrpcClient.ts
+++ dashboard/src/shared/GrpcClient.ts
@@ -6,16 +6,10 @@ export class GrpcClient {
   private baseUrl = URL.api.kubeappsapis;
   private grpcClient!: GrpcWebImpl;
 
-  private GetClientConfig = () => {
-    return {
-      // https://github.com/improbable-eng/grpc-web/blob/master/client/grpc-web/docs/transport.md#specifying-transports.
-      transport: grpc.CrossBrowserHttpTransport({ withCredentials: true }),
-    };
-  };
-
   public getGrpcClient = () => {
     if (!this.grpcClient) {
-      this.grpcClient = new GrpcWebImpl(this.baseUrl, this.GetClientConfig());
+      grpc.setDefaultTransport(grpc.CrossBrowserHttpTransport({}));
+      this.grpcClient = new GrpcWebImpl(this.baseUrl, {});
     }
     return this.grpcClient;
   };
