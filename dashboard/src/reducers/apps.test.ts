f52d9e289745b162f4d07b2e43af8e9d4beaaced
Antonio Gamez Diaz <agamez@vmware.com>
2021-08-25 19:29:18 +0200

    Remove bunch of tests in apps.test.ts as updateInfo no longer exists
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- dashboard/src/reducers/apps.test.ts
+++ dashboard/src/reducers/apps.test.ts
@@ -8,9 +8,7 @@ describe("appsReducer", () => {
 
   const actionTypes = {
     listApps: getType(actions.apps.listApps),
-    receiveAppList: getType(actions.apps.receiveAppList),
     requestApps: getType(actions.apps.requestApps),
-    receiveAppUpdateInfo: getType(actions.apps.receiveAppUpdateInfo),
   };
 
   beforeEach(() => {
@@ -40,131 +38,6 @@ describe("appsReducer", () => {
         type: actionTypes.listApps as any,
       });
       expect(state).toEqual({ ...initialState, isFetching: true });
-    });
-
-    describe("receieveAppUpdateInfo", () => {
-      const testListOverview = [
-        { releaseName: "test1" },
-        { releaseName: "test2" },
-        { releaseName: "test3" },
-      ];
-      const testUpdateInfo = {
-        upToDate: false,
-        chartLatestVersion: "1.0.0",
-        appLatestVersion: "1.0.0",
-        repository: { name: "myrepo", url: "myrepo.com", namespace: "my-namespace" },
-      };
-
-      it("updates the listOverview entry with the updateInfo if it exists", () => {
-        let state = {
-          ...initialState,
-          listOverview: testListOverview,
-        } as IAppState;
-
-        state = appsReducer(state, {
-          payload: {
-            releaseName: "test2",
-            updateInfo: testUpdateInfo,
-          },
-          type: actionTypes.receiveAppUpdateInfo,
-        });
-
-        expect(state.listOverview![1].updateInfo).toEqual(testUpdateInfo);
-      });
-
-      it("doesn't change listOverview if the entry doesn't exist", () => {
-        let state = {
-          ...initialState,
-          listOverview: testListOverview,
-        } as IAppState;
-
-        state = appsReducer(state, {
-          payload: {
-            releaseName: "test4",
-            updateInfo: testUpdateInfo,
-          },
-          type: actionTypes.receiveAppUpdateInfo,
-        });
-
-        expect(state.listOverview).toBe(testListOverview);
-      });
-
-      it("doesn't change listOverview if it is undefined", () => {
-        const state = appsReducer(initialState, {
-          payload: {
-            releaseName: "test4",
-            updateInfo: testUpdateInfo,
-          },
-          type: actionTypes.receiveAppUpdateInfo,
-        });
-
-        expect(state.listOverview).toBeUndefined();
-      });
-
-      it("sets updateInfo for selected if release name matches", () => {
-        const selected = { name: "test" };
-        let state = {
-          ...initialState,
-          selected,
-        } as IAppState;
-
-        state = appsReducer(state, {
-          payload: {
-            releaseName: "test",
-            updateInfo: testUpdateInfo,
-          },
-          type: actionTypes.receiveAppUpdateInfo,
-        });
-
-        expect(state.selected).toEqual({ ...selected, updateInfo: testUpdateInfo });
-      });
-
-      it("doesn't change selected if release name does not match", () => {
-        const selected = { name: "test" };
-        let state = {
-          ...initialState,
-          selected,
-        } as IAppState;
-
-        state = appsReducer(state, {
-          payload: {
-            releaseName: "nottest",
-            updateInfo: testUpdateInfo,
-          },
-          type: actionTypes.receiveAppUpdateInfo,
-        });
-
-        expect(state.selected).toBe(selected);
-      });
-
-      it("doesn't set updateInfo for selected if selected is undefined", () => {
-        const state = appsReducer(initialState, {
-          payload: {
-            releaseName: "test",
-            updateInfo: testUpdateInfo,
-          },
-          type: actionTypes.receiveAppUpdateInfo,
-        });
-
-        expect(state.selected).toBeUndefined();
-      });
-
-      it("sets isFetching to false", () => {
-        let state = {
-          ...initialState,
-          isFetching: true,
-        } as IAppState;
-
-        state = appsReducer(state, {
-          payload: {
-            releaseName: "test",
-            updateInfo: testUpdateInfo,
-          },
-          type: actionTypes.receiveAppUpdateInfo,
-        });
-
-        expect(state.isFetching).toBe(false);
-      });
     });
   });
 });
