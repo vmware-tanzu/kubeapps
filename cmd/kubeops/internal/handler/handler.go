bd3210dcf033ad1c7e576e9cd2747be58e7e9909
Michael Nelson <minelson@vmware.com>
2021-09-07 14:10:42 +1000

    Update chart.RegistrySecretsPerDomain to depend on kube clientset only.
    Signed-off-by: Michael Nelson <minelson@vmware.com>

--- cmd/kubeops/internal/handler/handler.go
+++ cmd/kubeops/internal/handler/handler.go
@@ -52,12 +52,13 @@ type Options struct {
 // Config represents data needed by each handler to be able to create Helm 3 actions.
 // It cannot be created without a bearer token, so a new one must be created upon each HTTP request.
 type Config struct {
-	ActionConfig *action.Configuration
-	Options      Options
-	KubeHandler  kube.AuthHandler
-	Resolver     handlerutil.ResolverFactory
-	Cluster      string
-	Token        string
+	ActionConfig  *action.Configuration
+	Options       Options
+	KubeHandler   kube.AuthHandler
+	Resolver      handlerutil.ResolverFactory
+	Cluster       string
+	Token         string
+	userClientSet kubernetes.Interface
 }
 
 // WithHandlerConfig takes a dependentHandler and creates a regular (WithParams) handler that,
@@ -109,12 +110,13 @@ func WithHandlerConfig(storageForDriver agent.StorageForDriver, options Options)
 			}
 
 			cfg := Config{
-				Options:      options,
-				ActionConfig: actionConfig,
-				KubeHandler:  kubeHandler,
-				Cluster:      cluster,
-				Token:        token,
-				Resolver:     &handlerutil.ClientResolver{},
+				Options:       options,
+				ActionConfig:  actionConfig,
+				KubeHandler:   kubeHandler,
+				Cluster:       cluster,
+				Token:         token,
+				Resolver:      &handlerutil.ClientResolver{},
+				userClientSet: userKubeClient,
 			}
 			f(cfg, w, req, params)
 		}
@@ -199,7 +201,7 @@ func CreateRelease(cfg Config, w http.ResponseWriter, req *http.Request, params 
 	releaseName := chartDetails.ReleaseName
 	namespace := params[namespaceParam]
 	valuesString := chartDetails.Values
-	registrySecrets, err := chartUtils.RegistrySecretsPerDomain(appRepo.Spec.DockerRegistrySecrets, cfg.Cluster, appRepo.Namespace, cfg.Token, cfg.KubeHandler)
+	registrySecrets, err := chartUtils.RegistrySecretsPerDomain(req.Context(), appRepo.Spec.DockerRegistrySecrets, appRepo.Namespace, cfg.userClientSet)
 	if err != nil {
 		returnErrMessage(err, w)
 		return
@@ -245,7 +247,7 @@ func upgradeRelease(cfg Config, w http.ResponseWriter, req *http.Request, params
 		caCertSecret, authSecret,
 		cfg.Resolver.New(appRepo.Spec.Type, cfg.Options.UserAgent),
 	)
-	registrySecrets, err := chartUtils.RegistrySecretsPerDomain(appRepo.Spec.DockerRegistrySecrets, cfg.Cluster, appRepo.Namespace, cfg.Token, cfg.KubeHandler)
+	registrySecrets, err := chartUtils.RegistrySecretsPerDomain(req.Context(), appRepo.Spec.DockerRegistrySecrets, appRepo.Namespace, cfg.userClientSet)
 	if err != nil {
 		returnErrMessage(err, w)
 		return
