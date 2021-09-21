a9ec3fc5ec528df80fdc292ecb4c19636d2b024e
Antonio Gámez <agamez@vmware.com>
2021-09-21 18:26:04 +0200

    Add versions - pinniped-proxy (#3452)
    * Display proper buildtime version in pinniped-proxy
    
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>
    
    * Use a build file instead to avoid build errors
    
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>
    
    * Run fmt
    
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>
    
    * Update deps, patch versions
    
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>
    
    * Rename var
    
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

---
+++ cmd/pinniped-proxy/build.rs
@@ -0,0 +1,9 @@
+use std::env;
+
+fn main() {
+    // If the binary is built with the ENV var "PINNIPED_PROXY_VERSION",
+    // the value will be available at buildime. Otherwise, it becomes "devel"
+    // We use this ENV var to display a custom version when passing the "-- version" flag
+    let version = env::var("PINNIPED_PROXY_VERSION").unwrap_or("devel".to_string());
+    println!("cargo:rustc-env=PINNIPED_PROXY_VERSION={}", version);
+}
