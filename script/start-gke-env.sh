9dd3beed9194057870fa9f6c1b008074c45ed360
Antonio Gamez Diaz <agamez@vmware.com>
2021-10-27 14:11:02 +0200

    Extract repo and branches to circle config
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- script/start-gke-env.sh
+++ script/start-gke-env.sh
@@ -32,18 +32,18 @@ fi
 if [[ $(gcloud container clusters list --filter="name:${CLUSTER}") ]]; then
     if gcloud container clusters list --filter="name:${CLUSTER}" | grep "STOPPING"; then
         cnt=300
-        while gcloud container clusters list | grep $CLUSTER; do
+        while gcloud container clusters list | grep "${CLUSTER}"; do
             ((cnt = cnt - 1)) || (echo "Waited 5m but cluster is still being deleted" && exit 1)
             sleep 1
         done
     else
         echo "GKE cluster already exits. Deleting it"
-        gcloud container clusters delete $CLUSTER --zone $ZONE
+        gcloud container clusters delete "${CLUSTER}" --zone "${ZONE}"
     fi
 fi
 
-echo "Creating cluster $CLUSTER in $ZONE (v$BRANCH)"
-gcloud container clusters create --cluster-version=${BRANCH} --zone $ZONE $CLUSTER --num-nodes 2 --machine-type=n1-standard-2 --preemptible --labels=team=kubeapps
+echo "Creating cluster ${CLUSTER} in ${ZONE} (v$BRANCH)"
+gcloud container clusters create --cluster-version="${BRANCH}" --zone "${ZONE}" "${CLUSTER}" --num-nodes 2 --machine-type=n1-standard-2 --preemptible --labels=team=kubeapps
 echo "Waiting for the cluster to respond..."
 cnt=20
 until kubectl get pods >/dev/null 2>&1; do
