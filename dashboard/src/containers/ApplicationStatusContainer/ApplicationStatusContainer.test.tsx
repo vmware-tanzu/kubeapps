fcfc4312d4406041ab82fc3ed3b3324ccf19b92f
Michael Nelson <minelson@vmware.com>
2021-11-30 17:51:53 +1100

    Fix lint.
    Signed-off-by: Michael Nelson <minelson@vmware.com>

--- dashboard/src/containers/ApplicationStatusContainer/ApplicationStatusContainer.test.tsx
+++ dashboard/src/containers/ApplicationStatusContainer/ApplicationStatusContainer.test.tsx
@@ -16,6 +16,7 @@ const makeStore = (resources: { [s: string]: IKubeItem<IResource> }) => {
     items: resources,
     kinds: initialKinds,
     sockets: {},
+    subscriptions: {},
     timers: {},
   };
   return mockStore({ kube: state, config: { featureFlags: {} } });
