e85449c917f90a376b695a791a3fba4f47a9cb23
Antonio Gámez <agamez@vmware.com>
2021-12-02 15:22:08 +0100

    Use cds-select in the deployment/upgrade views (#3880)
    * Use the cds select in the deployment/upgrade forms
    
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>
    
    * Use the pkg display name in the form header
    
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>
    
    * Fix test to get the dispaly name rather than id
    
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>
    
    * Remove old css code
    
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>
    
    * Edit e2e selector
    
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- dashboard/src/components/PackageHeader/PackageVersionSelector.tsx
+++ dashboard/src/components/PackageHeader/PackageVersionSelector.tsx
@@ -1,11 +1,14 @@
+import { CdsControlMessage } from "@cds/react/forms";
+import { CdsSelect } from "@cds/react/select";
 import { PackageAppVersion } from "gen/kubeappsapis/core/packages/v1alpha1/packages";
 import React from "react";
-
 interface IPackageVersionSelectorProps {
   versions: PackageAppVersion[];
   onSelect: (event: React.ChangeEvent<HTMLSelectElement>) => void;
   currentVersion?: string;
   selectedVersion?: string;
+  label?: JSX.Element | string;
+  message?: string;
 }
 
 export default function PackageVersionSelector({
@@ -13,24 +16,26 @@ export default function PackageVersionSelector({
   onSelect,
   currentVersion,
   selectedVersion,
+  label,
+  message,
 }: IPackageVersionSelectorProps) {
+  /* eslint-disable jsx-a11y/label-has-associated-control */
   return (
-    <div className="clr-select-wrapper">
+    <CdsSelect>
+      <label>{label}</label>
       <select
         name="package-versions"
-        className="clr-page-size-select"
-        onChange={onSelect}
         value={selectedVersion || currentVersion || (versions.length ? versions[0].pkgVersion : "")}
+        onChange={onSelect}
       >
-        {versions.map(v => {
-          return (
-            <option key={`package-version-selector-${v.pkgVersion}`} value={v.pkgVersion}>
-              {v.pkgVersion} / App Version {v.appVersion}
-              {currentVersion === v.pkgVersion ? " (current)" : ""}
-            </option>
-          );
-        })}
+        {versions.map(v => (
+          <option key={`package-version-selector-${v.pkgVersion}`} value={v.pkgVersion}>
+            {v.pkgVersion} / App Version {v.appVersion}
+            {currentVersion === v.pkgVersion ? " (current)" : ""}
+          </option>
+        ))}
       </select>
-    </div>
+      {message ? <CdsControlMessage>{message}</CdsControlMessage> : <></>}
+    </CdsSelect>
   );
 }
