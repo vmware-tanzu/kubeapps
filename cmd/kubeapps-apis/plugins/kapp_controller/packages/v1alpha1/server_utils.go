6c503395c0dcbf5c1fcfab59da5b98cee817a131
Antonio Gamez Diaz <agamez@vmware.com>
2022-01-13 18:50:47 +0100

    Remove unused code
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- cmd/kubeapps-apis/plugins/kapp_controller/packages/v1alpha1/server_utils.go
+++ cmd/kubeapps-apis/plugins/kapp_controller/packages/v1alpha1/server_utils.go
@@ -16,7 +16,6 @@ import (
 	"bufio"
 	"fmt"
 	"reflect"
-	"regexp"
 	"sort"
 	"strconv"
 	"strings"
@@ -108,23 +107,6 @@ func pageOffsetFromPageToken(pageToken string) (int, error) {
 	}
 
 	return int(offset), nil
-}
-
-// extracts the value for a key from a JSON-formatted string
-// body - the JSON-response as a string. Usually retrieved via the request body
-// key - the key for which the value should be extracted
-// returns - the value for the given key
-// https://stackoverflow.com/questions/17452722/how-to-get-the-key-value-from-a-json-string-in-go/37332972
-func extractValue(body string, key string) string {
-	keystr := "\"" + key + "\":[^,;\\]}]*"
-	r, _ := regexp.Compile(keystr)
-	match := r.FindString(body)
-	keyValMatch := strings.Split(match, ":")
-	value := ""
-	if len(keyValMatch) > 1 {
-		value = strings.ReplaceAll(keyValMatch[1], "\"", "")
-	}
-	return value
 }
 
 // defaultValuesFromSchema returns a yaml string with default values generated from an OpenAPI v3 Schema
