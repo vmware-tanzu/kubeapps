d251672f03ce486366b3bd11fa33bd8bbc17e2a6
Antonio Gamez Diaz <agamez@vmware.com>
2022-01-18 13:16:03 +0100

    Update copyright headers
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- dashboard/src/shared/Auth.ts
+++ dashboard/src/shared/Auth.ts
@@ -1,3 +1,6 @@
+// Copyright 2018-2022 the Kubeapps contributors.
+// SPDX-License-Identifier: Apache-2.0
+
 import { AxiosResponse } from "axios";
 import * as jwt from "jsonwebtoken";
 import { get } from "lodash";
