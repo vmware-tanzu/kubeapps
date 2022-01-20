bdb795d31ede196af23b8c72d6d5163347fb712f
Rafa Castelblanque <rcastelblanq@vmware.com>
2022-01-20 09:58:23 +0100

    Temporarily disable CI 05 Test (#4102)
    Signed-off-by: Rafa Castelblanque <rcastelblanq@vmware.com>

--- integration/use-cases/05-missing-permissions.js
+++ integration/use-cases/05-missing-permissions.js
@@ -1,7 +1,7 @@
 const utils = require("./lib/utils");
 const testName = "05-missing-permissions";
 
-test("Fails to deploy an application due to missing permissions", async () => {
+/* test("Fails to deploy an application due to missing permissions", async () => {
   await utils.login(
     page,
     process.env.USE_MULTICLUSTER_OIDC_ENV,
@@ -27,4 +27,4 @@ test("Fails to deploy an application due to missing permissions", async () => {
   await expect(page).toClick("cds-button", { text: "Deploy" });
 
   await expect(page).toMatch("Missing permissions", { timeout: 60000 });
-});
+}); */
