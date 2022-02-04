2315da3c655ce13134bdd9423ff69c12820037f4
Michael Nelson <minelson@vmware.com>
2022-02-04 16:45:15 +1100

    Call GetInstalledPackageResourceRefs with exp backoff.
    Signed-off-by: Michael Nelson <minelson@vmware.com>

--- dashboard/src/shared/InstalledPackage.ts
+++ dashboard/src/shared/InstalledPackage.ts
@@ -43,10 +43,20 @@ export class InstalledPackage {
 
   public static async GetInstalledPackageResourceRefs(
     installedPackageRef?: InstalledPackageReference,
+    initialWait: number = 500,
   ) {
-    return await this.coreClient().GetInstalledPackageResourceRefs({
-      installedPackageRef: installedPackageRef,
-    });
+    // TODO(minelson): The backend plugin may take care of waiting
+    // for the required data to become available in which case this
+    // can be removed.
+    // See https://github.com/kubeapps/kubeapps/issues/4213
+    // Note: initialWait is set with a default value so it can be
+    // tested with a value of 0 (because I couldn't get jest's mock
+    // timers to work with promises here.)
+    const fn = async () =>
+      this.coreClient().GetInstalledPackageResourceRefs({
+        installedPackageRef: installedPackageRef,
+      });
+    return await callWithRetry(fn, initialWait);
   }
 
   public static async CreateInstalledPackage(
@@ -105,3 +115,21 @@ export class InstalledPackage {
     } as DeleteInstalledPackageRequest);
   }
 }
+
+// Helpers to be able to call with a retry backing off exponentially
+// with 500ms, 1000ms, 2000ms, 4000ms etc.
+const wait = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+const callWithRetry = async (fn: any, initialWait: number, depth = 0): Promise<any> => {
+  try {
+    return await fn();
+  } catch (e) {
+    if (depth >= 4) {
+      throw e;
+    }
+    // Wait for initialWait ms first time, doubling each time.
+    await wait(initialWait * 2 ** depth);
+
+    return callWithRetry(fn, initialWait, depth + 1);
+  }
+};
