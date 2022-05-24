89df18fc0fabdc6b41572e222fe5a3642dee84ea
Michael Nelson <minelson@vmware.com>
2022-05-24 13:21:03 +1000

    Update the token-auth example for getting-started.
    Signed-off-by: Michael Nelson <minelson@vmware.com>

--- script/makefiles/cluster-kind.mk
+++ script/makefiles/cluster-kind.mk
@@ -6,6 +6,7 @@
 KUBE ?= ${HOME}/.kube
 CLUSTER_NAME ?= kubeapps
 ADDITIONAL_CLUSTER_NAME ?= kubeapps-additional
+IMAGE ?= kindest/node:v1.24.0
 
 CLUSTER_CONFIG = ${KUBE}/kind-config-${CLUSTER_NAME}
 ADDITIONAL_CLUSTER_CONFIG = ${KUBE}/kind-config-${ADDITIONAL_CLUSTER_NAME}
@@ -14,6 +15,7 @@ ADDITIONAL_CLUSTER_CONFIG = ${KUBE}/kind-config-${ADDITIONAL_CLUSTER_NAME}
 # but is sufficient for the pod to be created so that we can copy the certs below.
 ${CLUSTER_CONFIG}:
 	kind create cluster \
+		--image ${IMAGE} \
 		--kubeconfig ${CLUSTER_CONFIG} \
 		--name ${CLUSTER_NAME} \
 		--config=./site/content/docs/latest/reference/manifests/kubeapps-local-dev-apiserver-config.yaml \
