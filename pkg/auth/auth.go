21322dc2581fb9115a8d9890dfbc5da1298747d6
Antonio Gamez Diaz <agamez@vmware.com>
2022-06-09 00:15:05 +0200

    Clean pkg/auth dir
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -4,64 +4,9 @@
 package auth
 
 import (
-	"context"
 	"fmt"
 	"regexp"
-	"strings"
-
-	"github.com/vmware-tanzu/kubeapps/pkg/kube"
-	yamlUtils "github.com/vmware-tanzu/kubeapps/pkg/yaml"
-	authorizationapi "k8s.io/api/authorization/v1"
-	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	discovery "k8s.io/client-go/discovery"
-	"k8s.io/client-go/kubernetes"
-	authorizationv1 "k8s.io/client-go/kubernetes/typed/authorization/v1"
-	"k8s.io/client-go/rest"
 )
-
-type resource struct {
-	APIVersion string
-	Kind       string
-	Namespace  string
-}
-
-type k8sAuthInterface interface {
-	GetResourceList(groupVersion string) (*metav1.APIResourceList, error)
-	CanI(verb, group, resource, namespace string) (bool, error)
-}
-
-type k8sAuth struct {
-	AuthCli      authorizationv1.AuthorizationV1Interface
-	DiscoveryCli discovery.DiscoveryInterface
-}
-
-func (u k8sAuth) GetResourceList(groupVersion string) (*metav1.APIResourceList, error) {
-	return u.DiscoveryCli.ServerResourcesForGroupVersion(groupVersion)
-}
-
-func (u k8sAuth) CanI(verb, group, resource, namespace string) (bool, error) {
-	attr := &authorizationapi.ResourceAttributes{
-		Group:     group,
-		Resource:  resource,
-		Verb:      verb,
-		Namespace: namespace,
-	}
-	res, err := u.AuthCli.SelfSubjectAccessReviews().Create(context.TODO(), &authorizationapi.SelfSubjectAccessReview{
-		Spec: authorizationapi.SelfSubjectAccessReviewSpec{
-			ResourceAttributes: attr,
-		},
-	}, metav1.CreateOptions{})
-	if err != nil {
-		return false, err
-	}
-	return res.Status.Allowed, nil
-}
-
-// UserAuth contains information to check user permissions
-type UserAuth struct {
-	k8sAuth k8sAuthInterface
-}
 
 // Action represents a specific set of verbs against a resource
 type Action struct {
@@ -70,131 +15,6 @@ type Action struct {
 	Namespace   string   `json:"namespace"`
 	ClusterWide bool     `json:"clusterWide"`
 	Verbs       []string `json:"verbs"`
-}
-
-// Checker for the exported funcs
-type Checker interface {
-	ValidateForNamespace(namespace string) (bool, error)
-	GetForbiddenActions(namespace, action, manifest string) ([]Action, error)
-}
-
-// NewAuth creates an auth agent
-func NewAuth(token, clusterName string, clustersConfig kube.ClustersConfig) (*UserAuth, error) {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		return nil, err
-	}
-	config, err = kube.NewClusterConfig(config, token, clusterName, clustersConfig)
-	if err != nil {
-		return nil, err
-	}
-	kubeClient, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
-	authCli := kubeClient.AuthorizationV1()
-	discoveryCli := kubeClient.Discovery()
-	k8sAuthCli := k8sAuth{
-		AuthCli:      authCli,
-		DiscoveryCli: discoveryCli,
-	}
-
-	return &UserAuth{k8sAuthCli}, nil
-}
-
-// ValidateForNamespace checks if the user can access secrets in the given
-// namespace, as a check of whether they can view the namespace.
-func (u *UserAuth) ValidateForNamespace(namespace string) (bool, error) {
-	return u.k8sAuth.CanI("get", "", "secrets", namespace)
-}
-
-type resourceInfo struct {
-	Name       string
-	Namespaced bool
-}
-
-func (u *UserAuth) resolve(groupVersion, kind string) (resourceInfo, error) {
-	resourceList, err := u.k8sAuth.GetResourceList(groupVersion)
-	if err != nil {
-		return resourceInfo{}, err
-	}
-	for _, r := range resourceList.APIResources {
-		if r.Kind == kind {
-			return resourceInfo{r.Name, r.Namespaced}, nil
-		}
-	}
-	return resourceInfo{}, fmt.Errorf("Unable to find the kind %s in the resource group %s", kind, groupVersion)
-}
-
-func (u *UserAuth) getResourcesToCheck(namespace, manifest string) ([]resource, error) {
-	objs, err := yamlUtils.ParseObjects(manifest)
-	if err != nil {
-		return []resource{}, err
-	}
-	resourcesToCheck := map[string]*resource{}
-	result := []resource{}
-	for _, obj := range objs {
-		// Object can specify a different namespace, if not use the default one
-		ns := obj.GetNamespace()
-		if ns == "" {
-			ns = namespace
-		}
-		resourceToCheck := fmt.Sprintf("%s/%s/%s", ns, obj.GetAPIVersion(), obj.GetKind())
-		if resourcesToCheck[resourceToCheck] == nil {
-			r := resource{obj.GetAPIVersion(), obj.GetKind(), ns}
-			resourcesToCheck[resourceToCheck] = &r
-			result = append(result, r)
-		}
-	}
-	return result, nil
-}
-
-func (u *UserAuth) isAllowed(verb string, itemsToCheck []resource) ([]Action, error) {
-	rejectedActions := []Action{}
-	for _, i := range itemsToCheck {
-		rInfo, err := u.resolve(i.APIVersion, i.Kind)
-		if err != nil {
-			if k8sErrors.IsNotFound(err) {
-				// The resource version/kind is not registered in the k8s API so
-				// we assume it's a CRD that is going to be created with the chart
-				// In any case, if a chart tries to install a resource that doesn't
-				// exist it's fine to ignore it here since the installation will fail
-				continue
-			}
-			return []Action{}, err
-		}
-		group := i.APIVersion
-		if group == "v1" {
-			// The group should be empty for the core API group
-			group = ""
-		}
-		allowed, err := u.k8sAuth.CanI(verb, group, rInfo.Name, i.Namespace)
-		if err != nil {
-			return []Action{}, err
-		}
-		// If the "group" is versioned the user may be able to have access to any
-		// version of the group but the above call may return "false"
-		if !allowed && strings.Contains(group, "/") {
-			groupID := strings.Split(group, "/")[0]
-			allowed, err = u.k8sAuth.CanI(verb, groupID, rInfo.Name, i.Namespace)
-			if err != nil {
-				return []Action{}, err
-			}
-		}
-		if !allowed {
-			rejectedAction := Action{
-				APIVersion:  i.APIVersion,
-				Resource:    rInfo.Name,
-				Verbs:       []string{verb},
-				ClusterWide: !rInfo.Namespaced,
-			}
-			if rInfo.Namespaced {
-				rejectedAction.Namespace = i.Namespace
-			}
-			rejectedActions = append(rejectedActions, rejectedAction)
-		}
-	}
-	return rejectedActions, nil
 }
 
 func uniqVerbs(current []string, new []string) []string {
@@ -235,36 +55,6 @@ func reduceActionsByVerb(actions []Action) []Action {
 		res = append(res, a)
 	}
 	return res
-}
-
-// GetForbiddenActions parses a K8s manifest and checks if the current user can do the action given
-// over all the elements of the manifest. It return the list of forbidden Actions if any.
-func (u *UserAuth) GetForbiddenActions(namespace, action, manifest string) ([]Action, error) {
-	resources, err := u.getResourcesToCheck(namespace, manifest)
-	forbiddenActions := []Action{}
-	if err != nil {
-		return []Action{}, err
-	}
-	switch action {
-	case "upgrade":
-		// For upgrading a chart the user should be able to create, update and delete resources
-		for _, v := range []string{"create", "update", "delete"} {
-			actions, err := u.isAllowed(v, resources)
-			if err != nil {
-				return []Action{}, err
-			}
-			forbiddenActions = append(forbiddenActions, actions...)
-		}
-		if len(forbiddenActions) > 0 {
-			forbiddenActions = reduceActionsByVerb(forbiddenActions)
-		}
-	default:
-		forbiddenActions, err = u.isAllowed(action, resources)
-		if err != nil {
-			return []Action{}, err
-		}
-	}
-	return forbiddenActions, nil
 }
 
 // ParseForbiddenActions parses a forbidden error returned by the Kubernetes API and return the list of forbidden actions
--- pkg/auth/authgate.go
+++ pkg/auth/authgate.go
@@ -4,74 +4,11 @@
 package auth
 
 import (
-	"fmt"
-	"net/http"
 	"strings"
-
-	"github.com/gorilla/mux"
-	negroni "github.com/urfave/negroni/v2"
-	"github.com/vmware-tanzu/kubeapps/pkg/dbutils"
-	"github.com/vmware-tanzu/kubeapps/pkg/kube"
-	"github.com/vmware-tanzu/kubeapps/pkg/response"
 )
 
 // tokenPrefix is the string preceding the token in the Authorization header.
 const tokenPrefix = "Bearer "
-
-// CheckerForRequest defines a function type so we can also inject a fake for tests
-// rather than setting a context value.
-type CheckerForRequest func(clustersConfig kube.ClustersConfig, req *http.Request) (Checker, error)
-
-func AuthCheckerForRequest(clustersConfig kube.ClustersConfig, req *http.Request) (Checker, error) {
-	token := ExtractToken(req.Header.Get("Authorization"))
-	if token == "" {
-		return nil, fmt.Errorf("Authorization token missing")
-	}
-	clusterName := mux.Vars(req)["cluster"]
-	return NewAuth(token, clusterName, clustersConfig)
-}
-
-// AuthGate implements middleware to check if the user has access to charts from
-// the specific namespace before continuing.
-//   * If the path being handled by the
-//     AuthGate middleware does not include the 'namespace' mux var, or the value
-//     is _all, then the check is for cluster-wide access.
-//   * If the namespace is the global chart namespace (ie. kubeappsNamespace) then
-//     we allow read access regardless.
-func AuthGate(clustersConfig kube.ClustersConfig, kubeappsNamespace string) negroni.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
-		userAuth, err := AuthCheckerForRequest(clustersConfig, req)
-		if err != nil {
-			response.NewErrorResponse(http.StatusUnauthorized, err.Error()).Write(w)
-			return
-		}
-		namespace := mux.Vars(req)["namespace"]
-		if namespace == dbutils.AllNamespaces {
-			namespace = ""
-		}
-
-		// The auth-gate is used only for access to the asset-svc and the functionality should be
-		// moved to the assetsvc itself if and when the assetsvc is updated to be cluster aware.
-		authz := false
-		// TODO(absoludity): Update to allow access to assets from the global kubeapps namespace
-		// on the kubeapps cluster only. See #2037.
-		if namespace == kubeappsNamespace {
-			authz = true
-		} else {
-			authz, err = userAuth.ValidateForNamespace(namespace)
-		}
-
-		if err != nil || !authz {
-			msg := fmt.Sprintf("Unable to validate user for namespace %q", namespace)
-			if err != nil {
-				msg = fmt.Sprintf("%s: %s", msg, err.Error())
-			}
-			response.NewErrorResponse(http.StatusForbidden, msg).Write(w)
-			return
-		}
-		next(w, req)
-	}
-}
 
 // ExtractToken extracts the token from a correctly formatted Authorization header.
 func ExtractToken(headerValue string) string {
