488c88a9f9a4458eb9977cb9aff54572c7257bd3
Antonio Gamez Diaz <agamez@vmware.com>
2022-07-12 12:44:08 +0200

    Fix errata
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- cmd/kubeapps-apis/core/plugins/v1alpha1/plugins_test.go
+++ cmd/kubeapps-apis/core/plugins/v1alpha1/plugins_test.go
@@ -423,9 +423,10 @@ func TestCreateConfigGetterWithParams(t *testing.T) {
 			}
 
 			if tc.expectedErrMsg == nil {
-				if restConfig == nil && restConfig.Host != "" {
+				if restConfig == nil || restConfig.Host != "" {
 					t.Errorf("got: nil, want: rest.Config")
-				} else if got, want := restConfig.Host, tc.expectedAPIHost; got != want {
+				}
+				if got, want := restConfig.Host, tc.expectedAPIHost; got != want {
 					t.Errorf("got: %q, want: %q", got, want)
 				}
 			}
