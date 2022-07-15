7c05e3bb749cc7988a665fd198f767a28a2cf4a7
Rafa Castelblanque <rcastelblanq@vmware.com>
2022-07-15 15:14:17 +0200

    Regenerated code after merge from main
    Signed-off-by: Rafa Castelblanque <rcastelblanq@vmware.com>

--- dashboard/src/gen/google/api/annotations.ts
+++ dashboard/src/gen/google/api/annotations.ts
@@ -1,10 +1,2 @@
 /* eslint-disable */
-import Long from "long";
-import * as _m0 from "protobufjs/minimal";
-
 export const protobufPackage = "google.api";
-
-if (_m0.util.Long !== Long) {
-  _m0.util.Long = Long as any;
-  _m0.configure();
-}
