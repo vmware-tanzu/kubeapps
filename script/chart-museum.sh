7f2941e58a5db3ca58b7ca0fd1b6d38fab759488
Rafa Castelblanque <rcastelblanq@vmware.com>
2022-08-01 09:47:10 +0200

    Added comments from PR
    Signed-off-by: Rafa Castelblanque <rcastelblanq@vmware.com>

--- script/chart-museum.sh
+++ script/chart-museum.sh
@@ -3,13 +3,16 @@
 # Copyright 2022 the Kubeapps contributors.
 # SPDX-License-Identifier: Apache-2.0
 
-CM_PORT=${CM_PORT:-8090}
-CM_USER=${CM_USER:-"admin"}
-CM_PWD=${CM_PWD:-"password"}
-CHART_MUSEUM_NS=${CHART_MUSEUM_NS:-"chart-museum"}
-CM_VERSION=${CM_VERSION:-"3.9.0"}
+CHARTMUSEUM_PORT=${CHARTMUSEUM_PORT:-8090}
+CHARTMUSEUM_USER=${CHARTMUSEUM_USER:-"admin"}
+CHARTMUSEUM_PWD=${CHARTMUSEUM_PWD:-"password"}
+CHARTMUSEUM_NS=${CHARTMUSEUM_NS:-"chart-museum"}
+CHARTMUSEUM_VERSION=${CHARTMUSEUM_VERSION:-"3.9.0"}
 
 # Pull a Bitnami chart to a local TGZ file
+# Arguments:
+#   $1: Chart name
+#   $2: Chart version
 pullBitnamiChart() {
   if [ -z "$1" ]; then
     echo "No Bitnami chart name supplied"
@@ -32,6 +35,10 @@ pullBitnamiChart() {
 }
 
 # Push a local chart to ChartsMuseum
+# Arguments:
+#   $1: Chart name
+#   $2: Chart version
+#   $3: Chart file
 pushChartToChartMuseum() {
   if [ -z "$1" ]; then
     echo "No chart name supplied"
@@ -52,45 +59,45 @@ pushChartToChartMuseum() {
 
   echo "Pushing chart ${CHART_NAME} v${CHART_VERSION} to chart museum"
 
-  local CM_POD_NAME=$(kubectl get pods --namespace ${CHART_MUSEUM_NS} -l "app.kubernetes.io/name=chartmuseum" -o jsonpath="{.items[0].metadata.name}")
-  /bin/sh -c "kubectl port-forward $CM_POD_NAME ${CM_PORT}:8080 --namespace ${CHART_MUSEUM_NS} &"
+  local CHARTMUSEUM_POD_NAME=$(kubectl get pods --namespace ${CHARTMUSEUM_NS} -l "app.kubernetes.io/name=chartmuseum" -o jsonpath="{.items[0].metadata.name}")
+  /bin/sh -c "kubectl port-forward $CHARTMUSEUM_POD_NAME ${CHARTMUSEUM_PORT}:8080 --namespace ${CHARTMUSEUM_NS} &"
   sleep 2
 
-  CHART_EXISTS=$(curl -u "${CM_USER}:${CM_PWD}" -X GET http://localhost:${CM_PORT}/api/charts/${CHART_NAME}/${CHART_VERSION} | jq -r 'any([ .error] ; . > 0)')
+  CHART_EXISTS=$(curl -u "${CHARTMUSEUM_USER}:${CHARTMUSEUM_PWD}" -X GET http://localhost:${CHARTMUSEUM_PORT}/api/charts/${CHART_NAME}/${CHART_VERSION} | jq -r 'any([ .error] ; . > 0)')
   if [ "$CHART_EXISTS" == "true" ]; then
     echo ">> CHART EXISTS: deleting"
-    curl -u "${CM_USER}:${CM_PWD}" -X DELETE http://localhost:${CM_PORT}/api/charts/${CHART_NAME}/${CHART_VERSION}
+    curl -u "${CHARTMUSEUM_USER}:${CHARTMUSEUM_PWD}" -X DELETE http://localhost:${CHARTMUSEUM_PORT}/api/charts/${CHART_NAME}/${CHART_VERSION}
   fi
   
   echo ">> Uploading chart from file ${CHART_FILE}"
-  curl -u "${CM_USER}:${CM_PWD}" --data-binary "@${CHART_FILE}" http://localhost:${CM_PORT}/api/charts  
+  curl -u "${CHARTMUSEUM_USER}:${CHARTMUSEUM_PWD}" --data-binary "@${CHART_FILE}" http://localhost:${CHARTMUSEUM_PORT}/api/charts  
   
   # End port forward
-  pkill -f "kubectl port-forward $CM_POD_NAME ${CM_PORT}:8080 --namespace ${CHART_MUSEUM_NS}"
+  pkill -f "kubectl port-forward $CHARTMUSEUM_POD_NAME ${CHARTMUSEUM_PORT}:8080 --namespace ${CHARTMUSEUM_NS}"
 
   rm ${CHART_FILE}
 }
 
 # Install ChartsMuseum
 installChartMuseum() {
-  echo "Installing ChartMuseum ${CM_VERSION}..."
-  helm install chartmuseum --namespace ${CHART_MUSEUM_NS} --create-namespace "https://github.com/chartmuseum/charts/releases/download/chartmuseum-${CM_VERSION}/chartmuseum-${CM_VERSION}.tgz" \
+  echo "Installing ChartMuseum ${CHARTMUSEUM_VERSION}..."
+  helm install chartmuseum --namespace ${CHARTMUSEUM_NS} --create-namespace "https://github.com/chartmuseum/charts/releases/download/chartmuseum-${CHARTMUSEUM_VERSION}/chartmuseum-${CHARTMUSEUM_VERSION}.tgz" \
     --set env.open.DISABLE_API=false \
     --set persistence.enabled=true \
-    --set env.secret.BASIC_AUTH_USER=$CM_USER \
-    --set env.secret.BASIC_AUTH_PASS=$CM_PWD
-  kubectl rollout status -w deployment/chartmuseum --namespace=${CHART_MUSEUM_NS}
+    --set env.secret.BASIC_AUTH_USER=$CHARTMUSEUM_USER \
+    --set env.secret.BASIC_AUTH_PASS=$CHARTMUSEUM_PWD
+  kubectl rollout status -w deployment/chartmuseum --namespace=${CHARTMUSEUM_NS}
 
-  echo "Chart museum v${CM_VERSION} installed in namespace ${CHART_MUSEUM_NS}"
-  echo "Credentials: ${CM_USER} / ${CM_PWD}"
+  echo "Chart museum v${CHARTMUSEUM_VERSION} installed in namespace ${CHARTMUSEUM_NS}"
+  echo "Credentials: ${CHARTMUSEUM_USER} / ${CHARTMUSEUM_PWD}"
   echo "Cluster internal URL: "
-  echo "    http://chartmuseum.${CHART_MUSEUM_NS}.svc.cluster.local:8080/"
+  echo "    http://chartmuseum.${CHARTMUSEUM_NS}.svc.cluster.local:8080/"
 }
 
 # Uninstall ChartsMuseum
 uninstallChartMuseum() {
   echo "Uninstalling ChartMuseum..."
-  helm uninstall chartmuseum --namespace ${CHART_MUSEUM_NS}
+  helm uninstall chartmuseum --namespace ${CHARTMUSEUM_NS}
 }
 
 case $1 in
