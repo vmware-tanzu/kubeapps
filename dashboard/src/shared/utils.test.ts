f298db6ea6a0df3b08fed307dd581840be68e7d7
Antonio Gamez Diaz <agamez@vmware.com>
2022-08-04 12:56:40 +0200

    Fix failing test case after merge
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- dashboard/src/shared/utils.test.ts
+++ dashboard/src/shared/utils.test.ts
@@ -2,11 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { InstalledPackageStatus_StatusReason } from "gen/kubeappsapis/core/packages/v1alpha1/packages";
-import carvelIcon from "../icons/carvel.svg";
-import fluxIcon from "../icons/flux.svg";
-import helmIcon from "../icons/helm.svg";
-import olmIcon from "../icons/olm-icon.svg";
-import placeholder from "../placeholder.png";
+import carvelIcon from "icons/carvel.svg";
+import fluxIcon from "icons/flux.svg";
+import helmIcon from "icons/helm.svg";
+import olmIcon from "icons/olm-icon.svg";
+import placeholder from "icons/placeholder.svg";
 import {
   escapeRegExp,
   getAppStatusLabel,
