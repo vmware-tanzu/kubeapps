2d86d94e696dddbb75b3891da11419a1d0054389
Rafa Castelblanque <rcastelblanq@vmware.com>
2022-09-01 15:39:51 +0200

    Workaround for values encoding in Helm
    Signed-off-by: Rafa Castelblanque <rcastelblanq@vmware.com>

--- script/e2e-test.sh
+++ script/e2e-test.sh
@@ -195,6 +195,27 @@ installKappController() {
 }
 
 ########################
+# Creates a Yaml file with additional values for the Helm chart
+# Arguments: None
+# Returns: Path to the newly created file with additional values
+#########################
+generateAdditionalValuesFile() {
+  # Could be done better with $(cat <<EOF > ${ROOT_DIR}/additional_chart_values.yaml
+  # But it was breaking the formatting of the file
+  local valuesFile=${ROOT_DIR}/additional_chart_values.yaml;
+  echo "ingress:
+  hostname: localhost
+  tls: true
+  selfSigned: true
+  annotations:
+    kubernetes.io/ingress.class: nginx
+    nginx.ingress.kubernetes.io/proxy-buffer-size: 8k
+    nginx.ingress.kubernetes.io/proxy-buffers: 4.0
+    nginx.ingress.kubernetes.io/proxy-read-timeout: 600.0" > ${valuesFile}
+  echo ${valuesFile}
+}
+
+########################
 # Install Kubeapps or upgrades it if it's already installed
 # Arguments:
 #   $1: chart source
@@ -260,16 +281,12 @@ img_flags=(
   "--set" "kubeappsapis.image.repository=${images[5]}"
 )
 
+additional_flags_file=$(generateAdditionalValuesFile)
+
 if [ "$USE_MULTICLUSTER_OIDC_ENV" = true ]; then
   multiclusterFlags=(
     "--set" "ingress.enabled=true"
-    "--set" "ingress.hostname=localhost"
-    "--set" "ingress.tls=true"
-    "--set" "ingress.selfSigned=true"
-    "--set" "ingress.annotations.\"kubernetes\.io\/ingress\.class\"=nginx"
-    "--set" "ingress.annotations.\"nginx\.ingress\.kubernetes\.io\/proxy-buffer-size\"=8k"
-    "--set" "ingress.annotations.\"nginx\.ingress\.kubernetes\.io\/proxy-read-timeout\"=600.0"
-    "--set" "ingress.annotations.\"nginx\.ingress\.kubernetes\.io\/proxy-buffers\"=4.0"
+    "--values" "${additional_flags_file}"
     "--set" "authProxy.enabled=true"
     "--set" "authProxy.provider=oidc"
     "--set" "authProxy.clientID=default"
