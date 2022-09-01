84af47ff09b72d39829ab54f433cb7901c598ef3
Rafa Castelblanque <rcastelblanq@vmware.com>
2022-09-01 15:50:40 +0200

    Bring back original line
    Signed-off-by: Rafa Castelblanque <rcastelblanq@vmware.com>

--- integration/tests/carvel/04-default-deployment.spec.js
+++ integration/tests/carvel/04-default-deployment.spec.js
@@ -24,8 +24,7 @@ test("Deploys package with default values in main cluster", async ({ page }) => 
 
   // Deploy package
   await page.waitForSelector('select[name="package-versions"]');
-  const versionSelector = await page.$('select[name="package-versions"]')
-  await versionSelector?.selectOption("2.0.0")
+  await page.selectOption('select[name="package-versions"]', "2.0.0");
   const releaseNameLocator = page.locator("#releaseName");
   await releaseNameLocator.waitFor();
   await expect(releaseNameLocator).toHaveText("");
