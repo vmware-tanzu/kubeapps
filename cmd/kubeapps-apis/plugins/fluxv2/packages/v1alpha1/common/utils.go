c94d64ae271ae9d0acea39427dbdfb272f4f69f7
gfichtenholt <gfichtenholt@vmware.com>
2022-09-22 15:26:53 -0700

    try to fix code QL error

--- cmd/kubeapps-apis/plugins/fluxv2/packages/v1alpha1/common/utils.go
+++ cmd/kubeapps-apis/plugins/fluxv2/packages/v1alpha1/common/utils.go
@@ -364,6 +364,12 @@ func OCIChartRepositoryCredentialFromSecret(registryURL string, secret apiv1.Sec
 	case username == "" || password == "":
 		return nil, fmt.Errorf("invalid '%s' secret data: required fields 'username' and 'password'", secret.Name)
 	}
+
+	pwdRedacted := password
+	if len(pwdRedacted) > 4 {
+		pwdRedacted = pwdRedacted[0:3] + "..."
+	}
+	log.Infof("-OCIChartRepositoryCredentialFromSecret: username: [%s], password: [%s]", username, pwdRedacted)
 	return &orasregistryauthv2.Credential{
 		Username: username,
 		Password: password,
@@ -390,6 +396,11 @@ func OIDCAdaptHelper(authenticator authn.Authenticator) (*orasregistryauthv2.Cre
 	case username == "" || password == "":
 		return nil, fmt.Errorf("invalid auth data: required fields 'username' and 'password'")
 	}
+	pwdRedacted := password
+	if len(pwdRedacted) > 4 {
+		pwdRedacted = pwdRedacted[0:3] + "..."
+	}
+	log.Infof("-OIDCAdaptHelper: username: [%s], password: [%s]", username, pwdRedacted)
 	return &orasregistryauthv2.Credential{
 		Username: username,
 		Password: password,
