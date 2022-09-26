88cf3476348a481128bb49d0f03b47fe338303d8
Antonio Gamez Diaz <agamez@vmware.com>
2022-09-26 14:19:18 +0200

    Replace ace with monaco deps
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- dashboard/craco.config.js
+++ dashboard/craco.config.js
@@ -2,7 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 const webpack = require("webpack");
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
 module.exports = {
   webpack: {
@@ -11,12 +12,13 @@ module.exports = {
         // add the required polyfills (not included in webpack 5)
         new NodePolyfillPlugin({
           // Allow using console.log
-          excludeAliases: ['console'],
+          excludeAliases: ["console"],
         }),
         new webpack.ProvidePlugin({
-          process: 'process/browser.js',
-          Buffer: ['buffer', 'Buffer'],
+          process: "process/browser.js",
+          Buffer: ["buffer", "Buffer"],
         }),
+        new MonacoWebpackPlugin(),
       ],
       ignoreWarnings: [/Failed to parse source map/], // ignore source map warnings
     },
