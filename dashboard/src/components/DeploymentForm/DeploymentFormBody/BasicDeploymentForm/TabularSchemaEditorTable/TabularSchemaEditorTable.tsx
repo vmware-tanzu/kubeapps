e73bffd71a6276b9b1a694e9ef01155a29e2efa4
Antonio Gamez Diaz <agamez@vmware.com>
2022-09-29 16:40:04 +0200

    Add new json-schema tabular view
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

---
+++ dashboard/src/components/DeploymentForm/DeploymentFormBody/BasicDeploymentForm/TabularSchemaEditorTable/TabularSchemaEditorTable.tsx
@@ -0,0 +1,251 @@
+// Copyright 2022 the Kubeapps contributors.
+// SPDX-License-Identifier: Apache-2.0
+
+import { CdsButton } from "@cds/react/button";
+import { CdsSelect } from "@cds/react/select";
+import {
+  ColumnFiltersState,
+  ExpandedState,
+  flexRender,
+  getCoreRowModel,
+  getExpandedRowModel,
+  getFacetedMinMaxValues,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import Column from "components/js/Column";
+import Row from "components/js/Row";
+import LoadingWrapper from "components/LoadingWrapper";
+import { useState } from "react";
+import DebouncedInput from "./DebouncedInput";
+import { fuzzyFilter } from "./TabularSchemaEditorTableHelpers";
+import "./TabularSchemaEditorTable.css";
+import { IBasicFormParam } from "shared/types";
+
+export interface TabularSchemaEditorTableProps {
+  columns: any;
+  data: IBasicFormParam[];
+  globalFilter: any;
+  setGlobalFilter: any;
+  isLoading: boolean;
+  saveAllChanges: () => void;
+}
+
+export default function TabularSchemaEditorTable(props: TabularSchemaEditorTableProps) {
+  const { columns, data, globalFilter, setGlobalFilter, isLoading, saveAllChanges } = props;
+
+  // Component state
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [globalExpanded, setGlobalExpanded] = useState<ExpandedState>({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    filterFns: {
+      fuzzy: fuzzyFilter,
+    },
+    state: {
+      columnFilters,
+      globalFilter,
+      expanded: globalExpanded,
+    },
+    autoResetPageIndex: false,
+    getCoreRowModel: getCoreRowModel(),
+    getExpandedRowModel: getExpandedRowModel(),
+    getFacetedMinMaxValues: getFacetedMinMaxValues(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getSubRows: (row: IBasicFormParam) => row.params,
+    globalFilterFn: fuzzyFilter,
+    onColumnFiltersChange: setColumnFilters,
+    onExpandedChange: setGlobalExpanded,
+    onGlobalFilterChange: setGlobalFilter,
+    enableColumnResizing: true,
+  });
+
+  const paginationButtons = (
+    <>
+      <div>
+        <CdsButton
+          title="First page"
+          style={{ marginRight: "0.5em" }}
+          action="solid"
+          status="primary"
+          type="button"
+          size="sm"
+          onClick={() => {
+            saveAllChanges();
+            table.setPageIndex(0);
+          }}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<<"}
+        </CdsButton>
+        <CdsButton
+          title="Previous Page"
+          style={{ marginRight: "0.5em" }}
+          action="solid"
+          status="primary"
+          type="button"
+          size="sm"
+          onClick={() => {
+            saveAllChanges();
+            table.previousPage();
+          }}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<"}
+        </CdsButton>
+
+        <CdsButton
+          style={{ marginRight: "0.5em" }}
+          action="flat"
+          status="neutral"
+          type="button"
+          size="sm"
+        >
+          <span>
+            Page{" "}
+            <strong>
+              {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+            </strong>
+          </span>
+        </CdsButton>
+        <CdsButton
+          title="Next Page"
+          style={{ marginRight: "0.5em" }}
+          action="solid"
+          status="primary"
+          type="button"
+          size="sm"
+          onClick={() => {
+            saveAllChanges();
+            table.nextPage();
+          }}
+          disabled={!table.getCanNextPage()}
+        >
+          {">"}
+        </CdsButton>
+        <CdsButton
+          title="Last Page"
+          style={{ marginRight: "0.5em" }}
+          action="solid"
+          status="primary"
+          type="button"
+          size="sm"
+          onClick={() => {
+            saveAllChanges();
+            table.setPageIndex(table.getPageCount() - 1);
+          }}
+          disabled={!table.getCanNextPage()}
+        >
+          {">>"}
+        </CdsButton>
+      </div>
+    </>
+  );
+
+  const topButtons = (
+    <>
+      <div className="table-control">
+        <Row>
+          <Column span={8}>{paginationButtons}</Column>
+          <Column span={4}>
+            <DebouncedInput
+              title="Search"
+              value={globalFilter ?? ""}
+              onChange={value => {
+                saveAllChanges();
+                setGlobalFilter(String(value));
+              }}
+              placeholder="Type to search by key..."
+            />
+          </Column>
+        </Row>
+      </div>
+    </>
+  );
+  const bottomButtons = (
+    <>
+      <div className="table-control">
+        <Row>
+          <Column span={8}>{paginationButtons}</Column>
+          <Column span={4}>
+            <CdsSelect>
+              <label htmlFor="page-size">Page size</label>
+              <select
+                id="page-size"
+                value={table.getState().pagination.pageSize}
+                onChange={e => {
+                  saveAllChanges();
+                  table.setPageSize(Number(e.target.value));
+                }}
+              >
+                {[10, 20, 30, 40, 50].map(pageSize => (
+                  <option key={pageSize} value={pageSize}>
+                    Show {pageSize}
+                  </option>
+                ))}
+              </select>
+            </CdsSelect>
+          </Column>
+        </Row>
+      </div>
+    </>
+  );
+
+  const tableHeader = table.getHeaderGroups().map((headerGroup: any) => (
+    <tr key={headerGroup.id}>
+      {headerGroup.headers.map((header: any) => (
+        <th key={header.id}>
+          {header.isPlaceholder
+            ? null
+            : flexRender(header.column.columnDef.header, header.getContext())}
+        </th>
+      ))}
+    </tr>
+  ));
+
+  const tableBody = table.getRowModel().rows.map((row: any) => (
+    <tr key={row.id}>
+      {row.getVisibleCells().map((cell: any) => (
+        <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+      ))}
+    </tr>
+  ));
+
+  const tableFooter = table.getFooterGroups().map((footerGroup: any) => (
+    <tr key={footerGroup.id}>
+      {footerGroup.headers.map((header: any) => (
+        <th key={header.id}>
+          {header.isPlaceholder
+            ? null
+            : flexRender(header.column.columnDef.footer, header.getContext())}
+        </th>
+      ))}
+    </tr>
+  ));
+
+  const tableObject = (
+    <table className="table table-valign-center">
+      <thead>{tableHeader}</thead>
+      <tbody>{tableBody}</tbody>
+      <tfoot>{tableFooter}</tfoot>
+    </table>
+  );
+
+  return (
+    <LoadingWrapper loaded={!isLoading}>
+      {topButtons}
+      {tableObject}
+      {bottomButtons}
+    </LoadingWrapper>
+  );
+}
---
+++ dashboard/src/components/DeploymentForm/DeploymentFormBody/BasicDeploymentForm/TabularSchemaEditorTable/TabularSchemaEditorTableRenderer.tsx
@@ -0,0 +1,219 @@
+import { CdsButton } from "@cds/react/button";
+import { CdsIcon } from "@cds/react/icon";
+import ReactTooltip from "react-tooltip";
+import { IBasicFormParam } from "shared/types";
+import ArrayParam from "./Params/ArrayParam";
+import BooleanParam from "./Params/BooleanParam";
+import CustomFormComponentLoader from "./Params/CustomFormParam";
+import SliderParam from "./Params/SliderParam";
+import TextParam from "./Params/TextParam";
+
+const MAX_LENGTH = 60;
+
+function renderCellWithTooltip(
+  value: IBasicFormParam,
+  property: string,
+  className = "",
+  trimFromBeginning = false,
+  maxLength = MAX_LENGTH,
+) {
+  // If the value is an object/array, we need to stringify it
+  const stringValue = ["string", "number"].includes(typeof value?.[property])
+    ? value?.[property] || ""
+    : JSON.stringify(value?.[property]);
+
+  if (stringValue?.length > maxLength) {
+    const trimmedString = trimFromBeginning
+      ? "..." + stringValue.substring(stringValue.length - maxLength, stringValue.length)
+      : stringValue.substring(0, maxLength - 1) + "...";
+
+    return (
+      <span className={className}>
+        <p data-tip={stringValue}>{trimmedString}</p>
+        <ReactTooltip />
+      </span>
+    );
+  } else {
+    return <span className={className}>{stringValue}</span>;
+  }
+}
+
+export function renderConfigKeyHeader(table: any, _saveAllChanges: any) {
+  return (
+    <>
+      <div
+        style={{
+          textAlign: "left",
+        }}
+      >
+        <>
+          <CdsButton
+            title={table.getIsAllRowsExpanded() ? "Collapse All" : "Expand All"}
+            type="button"
+            onClick={table.getToggleAllRowsExpandedHandler()}
+            action="flat"
+            status="primary"
+            size="sm"
+            className="table-button"
+          >
+            {table.getIsAllRowsExpanded() ? (
+              <CdsIcon shape="minus" size="sm" solid={true} />
+            ) : (
+              <CdsIcon shape="plus" size="sm" solid={true} />
+            )}
+          </CdsButton>
+          <span>Key</span>
+        </>
+      </div>
+    </>
+  );
+}
+
+export function renderConfigKey(value: IBasicFormParam, row: any, _saveAllChanges: any) {
+  return (
+    <div
+      className="left-align self-center"
+      style={{
+        paddingLeft: `${row.depth * 0.5}rem`,
+      }}
+    >
+      <>
+        <div style={{ display: "inline-flex" }}>
+          <CdsButton
+            title={row.getIsExpanded() ? "Collapse" : "Expand"}
+            type="button"
+            onClick={row.getToggleExpandedHandler()}
+            action="flat"
+            status="primary"
+            size="sm"
+            disabled={!row.getCanExpand()}
+            className="table-button"
+          >
+            {row.getCanExpand() ? (
+              row.getIsExpanded() ? (
+                <CdsIcon shape="minus" size="sm" solid={true} />
+              ) : (
+                <CdsIcon shape="plus" size="sm" solid={true} />
+              )
+            ) : (
+              <></>
+            )}
+          </CdsButton>
+          {renderCellWithTooltip(value, "key", "breakable self-center", true, MAX_LENGTH / 1.5)}
+        </div>
+      </>
+    </div>
+  );
+}
+
+export function renderConfigType(value: IBasicFormParam) {
+  return renderCellWithTooltip(value, "type", "italics");
+}
+
+export function renderConfigDescription(value: IBasicFormParam) {
+  return renderCellWithTooltip(value, "title", "breakable");
+}
+
+export function renderConfigDefaultValue(value: IBasicFormParam) {
+  return renderCellWithTooltip(value, "defaultValue", "breakable");
+}
+
+export function renderConfigDeployedValue(value: IBasicFormParam) {
+  return renderCellWithTooltip(value, "deployedValue");
+}
+
+export function renderConfigCurrentValuePro(
+  param: IBasicFormParam,
+  handleBasicFormParamChange: (
+    p: IBasicFormParam,
+  ) => (e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => void,
+) {
+  // early return if the value is marked as a custom form component
+  if (param.isCustomComponent) {
+    // TODO(agamez): consider using a modal window to display the full value
+    return (
+      <div id={param.key}>
+        <CustomFormComponentLoader
+          param={param}
+          handleBasicFormParamChange={handleBasicFormParamChange}
+        />
+      </div>
+    );
+  }
+  // if the param has properties, each of them will be rendered as a row
+  if (param.hasProperties) {
+    return <></>;
+  }
+
+  // if it isn't a custom component or an with more properties, render an input
+  switch (param.type) {
+    case "string":
+      return (
+        <TextParam
+          id={param.key}
+          label={param.title || param.path}
+          param={param}
+          inputType={param.title.includes("password") ? "password" : "string"}
+          handleBasicFormParamChange={handleBasicFormParamChange}
+        />
+      );
+
+    case "boolean":
+      return (
+        <BooleanParam
+          id={param.key}
+          label={param.title || param.path}
+          param={param}
+          handleBasicFormParamChange={handleBasicFormParamChange}
+        />
+      );
+
+    case "integer":
+    case "number":
+      return (
+        <SliderParam
+          id={param.key}
+          label={param.title || param.path}
+          param={param}
+          handleBasicFormParamChange={handleBasicFormParamChange}
+          min={param.minimum || 1}
+          max={param.maximum || 1000}
+          step={param.type === "integer" ? 1 : 0.1}
+          unit={""}
+        />
+      );
+    case "array":
+      if (param?.schema?.items?.type !== "object") {
+        return (
+          <ArrayParam
+            id={param.key}
+            label={param.title || param.path}
+            param={param}
+            handleBasicFormParamChange={handleBasicFormParamChange}
+            type={param?.schema?.items?.type ?? "string"}
+          />
+        );
+      } else {
+        // TODO(agamez): render the object properties
+        return (
+          <TextParam
+            id={param.key}
+            label={param.title || param.path}
+            param={param}
+            inputType={"textarea"}
+            handleBasicFormParamChange={handleBasicFormParamChange}
+          />
+        );
+      }
+    default:
+      return (
+        <TextParam
+          id={param.key}
+          label={param.title || param.path}
+          param={param}
+          inputType={"textarea"}
+          handleBasicFormParamChange={handleBasicFormParamChange}
+        />
+      );
+  }
+}
