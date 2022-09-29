0704dfdaa55a1bb23e1d5e02b5f31a3ac2ba2c23
Antonio Gamez Diaz <agamez@vmware.com>
2022-09-29 11:44:42 +0200

    Remove "Differential" view from Operators. Use the MonacoDiffEditor instead
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- dashboard/src/components/OperatorInstanceFormBody/OperatorInstanceFormBody.tsx
+++ dashboard/src/components/OperatorInstanceFormBody/OperatorInstanceFormBody.tsx
@@ -3,16 +3,14 @@
 
 import { CdsButton } from "@cds/react/button";
 import { CdsIcon } from "@cds/react/icon";
-import DifferentialTab from "components/DeploymentFormBody/DifferentialTab";
+import ConfirmDialog from "components/ConfirmDialog";
 import Alert from "components/js/Alert";
+import LoadingWrapper from "components/LoadingWrapper";
 import Tabs from "components/Tabs";
 import * as yaml from "js-yaml";
 import { useEffect, useState } from "react";
 import { IResource } from "shared/types";
-import ConfirmDialog from "../ConfirmDialog/ConfirmDialog";
-import AdvancedDeploymentForm from "../DeploymentFormBody/AdvancedDeploymentForm";
-import Differential from "../DeploymentFormBody/Differential";
-import LoadingWrapper from "../LoadingWrapper";
+import OperatorAdvancedDeploymentForm from "./OperatorAdvancedDeploymentForm/OperatorAdvancedDeploymentForm";
 
 export interface IOperatorInstanceFormProps {
   isFetching: boolean;
@@ -98,33 +96,13 @@ function DeploymentFormBody({
         <div className="deployment-form-tabs">
           <Tabs
             id="deployment-form-body-tabs"
-            columns={[
-              "YAML",
-              <DifferentialTab
-                key="differential-selector"
-                deploymentEvent={deployedValues ? "upgrade" : "install"}
-                defaultValues={defaultValues}
-                deployedValues={deployedValues || ""}
-                appValues={values}
-              />,
-            ]}
+            columns={["YAML editor"]}
             data={[
-              <AdvancedDeploymentForm
+              <OperatorAdvancedDeploymentForm
                 appValues={values}
+                oldAppValues={deployedValues || defaultValues}
                 handleValuesChange={handleValuesChange}
                 key="advanced-deployment-form"
-              />,
-              <Differential
-                oldValues={deployedValues || defaultValues}
-                newValues={values}
-                emptyDiffElement={
-                  deployedValues ? (
-                    <span>No changes detected from deployed values</span>
-                  ) : (
-                    <span>No changes detected from example defaults</span>
-                  )
-                }
-                key="differential-selector"
               />,
             ]}
           />
@@ -133,19 +111,9 @@ function DeploymentFormBody({
           <CdsButton status="primary" type="submit">
             <CdsIcon shape="deploy" /> Deploy
           </CdsButton>
-          {/* TODO(andresmgot): CdsButton "type" property doesn't work, so we need to use a normal <button>
-            https://github.com/vmware/clarity/issues/5038
-          */}
-          <span className="color-icon-info">
-            <button
-              className="btn btn-info-outline"
-              type="button"
-              onClick={openModal}
-              style={{ marginTop: "-22px" }}
-            >
-              <CdsIcon shape="backup-restore" /> Restore Defaults
-            </button>
-          </span>
+          <CdsButton type="button" status="primary" action="outline" onClick={openModal}>
+            <CdsIcon shape="backup-restore" /> Restore Defaults
+          </CdsButton>
         </div>
       </form>
     </>
