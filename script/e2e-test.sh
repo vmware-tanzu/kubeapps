1109da98f0bbe44964e0e7871082418dc13ac70e
Jesús Benito Calzada <bjesus@vmware.com>
2022-10-03 08:29:05 +0200

    Revert "Change default versions to check its working properly"
    This reverts commit a67a02b9db7978b530281abbd7d133411e610dbe.

--- script/e2e-test.sh
+++ script/e2e-test.sh
@@ -16,8 +16,8 @@ IMG_MODIFIER=${4:-""}
 TEST_TIMEOUT_MINUTES=${5:-"4"}
 DEX_IP=${6:-"172.18.0.2"}
 ADDITIONAL_CLUSTER_IP=${7:-"172.18.0.3"}
-KAPP_CONTROLLER_VERSION=${8:-"v0.41.1"}
-CHARTMUSEUM_VERSION=${9:-"3.8.0"}
+KAPP_CONTROLLER_VERSION=${8:-"v0.41.2"}
+CHARTMUSEUM_VERSION=${9:-"3.9.0"}
 
 # TODO(andresmgot): While we work with beta releases, the Bitnami pipeline
 # removes the pre-release part of the tag
