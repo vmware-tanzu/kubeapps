7566d0b00bff089d9e21641f1d2496479f0bf56d
Rafa Castelblanque <67455978+castelblanque@users.noreply.github.com>
2022-10-05 11:58:27 +0200

    Update script/install-nginx.sh
    Co-authored-by: Jesús Miguel Benito Calzada <bjesus@vmware.com>
    Signed-off-by: Rafa Castelblanque <67455978+castelblanque@users.noreply.github.com>

--- script/install-nginx.sh
+++ script/install-nginx.sh
@@ -3,9 +3,7 @@
 # Copyright 2018-2022 the Kubeapps contributors.
 # SPDX-License-Identifier: Apache-2.0
 
-set -o errexit
-set -o nounset
-set -o pipefail
+set -euo pipefail
 
 echo "Instaling Nginx"
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
