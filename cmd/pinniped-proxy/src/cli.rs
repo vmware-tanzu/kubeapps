c54c977499e6189778bdfba84328437542292418
Michael Nelson <minelson@vmware.com>
2022-10-10 10:32:08 +1100

    [pinniped-proxy] Update from structopt to clapv4. Fixes #5357
    Signed-off-by: Michael Nelson <minelson@vmware.com>

--- cmd/pinniped-proxy/src/cli.rs
+++ cmd/pinniped-proxy/src/cli.rs
@@ -1,10 +1,10 @@
 // Copyright 2020-2022 the Kubeapps contributors.
 // SPDX-License-Identifier: Apache-2.0
 
-use structopt::StructOpt;
+use clap;
 
-#[derive(StructOpt, Debug)]
-#[structopt(version = env!("PINNIPED_PROXY_VERSION"))]
+#[derive(clap::Parser, Debug)]
+#[clap(version = env!("PINNIPED_PROXY_VERSION"))]
 /// A proxy server which converts k8s API server requests with bearer tokens to
 /// requests with short-lived X509 certs exchanged by pinniped.
 ///
@@ -13,29 +13,29 @@ use structopt::StructOpt;
 /// short-lived client certificates, before forwarding the request onwards
 /// to the destination k8s API server.
 pub struct Options {
-    #[structopt(
-        short = "p",
+    #[clap(
+        short = 'p',
         long = "port",
         env = "PINNIPED_PROXY_PORT",
         default_value = "3333",
         help = "Specify the port on which pinniped-proxy listens."
     )]
     pub port: u16,
-    #[structopt(
+    #[clap(
         long = "default-ca-cert",
         env = "PINNIPED_PROXY_DEFAULT_CA_CERT",
         default_value = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt",
         help = "Specify the file path to the cert authority for the default api server https://kubernetes.default"
     )]
     pub default_ca_cert: String,
-    #[structopt(
+    #[clap(
         long = "proxy-tls-cert",
         env = "PINNIPED_PROXY_TLS_CERT",
         default_value = "",
         help = "Specify the file path to a PEM encoded TLS certificate. Providing the cert and key implies listening for TLS requests."
     )]
     pub proxy_tls_cert: String,
-    #[structopt(
+    #[clap(
         long = "proxy-tls-cert-key",
         env = "PINNIPED_PROXY_TLS_CERT_KEY",
         default_value = "",
