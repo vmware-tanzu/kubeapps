e9b9aa8f4597517cb30d291eedec558c8a9dffe8
Michael Nelson <minelson@vmware.com>
2022-10-10 10:40:41 +1100

    Fix clap deprecations.
    Signed-off-by: Michael Nelson <minelson@vmware.com>

--- cmd/pinniped-proxy/src/cli.rs
+++ cmd/pinniped-proxy/src/cli.rs
@@ -4,7 +4,7 @@
 use clap;
 
 #[derive(clap::Parser, Debug)]
-#[clap(version = env!("PINNIPED_PROXY_VERSION"))]
+#[command(version = env!("PINNIPED_PROXY_VERSION"))]
 /// A proxy server which converts k8s API server requests with bearer tokens to
 /// requests with short-lived X509 certs exchanged by pinniped.
 ///
@@ -13,7 +13,7 @@ use clap;
 /// short-lived client certificates, before forwarding the request onwards
 /// to the destination k8s API server.
 pub struct Options {
-    #[clap(
+    #[arg(
         short = 'p',
         long = "port",
         env = "PINNIPED_PROXY_PORT",
@@ -21,21 +21,21 @@ pub struct Options {
         help = "Specify the port on which pinniped-proxy listens."
     )]
     pub port: u16,
-    #[clap(
+    #[arg(
         long = "default-ca-cert",
         env = "PINNIPED_PROXY_DEFAULT_CA_CERT",
         default_value = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt",
         help = "Specify the file path to the cert authority for the default api server https://kubernetes.default"
     )]
     pub default_ca_cert: String,
-    #[clap(
+    #[arg(
         long = "proxy-tls-cert",
         env = "PINNIPED_PROXY_TLS_CERT",
         default_value = "",
         help = "Specify the file path to a PEM encoded TLS certificate. Providing the cert and key implies listening for TLS requests."
     )]
     pub proxy_tls_cert: String,
-    #[clap(
+    #[arg(
         long = "proxy-tls-cert-key",
         env = "PINNIPED_PROXY_TLS_CERT_KEY",
         default_value = "",
