867c54f87c22e9b7543b6a0f3dc99478a66a41a8
Jesús Benito Calzada <bjesus@vmware.com>
2022-10-10 22:48:33 +0200

    Add debugging traces
    Signed-off-by: Jesús Benito Calzada <bjesus@vmware.com>

--- script/run_e2e_tests.sh
+++ script/run_e2e_tests.sh
@@ -5,6 +5,8 @@ IFS=$'\n\t'
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null && pwd)"
 
+source "${ROOT_DIR}/script/lib/liblog.sh"
+
 # If we want to test the latest version instead we override the image to be used
 if [[ -n "${TEST_LATEST_RELEASE:-}" ]]; then
   source "${ROOT_DIR}/script/chart_sync_utils.sh"
@@ -23,6 +25,10 @@ test_command="${ROOT_DIR}/script/e2e-test.sh \
   ${ADDITIONAL_CLUSTER_IP} \
   ${KAPP_CONTROLLER_VERSION} \
   ${CHARTMUSEUM_VERSION}"
+
+info "IMG_PREFIX: $IMG_PREFIX"
+info "TESTS_GROUP: $TESTS_GROUP"
+info "CMD: $test_command"
 
 if IMG_PREFIX=${IMG_PREFIX} TESTS_GROUP=${TESTS_GROUP} ${test_command}; then
   echo "TEST_RESULT=0" >> "${GITHUB_ENV}"
