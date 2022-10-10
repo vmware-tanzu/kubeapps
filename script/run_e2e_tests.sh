9e6fbe89d9818e7ac0991f14f8679b4228f70061
Jesús Benito Calzada <bjesus@vmware.com>
2022-10-10 23:39:30 +0200

    Testing...
    Signed-off-by: Jesús Benito Calzada <bjesus@vmware.com>

--- script/run_e2e_tests.sh
+++ script/run_e2e_tests.sh
@@ -5,8 +5,6 @@ IFS=$'\n\t'
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null && pwd)"
 
-source "${ROOT_DIR}/script/lib/liblog.sh"
-
 # If we want to test the latest version instead we override the image to be used
 if [[ -n "${TEST_LATEST_RELEASE:-}" ]]; then
   source "${ROOT_DIR}/script/chart_sync_utils.sh"
@@ -15,32 +13,19 @@ if [[ -n "${TEST_LATEST_RELEASE:-}" ]]; then
   IMG_MODIFIER=""
 fi
 
-test_command="${ROOT_DIR}/script/e2e-test.sh \
-  ${USE_MULTICLUSTER_OIDC_ENV} \
-  ${OLM_VERSION} \
-  ${IMG_DEV_TAG} \
-  ${IMG_MODIFIER} \
-  ${TEST_TIMEOUT_MINUTES} \
-  ${DEFAULT_DEX_IP} \
-  ${ADDITIONAL_CLUSTER_IP} \
-  ${KAPP_CONTROLLER_VERSION} \
-  ${CHARTMUSEUM_VERSION}"
+params=(
+  "${USE_MULTICLUSTER_OIDC_ENV}"
+  "${OLM_VERSION}"
+  "${IMG_DEV_TAG}"
+  "${IMG_MODIFIER}"
+  "${TEST_TIMEOUT_MINUTES}"
+  "${DEFAULT_DEX_IP}"
+  "${ADDITIONAL_CLUSTER_IP}"
+  "${KAPP_CONTROLLER_VERSION}"
+  "${CHARTMUSEUM_VERSION}"
+)
 
-info "IMG_PREFIX: $IMG_PREFIX"
-info "TESTS_GROUP: $TESTS_GROUP"
-info "CMD: $test_command"
-
-#if IMG_PREFIX=${IMG_PREFIX} TESTS_GROUP=${TESTS_GROUP} ${test_command}; then
-if IMG_PREFIX=${IMG_PREFIX} TESTS_GROUP=${TESTS_GROUP} "${ROOT_DIR}/script/e2e-test.sh" \
-     ${USE_MULTICLUSTER_OIDC_ENV} \
-     ${OLM_VERSION} \
-     ${IMG_DEV_TAG} \
-     ${IMG_MODIFIER} \
-     ${TEST_TIMEOUT_MINUTES} \
-     ${DEFAULT_DEX_IP} \
-     ${ADDITIONAL_CLUSTER_IP} \
-     ${KAPP_CONTROLLER_VERSION} \
-     ${CHARTMUSEUM_VERSION}; then
+if IMG_PREFIX=${IMG_PREFIX} TESTS_GROUP=${TESTS_GROUP} "${ROOT_DIR}/script/e2e-test.sh" "${params[@]}"; then
   echo "TEST_RESULT=0" >> "${GITHUB_ENV}"
   exit 0
 fi
