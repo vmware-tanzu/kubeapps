4b8c32281076d91fbd0e6d0b8d060dbc1b4c019d
Jesús Benito Calzada <bjesus@vmware.com>
2022-10-10 14:01:23 +0200

    Move run e2e tests logic to a script
    Signed-off-by: Jesús Benito Calzada <bjesus@vmware.com>

---
+++ script/run_e2e_tests.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null && pwd)"
+
+# If we want to test the latest version instead we override the image to be used
+if [[ -n "${TEST_LATEST_RELEASE:-}" ]]; then
+  source "${ROOT_DIR}/script/chart_sync_utils.sh"
+  latest="$(latestReleaseTag)"
+  IMG_DEV_TAG=${latest/v/}
+  IMG_MODIFIER=""
+fi
+
+test_command="./script/e2e-test.sh \
+  ${USE_MULTICLUSTER_OIDC_ENV} \
+  ${OLM_VERSION} \
+  ${IMG_DEV_TAG} \
+  ${IMG_MODIFIER} \
+  ${TEST_TIMEOUT_MINUTES} \
+  ${DEFAULT_DEX_IP} \
+  ${ADDITIONAL_CLUSTER_IP} \
+  ${KAPP_CONTROLLER_VERSION} \
+  ${CHARTMUSEUM_VERSION}"
+
+if IMG_PREFIX=${IMG_PREFIX} TESTS_GROUPS=${TESTS_GROUPS} ${test_command}; then
+  echo "TEST_RESULT=0" >> "${GITHUB_ENV}"
+  exit 0
+fi
+
+echo "TEST_RESULT=1" >> "${GITHUB_ENV}"
+exit 1
