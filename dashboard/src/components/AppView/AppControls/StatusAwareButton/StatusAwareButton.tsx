fbf07917ede4356022a08580b7c84a6995b7ed82
Antonio Gámez, PhD <agamez@vmware.com>
2022-10-26 13:27:26 +0200

    Allow deleting pending pkgs (#5565)
    ### Description of the change
    
    As reported in #5538, sometimes, packages get stuck in a pending state.
    Currently, Kubeapps UI was preventing the deletion of pending packages,
    but this might not be a good solution if we really want to delete a
    package.
    It should be the backend that manages the deletion (or not) of a pending
    package.
    
    However, the tooltip is still being shown, this way users, at least,
    will notice something is happening with this package.
    
    ### Benefits
    
    No more pending packages stuck
    
    ### Possible drawbacks
    
    N/A
    
    ### Applicable issues
    
    - fixes #5538
    
    ### Additional information
    
    
    ![image](https://user-images.githubusercontent.com/11535726/197772089-1332fcf9-dcf3-4b0f-b7ca-a524a0ca08c6.png)
    
    
    ![image](https://user-images.githubusercontent.com/11535726/197772022-857f5f7c-fb56-4ad9-972e-f36a27259af4.png)
    
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- dashboard/src/components/AppView/AppControls/StatusAwareButton/StatusAwareButton.tsx
+++ dashboard/src/components/AppView/AppControls/StatusAwareButton/StatusAwareButton.tsx
@@ -12,27 +12,45 @@ export interface IStatusAwareButtonProps {
   id: string;
   releaseStatus: InstalledPackageStatus | undefined | null;
   disabled?: boolean;
+  statusesToDeactivate?: InstalledPackageStatus_StatusReason[];
+  statusesToDeactivateTooltips?: { [key: string]: string };
 }
 
 export default function StatusAwareButton<T extends IStatusAwareButtonProps>(props: T) {
-  const { id, releaseStatus, disabled, ...otherProps } = props;
-  // Deactivate the button if: the status code is undefined or null OR the status code is (uninstalled or pending)
-  const isDisabled =
-    disabled || releaseStatus?.reason == null
-      ? true
-      : [
-          InstalledPackageStatus_StatusReason.STATUS_REASON_UNINSTALLED,
-          InstalledPackageStatus_StatusReason.STATUS_REASON_PENDING,
-        ].includes(releaseStatus.reason);
+  const {
+    id,
+    releaseStatus,
+    disabled,
+    statusesToDeactivate,
+    statusesToDeactivateTooltips,
+    ...otherProps
+  } = props;
 
-  const tooltips = {
+  const defaultStatusesToDeactivate = [
+    InstalledPackageStatus_StatusReason.STATUS_REASON_UNINSTALLED,
+    InstalledPackageStatus_StatusReason.STATUS_REASON_PENDING,
+  ];
+  const defaultStatusesToDeactivateTooltips = {
     [InstalledPackageStatus_StatusReason.STATUS_REASON_UNINSTALLED]:
       "The application is being deleted.",
     [InstalledPackageStatus_StatusReason.STATUS_REASON_PENDING]:
       "The application is pending installation.",
   };
 
-  const tooltip = releaseStatus?.reason ? tooltips[releaseStatus.reason] : undefined;
+  // allow buttons to override the default statuses to deactivate
+  const statuses = statusesToDeactivate?.length
+    ? statusesToDeactivate
+    : defaultStatusesToDeactivate;
+
+  const tooltips = statusesToDeactivateTooltips
+    ? statusesToDeactivateTooltips
+    : defaultStatusesToDeactivateTooltips;
+
+  // Deactivate the button if: the status code is undefined or null OR the status code is (uninstalled or pending)
+  const isDisabled =
+    disabled || releaseStatus?.reason == null ? true : statuses.includes(releaseStatus.reason);
+
+  const tooltip = releaseStatus?.reason ? tooltips![releaseStatus.reason] : undefined;
   return (
     <>
       <CdsButton {...otherProps} disabled={isDisabled} data-for={id} data-tip={true} />
