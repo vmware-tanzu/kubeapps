dfbec45b7e14d23bcdb4bca4917cfd3ce2237104
Jesús Benito Calzada <bjesus@vmware.com>
2022-10-26 16:47:40 +0200

    Do not set labels for GKE cluster
    Signed-off-by: Jesús Benito Calzada <bjesus@vmware.com>

--- script/start-gke-env.sh
+++ script/start-gke-env.sh
@@ -34,7 +34,10 @@ if [[ $(gcloud container clusters list --filter="name:${CLUSTER}") ]]; then
 fi
 
 echo "Creating cluster ${CLUSTER} in ${ZONE} (v$BRANCH)"
-gcloud container clusters create --cluster-version="${BRANCH}" --zone "${ZONE}" "${CLUSTER}" --num-nodes 2 --machine-type=n1-standard-2 --preemptible --labels=team=kubeapps --quiet
+
+# FIXME Removing the use of --labels=team=kubeapps due to a bug in gcloud cli: https://issuetracker.google.com/issues/255708239
+#gcloud container clusters create --cluster-version="${BRANCH}" --zone "${ZONE}" "${CLUSTER}" --num-nodes 2 --machine-type=n1-standard-2 --preemptible --labels=team=kubeapps --quiet
+gcloud container clusters create --cluster-version="${BRANCH}" --zone "${ZONE}" "${CLUSTER}" --num-nodes 2 --machine-type=n1-standard-2 --preemptible --quiet
 echo "Waiting for the cluster to respond..."
 cnt=20
 until kubectl get pods >/dev/null 2>&1; do
