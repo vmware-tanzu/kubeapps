7adaa909222da5c2a60ec4edeb293cad037736c9
Jesús Miguel Benito Calzada <bjesus@vmware.com>
2022-11-02 18:23:02 +0100

    Migrate release job to GitHub Actions (#5600)
    This commit contains the migration of the `release` job to GitHub actions. A
    new workflow called `Release Pipeline` has been created. It calls the
    reusable `kubeapps-general.yml` workflow passing the parameter
    `run_gke_tests` with the value `true`, and is triggered only on new
    version tags.
    
    Signed-off-by: Jesús Benito Calzada <bjesus@vmware.com>

--- script/create_release.sh
+++ script/create_release.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Copyright 2021-2022 the Kubeapps contributors.
 # SPDX-License-Identifier: Apache-2.0
@@ -7,14 +7,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-source $(dirname $0)/chart_sync_utils.sh
+source $(dirname "$0")/chart_sync_utils.sh
 
 TAG=${1:?Missing tag}
 KUBEAPPS_REPO=${2:?Missing kubeapps repo}
+DEV_MODE=${DEV_MODE:-false}
 
 if [[ -z "${TAG}" ]]; then
   echo "A git tag is required for creating a release"
   exit 1
 fi
 
-gh release create -R "${KUBEAPPS_REPO}" -d "${TAG}" -t "${TAG}" -F "${RELEASE_NOTES_TEMPLATE_FILE}"
+if [[ "${DEV_MODE}" != "false" ]]; then
+  gh release create -R "${KUBEAPPS_REPO}" -d "${TAG}" -t "${TAG}" -F "${RELEASE_NOTES_TEMPLATE_FILE}"
+else
+  echo "Skipping release due to DEV_MODE!"
+fi
