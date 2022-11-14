12d2b5170fa667d425a1438da96381a5fbbe78e6
gfichtenholt <gfichtenholt@vmware.com>
2022-11-13 20:48:04 -0800

    incremental

--- script/e2e-test.sh
+++ script/e2e-test.sh
@@ -250,9 +250,9 @@ installFlux() {
   kubectl apply -f "${url}"
 
   # wait for deployment to be ready
-  kubectl rollout status -w deployment/helm-controller --namespace="${namespace}"
-  kubectl rollout status -w deployment/source-controller --namespace="${namespace}"
-
+  k8s_wait_for_deployment "${namespace}" helm-controller
+  k8s_wait_for_deployment "${namespace}" source-controller
+ 
   # Add test repository.
   kubectl apply -f https://raw.githubusercontent.com/fluxcd/source-controller/main/config/samples/source_v1beta2_helmrepository.yaml
 
