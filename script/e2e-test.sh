e3a6b73632f8d91adf9eb0dfc508f0e7cba5d316
Greg Fichtenholtz <74032303+gfichtenholt@users.noreply.github.com>
2022-11-18 08:24:37 -0800

    Run flux e2e test group in GHA's local_e2e_tests job (#5656)
    Original description from https://github.com/vmware-tanzu/kubeapps/pull/5625
    Description of the change
    
    It seems that at some point, a new flux test group has been added to the
    /script/e2e_test.sh file, but we have forgotten to add it to the GHA's
    local_e2e_tests job, so this test group is not being run in the GHA
    workflow.
    
    I have also took advantage of this PR to simplify the local_e2e_tests
    GHA job by setting a timeout of 6 minutes for every tests group, instead
    of having a default of 4 and setting it to 6 for the operator's group to
    avoid flaky tests.
    Benefits
    
        Run all the e2e tests from GHA.
    
    Possible drawbacks
    
    N/A

--- script/e2e-test.sh
+++ script/e2e-test.sh
@@ -247,11 +247,15 @@ installFlux() {
   url="https://github.com/fluxcd/flux2/releases/download/${release}/install.yaml"
   namespace=flux-system
 
-  kubectl apply -f "${url}"
+  # this is a workaround for flux e2e tests failing when run by GitHub Action Runners 
+  # due to not being able to deploy source-controller pod error:
+  # Warning  FailedScheduling  19s (x7 over 6m)  default-scheduler  0/1 nodes are available: 1 Insufficient cpu.
+  curl -o /tmp/flux_install.yaml -LO "${url}" 
+  cat /tmp/flux_install.yaml | sed -e 's/cpu: 100m/cpu: 75m/g' | kubectl apply -f -
 
-  # wait for deployment to be ready
-  kubectl rollout status -w deployment/helm-controller --namespace="${namespace}"
-  kubectl rollout status -w deployment/source-controller --namespace="${namespace}"
+  # wait for deployments to be ready
+  k8s_wait_for_deployment ${namespace} helm-controller
+  k8s_wait_for_deployment ${namespace} source-controller
 
   # Add test repository.
   kubectl apply -f https://raw.githubusercontent.com/fluxcd/source-controller/main/config/samples/source_v1beta2_helmrepository.yaml
