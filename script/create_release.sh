b827c81cb2a16a62c4acd7014e1c4de1d8c7b8eb
Jesús Benito Calzada <bjesus@vmware.com>
2022-11-25 13:26:21 +0100

    Add GH_TOKEN env var to release job
    Signed-off-by: Jesús Benito Calzada <bjesus@vmware.com>

--- script/create_release.sh
+++ script/create_release.sh
@@ -12,6 +12,7 @@ source $(dirname "$0")/chart_sync_utils.sh
 TAG=${1:?Missing tag}
 KUBEAPPS_REPO=${2:?Missing kubeapps repo}
 DEV_MODE=${DEV_MODE:-false}
+GH_TOKEN=${GH_TOKEN:?Missing GitHub token}
 
 if [[ -z "${TAG}" ]]; then
   echo "A git tag is required for creating a release"
