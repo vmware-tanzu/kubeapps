63d511a65d733b5626118756dc428e25f1d67c36
gfichtenholt <gfichtenholt@vmware.com>
2022-12-02 17:00:03 -0800

    incremental

--- script/makefiles/deploy-dev.mk
+++ script/makefiles/deploy-dev.mk
@@ -72,9 +72,7 @@ deploy-flux-controllers:
 
 # Add Operator Lifecycle Manager used for testing the kubeapps-apis integration.
 deploy-olm:
-	curl -L https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.22.0/install.sh -o install.sh
-	chmod +x install.sh
-	./install.sh v0.22.0
+	curl -s https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.22.0/install.sh | bash -s v0.22.0
 
 reset-dev:
 	helm --kubeconfig=${CLUSTER_CONFIG} -n kubeapps delete kubeapps  || true
