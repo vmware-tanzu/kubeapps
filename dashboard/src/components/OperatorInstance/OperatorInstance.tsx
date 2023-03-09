64acc1e800ad4e107782966848627f783e4aaeb0
Michael Nelson <minelson@vmware.com>
2023-03-09 11:12:24 +1100

    Extend ResourceRef for custom resource ref used by operators to avoid type error.
    Signed-off-by: Michael Nelson <minelson@vmware.com>

--- dashboard/src/components/OperatorInstance/OperatorInstance.tsx
+++ dashboard/src/components/OperatorInstance/OperatorInstance.tsx
@@ -58,22 +58,11 @@ function parseResource(
   if (crd.resources) {
     crd.resources?.forEach(r => {
       switch (r.kind) {
-        // So there is confusion here because, for the operator functionality,
-        // we have extra info on the resource ref (eg. filter for
-        // ownerreference), which we don't use for the normal, non-operator
-        // functionality. Yet, we're using the same components (ResourceTabs
-        // etc.) which expect the normal resource type. Hence
-        // IAppViewResourceRefs is defined with the normal type, but fromCRD
-        // returns the marked-up type.
-        // TODO: What if the internal ResourceRef extended the normal one? Could
-        // we then pass it happily? Or other option is to check whether the filter object
-        // owner reference is actually used (doesn't appear to be used)?
-        // CustomAppView appears to use `namespaced` in tests (but doesn't appear to need to)
-        // Similarly, Kube.test.ts appears to use `namespaced` unnecessarily.
-        // What about initial resources... where else is `namespaced` actually used?
-        // The test "updates the state with the CRD resources" fails if using normal
-        // resourcerefs here, because of the missing filter. But where is this filter used?
+        // TODO(minelson): Audit code to see if we can switch to use the normal ResourceRef
+        // here instead, then remove the shared/ResourceRef with the extra fields.
+        // https://github.com/vmware-tanzu/kubeapps/issues/6062
         case "Deployment":
+          // eslint-disable-next-line @typescript-eslint/TS2322
           result.deployments.push(fromCRD(r, kind, cluster, namespace, ownerRef));
           break;
         case "StatefulSet":
--- dashboard/src/shared/ResourceRef.ts
+++ dashboard/src/shared/ResourceRef.ts
@@ -1,6 +1,7 @@
 // Copyright 2019-2022 the Kubeapps contributors.
 // SPDX-License-Identifier: Apache-2.0
 
+import type { PartialMessage } from "@bufbuild/protobuf";
 import { IClusterServiceVersionCRDResource, IKind } from "./types";
 import { ResourceRef as APIResourceRef } from "gen/kubeappsapis/core/packages/v1alpha1/packages_pb";
 
@@ -11,11 +12,7 @@ export function fromCRD(
   namespace: string,
   ownerReference: any,
 ) {
-  const apiResourceRef = {
-    apiVersion: kind.apiVersion,
-    kind: r.kind,
-  } as APIResourceRef;
-  const ref = new ResourceRef(apiResourceRef, cluster, kind.plural, kind.namespaced, namespace);
+  const ref = new ResourceRef(cluster, kind.plural, kind.namespaced, namespace, { apiVersion: kind.apiVersion, kind: r.kind });
   ref.filter = {
     metadata: { ownerReferences: [ownerReference] },
   };
@@ -29,32 +26,22 @@ export const keyForResourceRef = (r: APIResourceRef) =>
 
 // ResourceRef defines a reference to a namespaced Kubernetes API Object and
 // provides helpers to retrieve the resource URL
-class ResourceRef {
+class ResourceRef extends APIResourceRef {
   public cluster: string;
-  public apiVersion: string;
-  public kind: string;
   public plural: string;
   public namespaced: boolean;
-  public name: string;
-  public namespace: string;
   public filter: any;
 
   // Creates a new ResourceRef instance from an existing IResource. Provide
   // defaultNamespace to set if the IResource doesn't specify a namespace.
-  constructor(
-    apiRef: APIResourceRef,
-    cluster: string,
-    plural: string,
-    namespaced: boolean,
-    releaseNamespace: string,
-  ) {
+  //constructor(data?: PartialMessage<ResourceRef>) {
+  constructor(cluster: string, plural: string, namespaced: boolean, releaseNamespace: string, data?: PartialMessage<APIResourceRef>) {
+    data = data || {};
+    data.namespace = namespaced ? data.namespace || releaseNamespace || "" : "";
+    super(data);
+    this.namespaced = namespaced;
     this.cluster = cluster;
     this.plural = plural;
-    this.apiVersion = apiRef.apiVersion;
-    this.kind = apiRef.kind;
-    this.name = apiRef.name;
-    this.namespace = namespaced ? apiRef.namespace || releaseNamespace || "" : "";
-    this.namespaced = namespaced;
     return this;
   }
 }
