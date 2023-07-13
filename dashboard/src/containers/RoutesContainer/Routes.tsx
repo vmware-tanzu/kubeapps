cbf4f1ea60a2d71c1dd65329698cc184fdd7095c
Michael Nelson <minelson@vmware.com>
2023-07-13 15:25:37 +1000

    Update OperatorCreateForm to use react-router hooks and remove container.
    Signed-off-by: Michael Nelson <minelson@vmware.com>

--- dashboard/src/containers/RoutesContainer/Routes.tsx
+++ dashboard/src/containers/RoutesContainer/Routes.tsx
@@ -26,13 +26,13 @@ import { IFeatureFlags } from "shared/Config";
 
 // TODO(andresmgot): Containers should be no longer needed, replace them when possible
 import LoginFormContainer from "../../containers/LoginFormContainer";
-import OperatorInstanceCreateContainer from "../../containers/OperatorInstanceCreateContainer";
 import OperatorInstanceUpdateContainer from "../../containers/OperatorInstanceUpdateContainer";
 import OperatorInstanceViewContainer from "../../containers/OperatorInstanceViewContainer";
 import OperatorsListContainer from "../../containers/OperatorsListContainer";
 import OperatorViewContainer from "../../containers/OperatorViewContainer";
 import PrivateRouteContainer from "../../containers/PrivateRouteContainer";
 import OperatorNew from "components/OperatorNew";
+import OperatorInstanceForm from "components/OperatorInstanceForm";
 
 type IRouteComponentPropsAndRouteProps = RouteProps & RouteComponentProps<any>;
 
@@ -60,7 +60,7 @@ const operatorsRoutes = {
   "/c/:cluster/ns/:namespace/operators": OperatorsListContainer,
   "/c/:cluster/ns/:namespace/operators/:operator": OperatorViewContainer,
   "/c/:cluster/ns/:namespace/operators/new/:operator": OperatorNew,
-  "/c/:cluster/ns/:namespace/operators-instances/new/:csv/:crd": OperatorInstanceCreateContainer,
+  "/c/:cluster/ns/:namespace/operators-instances/new/:csv/:crd": OperatorInstanceForm,
   "/c/:cluster/ns/:namespace/operators-instances/:csv/:crd/:instanceName":
     OperatorInstanceViewContainer,
   "/c/:cluster/ns/:namespace/operators-instances/:csv/:crd/:instanceName/update":
