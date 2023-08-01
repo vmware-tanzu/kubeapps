7b9ec80a43f62b379ae898dbbd44db12e1503cf5
Antonio Gamez Diaz <agamez@vmware.com>
2023-08-01 14:00:22 +0200

    Remove unused button js component
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- dashboard/src/components/js/Button/index.js
+++
@@ -1,6 +0,0 @@
-// Copyright 2020-2022 the Kubeapps contributors.
-// SPDX-License-Identifier: Apache-2.0
-
-import Button, { ButtonThemes, ButtonTypes } from "./Button";
-export { ButtonThemes, ButtonTypes };
-export default Button;
