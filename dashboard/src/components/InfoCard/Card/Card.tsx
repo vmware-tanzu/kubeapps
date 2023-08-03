d618dd70f1949eea69771140d3c4dc07c5effd88
Antonio Gamez Diaz <agamez@vmware.com>
2023-08-03 13:12:55 +0200

    Initial transformation to tsx
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- dashboard/src/components/InfoCard/Card/Card.tsx
+++ dashboard/src/components/InfoCard/Card/Card.tsx
@@ -1,58 +1,37 @@
-// Copyright 2020-2022 the Kubeapps contributors.
+// Copyright 2020-2023 the Kubeapps contributors.
 // SPDX-License-Identifier: Apache-2.0
 
-import cs from "classnames";
-import PropTypes from "prop-types";
-import React from "react";
+import { HTMLAttributes } from "react";
 
-const Card = ({ children, clickable, htmlTag, onClick }) => {
-  const cssClass = cs("card", {
-    clickable: clickable || typeof onClick === "function",
-  });
+export interface ICardProps {
+  children: React.ReactNode;
+  clickable?: boolean;
+  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+}
 
+const Card = ({ children, clickable = false, onClick }: ICardProps) => {
   // Common props for the element
-  const innerProps = {
-    className: cssClass,
-    onClick,
+  const innerProps: HTMLAttributes<HTMLDivElement> = {
+    className: `card ${clickable || typeof onClick === "function" ? "clickable" : ""}`,
+    onClick: onClick,
   };
 
   // Make the card focusable by keyboard navigation. I'm not adding it based on the
   // clickable prop because I assume there's something around the card that manages
   // the onClick event.
   if (typeof onClick === "function") {
-    innerProps["tabIndex"] = "0";
+    innerProps.tabIndex = 0;
 
-    // Runs onClick when the user types `enter`
-    innerProps["onKeyPress"] = e => {
+    // Runs onClick when the user types `enter` or `space`
+    innerProps.onKeyDown = e => {
       // Enter
-      if (e.key === "Enter") {
-        onClick(e);
-      }
-    };
-
-    // Runs onClick when the user types `space`
-    innerProps["onKeyUp"] = e => {
-      // Space
-      if (e.key === " ") {
-        onClick(e);
+      if (e.key === "Enter" || e.key === " ") {
+        onClick(e as any);
       }
     };
   }
 
-  return React.createElement(htmlTag, innerProps, children);
-};
-
-Card.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.element), PropTypes.element])
-    .isRequired,
-  clickable: PropTypes.bool,
-  htmlTag: PropTypes.string,
-  onClick: PropTypes.func,
-};
-
-Card.defaultProps = {
-  clickable: false,
-  htmlTag: "div",
+  return <div {...innerProps}>{children}</div>;
 };
 
 export default Card;
---
+++ dashboard/src/components/InfoCard/Card/CardTitle/CardTitle.tsx
@@ -0,0 +1,16 @@
+// Copyright 2020-2022 the Kubeapps contributors.
+// SPDX-License-Identifier: Apache-2.0
+
+import React from "react";
+import "./CardTitle.scss";
+
+export interface ICardTitleProps {
+  children: React.ReactNode;
+  level: 1 | 2 | 3 | 4 | 5 | 6;
+}
+
+const CardTitle = ({ level, children }: ICardTitleProps) => {
+  return React.createElement(`h${level}`, { className: "card-title" }, children);
+};
+
+export default CardTitle;
