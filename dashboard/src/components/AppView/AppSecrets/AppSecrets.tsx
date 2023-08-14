de0968bff7c1cd2693bc19a9377c8e5a68a4a306
Antonio Gámez, PhD <agamez@vmware.com>
2023-08-14 14:33:37 +0200

    Remove card subcomponents (#6604)
    ### Description of the change
    
    As pointed out
    https://github.com/vmware-tanzu/kubeapps/pull/6560#discussion_r1289359646,
    we were using plenty of sub-components with no clear motivation. This is
    to simplify the `InfoCard` component in a single one, as it is just
    `<div>` and CSS classes.
    
    ### Benefits
    
    No unused or with extra complexity code.
    
    ### Possible drawbacks
    
    N/A
    
    ### Applicable issues
    
    - related #6563
    
    ### Additional information
    
    Before/after pics, they look exactly the same, which is what is intended
    here.
    
    
    ![image](https://github.com/vmware-tanzu/kubeapps/assets/11535726/03bc0a9d-256b-4a42-81fd-98159a181b63)
    
    
    Extra: I'm also squeezing two minor CSS changes: 1) I [ran a tool for
    detecting unused
    code](https://www.npmjs.com/package/webpack-deadcode-plugin), and
    noticed there were some CSS files being inconsistently imported; 2) the
    secrets view was badly positioned, see below:
    
    <details><summary>Secrets view</summary>
    
    
    ![image](https://github.com/vmware-tanzu/kubeapps/assets/11535726/c87e8689-ec1f-4454-91bb-fcfe1a2cdb04)
    
    </details>
    
    ---------
    
    Signed-off-by: Antonio Gamez Diaz <agamez@vmware.com>

--- dashboard/src/components/AppView/AppSecrets/AppSecrets.tsx
+++ dashboard/src/components/AppView/AppSecrets/AppSecrets.tsx
@@ -2,13 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import LoadingWrapper from "components/LoadingWrapper";
+import { filterByResourceRefs } from "containers/helpers";
+import { ResourceRef } from "gen/kubeappsapis/core/packages/v1alpha1/packages_pb";
 import { isEmpty } from "lodash";
 import { useSelector } from "react-redux";
-import { ResourceRef } from "gen/kubeappsapis/core/packages/v1alpha1/packages_pb";
 import { ISecret, IStoreState } from "shared/types";
 import SecretItemDatum from "../ResourceTable/ResourceItem/SecretItem/SecretItemDatum";
-import "./AppSecrets.css";
-import { filterByResourceRefs } from "containers/helpers";
 
 interface IResourceTableProps {
   secretRefs: ResourceRef[];
--- dashboard/src/components/InfoCard/Card/CardBlock/index.tsx
+++
@@ -1,5 +0,0 @@
-// Copyright 2020-2023 the Kubeapps contributors.
-// SPDX-License-Identifier: Apache-2.0
-
-import CardBlock from "./CardBlock";
-export default CardBlock;
